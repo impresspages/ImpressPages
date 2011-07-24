937eca2e859d7b8413cca17733eef8b9cb0bb2de
Mangirdas <mangirdas@impresspages.org>
2011-07-24 11:46:14 +0300

    Install / Update scripts

--- update/includes/scripts.php
+++ update/includes/scripts.php
@@ -11,7 +11,7 @@ if (!defined('CMS')) exit;
 
 class Scripts {
   private $scripts;
-  const destinationVersion = '1.0.11';
+  const destinationVersion = '1.0.12';
   
   public function __construct(){
     $this->scripts = array();        
@@ -30,6 +30,7 @@ class Scripts {
 
     $this->scripts[] = array("from" => "1.0.9", "to" => "1.0.10", "script" => "update_1_0_9_to_1_0_10");
     $this->scripts[] = array("from" => "1.0.10", "to" => "1.0.11", "script" => "update_1_0_10_to_1_0_11");
+    $this->scripts[] = array("from" => "1.0.11", "to" => "1.0.12", "script" => "update_1_0_11_to_1_0_12");
   }
 
   public function getScripts($fromVersion = "1.0.0 Alpha"){
---
+++ update/scripts/update_1_0_11_to_1_0_12/translations.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package   ImpressPages
+ * @copyright Copyright (C) 2011 ImpressPages LTD.
+ * @license   GNU/GPL, see ip_license.html
+ */
+
+namespace update_1_0_11_beta_to_1_0_12;
+
+if (!defined('CMS')) exit;
+
+define ('REMOVE_DIRECTORIES','<p><b>Please remove following directories:</b></p>');
+define ('REMOVE_FILES','<p><b>Please remove following files:</b></p>');
+define ('UPLOAD_DIRECTORIES', '<p><b>Please upload following folders from downloaded archive:</b></p>');
+define ('UPLOAD_FILES', '<p><b>Please upload following files from downloaded archive:</b></p>');
+define ('MAKE_ROBOTS_WRITEABLE', '<p>Make file robots.txt writeable.</p>');
+define ('LATER', 'I will do that later.');
