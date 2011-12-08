5acd506c551911377254882d07f97ecbd9897c42
Mangirdas <mangirdas@impresspages.org>
2011-12-08 22:50:38 +0200

    New version number

--- install/translations/nl.php
+++ install/translations/nl.php
@@ -14,7 +14,7 @@ define('IP_BACK', 'Vorige');
 define('IP_NEXT', 'Volgende');
 define('IP_ACCEPT', 'Accepteren');
 define('IP_INSTALLATION', 'ImpressPages CMS installatie wizard');
-define('IP_VERSION', 'Versie 1.1.0rc1');
+define('IP_VERSION', 'Versie 2.0rc1');
 define('IP_SUBDIRECTORIES', '(inclusief submappen en bestanden)');
 define('IP_OPTIONAL', '(optie)');
 
