a73d876054eba7d360d14aa73ee4364c8ce2416c
Audrius Jankauskas <audrius@impresspages.org>
2011-12-11 15:42:47 +0200

    Renaming IDs and Classes to follow naming convention

--- ip_cms/modules/standard/content_management/public/jquery.ip.widget.js
+++ ip_cms/modules/standard/content_management/public/jquery.ip.widget.js
@@ -195,7 +195,7 @@
                 $this.data('ipWidget').status = IP_WIDGET_STATE_SAVE_PROGRESS;
                 widgetPluginObject.prepareData();
             } else {
-                var widgetInputs = $this.find('.ipAdminWidgetBody').find(':input');
+                var widgetInputs = $this.find('.ipaBody').find(':input');
                 var data = Object();
                 widgetInputs.each(function(index) {
                     data[$(this).attr('name')] = $(this).val();
