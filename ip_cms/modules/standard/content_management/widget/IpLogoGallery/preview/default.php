13ff736338a723563206551af8fec5251e5fe4a6
Audrius Jankauskas <audrius@impresspages.org>
2011-12-14 16:51:54 +0200

    Fixing theme to follow new structure for preview

--- ip_cms/modules/standard/content_management/widget/IpLogoGallery/preview/default.php
+++ ip_cms/modules/standard/content_management/widget/IpLogoGallery/preview/default.php
@@ -1,14 +1,16 @@
 <?php if (isset($logos) && is_array($logos)){ ?>
 <ul>
-<?php foreach ($logos as $logoKey => $logo) { ?>
-<?php $curLogo = isset($logo['logoSmall']) ? $logo['logoSmall'] : ''; ?>
-<?php $curTitle = isset($logo['title']) ? $logo['title'] : ''; ?>
-<?php $curLink = isset($logo['link']) ? $logo['link'] : ''; ?>
-    <li><a href="<?php echo htmlspecialchars($curLink) ?>"
-        title="<?php echo htmlspecialchars($curTitle) ?>"> <img
-            src="<?php echo htmlspecialchars(BASE_URL.$curLogo ) ?>"
-            alt="<?php htmlspecialchars($curTitle) ?>" /> </a>
+<?php
+foreach ($logos as $logoKey => $logo) { 
+    $curLogo = isset($logo['logoSmall']) ? $logo['logoSmall'] : '';
+    $curTitle = isset($logo['title']) ? $logo['title'] : '';
+    $curLink = isset($logo['link']) ? $logo['link'] : '';
+?>
+    <li>
+        <a href="<?php echo htmlspecialchars($curLink); ?>" title="<?php echo htmlspecialchars($curTitle); ?>">
+            <img src="<?php echo htmlspecialchars(BASE_URL.$curLogo); ?>" alt="<?php htmlspecialchars($curTitle); ?>" />
+        </a>
     </li>
-    <?php } ?>
+<?php } ?>
 </ul>
-    <?php } ?>
+<?php } ?>
