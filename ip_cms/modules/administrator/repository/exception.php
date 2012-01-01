1ee0060e57b0e348f6e916f34dc5b5e201b206ee
Mangirdas <mangirdas@impresspages.org>
2012-01-01 19:50:12 +0200

    repository module

---
+++ ip_cms/modules/administrator/repository/exception.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @package ImpressPages
+ * @copyright   Copyright (C) 2011 ImpressPages LTD.
+ * @license GNU/GPL, see ip_license.html
+ */
+
+namespace Modules\administrator\repository;
+
+
+if (!defined('CMS')) exit;
+
+/**
+ * IpCmsException class
+ */
+class Exception extends \Exception
+{
+    //error codes
+    const DB = 0;
+
+    // Redefine the exception so message isn't optional
+    public function __construct($message, $code = 0, \Exception $previous = null) {
+        // make sure everything is assigned properly
+        parent::__construct($message, $code, $previous);
+    }
+    
+
+}
---
+++ ip_cms/modules/administrator/repository/model.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * @package ImpressPages
+ * @copyright   Copyright (C) 2011 ImpressPages LTD.
+ * @license GNU/GPL, see ip_license.html
+ */
+namespace Modules\administrator\repository;
+if (!defined('CMS')) exit;
+
+
+
+/**
+ * 
+ * Centralized repository to store files. Often the same image needs to be used by many 
+ * modules / widgets. This class handles these dependences. Use this module to add new files to global
+ * files repository. Bind new modules to already existing files. When the file is not bind to any module,
+ * it is automatically removed. So bind to existing files, undbind from them and don't whorry if some other
+ * modules uses the same files. This class will take care.
+ * 
+ * @author Mangirdas
+ *
+ */
+class Model{
+
+    
+    /**
+     * Add new file to the repository. Defined file will be duplicated. File name of dupliace will be returned. 
+     * 
+     * @param string $file file to be added. Relative to BASE_DIR. E.g. file/tmp/file.doc
+     * @param string $module module that uses this file (eg. standard/content_management)
+     * @param int $id Unique identificator. Tells in which part of the module the file is used.
+     * @return string where duplicated file is being stored. 
+     **/
+    public static function addFile($file, $module, $instanceId) {
+        $destinationDir = FILE_DIR;
+        $unocupiedName = \Library\Php\File\Functions::genUnocupiedName($file, $destinationDir);
+        copy(BASE_DIR.$file, BASE_DIR.$destinationDir.$unocupiedName);
+        self::bindFile($destinationDir.$unocupiedName, $module, $id);
+        return BASE_DIR.$unocupiedName;
+    }
+    
+    public static function bindFile($file, $module, $instanceId) {
+        $sql = "
+        INSERT INTO
+            `".DB_PREF."m_administrator_repository`
+        SET
+            `fileName` = '".mysql_real_escape_string($file)."',
+            `module` = '".mysql_real_escape_string($module)."',
+            `instanceId` = '".mysql_real_escape_string($instanceId)."',
+            `date` = '".time()."'
+        ";
+        
+        $rs = mysql_query($sql);
+        if (!$rs){
+            throw new Exception('Can\'t bind new instance to the file '.$sql.' '.mysql_error(), Exception::DB);
+        }
+        
+    }
+    
+    public static function unbindFile($file, $module, $id) {
+        $sql = "
+        DELETE FROM
+            `".DB_PREF."m_administrator_repository`
+        WHERE
+            `fileName` = '".mysql_real_escape_string($file)."' AND
+            `module` = '".mysql_real_escape_string($module)."' AND
+            `instanceId` = '".mysql_real_escape_string($instanceId)."'
+        ";
+        
+        $rs = mysql_query($sql);
+        if (!$rs){
+            throw new Exception('Can\'t file instance '.$sql.' '.mysql_error(), Exception::DB);
+        }
+        
+        $whoUses = self::whoUsesFile($file);
+        
+        if (count($whoUses) == 0) {
+            unlink(BASE_DIR.$file);
+        }
+    }
+    
+    public static function whoUsesFile($file){
+        $sql = "
+        SELECT FROM
+            `".DB_PREF."m_administrator_repository`
+        WHERE
+            `fileName` = '".mysql_real_escape_string($file)."'
+        ";
+        
+        $rs = mysql_query($sql);
+        if (!$rs){
+            throw new Exception('Can\'t bind new instance to the file '.$sql.' '.mysql_error(), Exception::DB);
+        }
+        
+        $answer = array();
+        
+        while($lock = mysql_fetch_assoc($rs)) {
+            $answer[] = $lock;
+        }
+        return $answer;
+    }
+    
+    
+}
