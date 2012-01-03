d346456275b4db731c16ce9d6b667cd0c575be7e
Mangirdas <mangirdas@impresspages.org>
2012-01-03 18:32:37 +0200

    IpTextImage use global file repository

--- ip_cms/modules/standard/content_management/widget/IpTextImage/IpTextImage.php
+++ ip_cms/modules/standard/content_management/widget/IpTextImage/IpTextImage.php
@@ -20,7 +20,6 @@ class IpTextImage extends \Modules\standard\content_management\Widget{
         $answer = '';
 
 
-        $destinationDir = BASE_DIR.IMAGE_DIR;
 
         $newData = $currentData;
 
@@ -34,33 +33,50 @@ class IpTextImage extends \Modules\standard\content_management\Widget{
                 throw new \Exception("Security notice. Try to access an image (".$postData['newImage'].") from a non temporary folder.");
             }
 
+            //remove old image
+            if (isset($currentData['imageOriginal']) && $currentData['imageOriginal']) {
+                \Modules\administrator\repository\Model::unbindFile($currentData['imageOriginal'], 'standard/content_management', $widgetId);
+            }
+            
+            
+            
             //new original image
-            $unocupiedName = \Library\Php\File\Functions::genUnocupiedName($postData['newImage'], $destinationDir);
-            copy($postData['newImage'], $destinationDir.$unocupiedName);
-            $newData['imageOriginal'] = IMAGE_DIR.$unocupiedName;
+            $newData['imageOriginal'] = \Modules\administrator\repository\Model::addFile($postData['newImage'], 'standard/content_management', $widgetId);
+            
 
+            //remove old big image
+            if (isset($currentData['imageBig']) && $currentData['imageBig']) {
+                \Modules\administrator\repository\Model::unbindFile($currentData['imageBig'], 'standard/content_management', $widgetId);
+            }            
 
-
-            $bigImageName = \Library\Php\Image\Functions::resize(
+            //new big image
+            $tmpBigImageName = \Library\Php\Image\Functions::resize(
             $postData['newImage'],
             $parametersMod->getValue('standard', 'content_management', 'widget_text_photo', 'big_width'),
             $parametersMod->getValue('standard', 'content_management', 'widget_text_photo', 'big_height'),
-            BASE_DIR.IMAGE_DIR,
+            TMP_IMAGE_DIR,
             \Library\Php\Image\Functions::CROP_TYPE_FIT,
             false,
             $parametersMod->getValue('standard', 'content_management', 'widget_text_photo', 'big_quality')
             );
-            $newData['imageBig'] = IMAGE_DIR.$bigImageName;
+            $newData['imageBig'] = \Modules\administrator\repository\Model::addFile(TMP_IMAGE_DIR.$tmpBigImageName, 'standard/content_management', $widgetId);
+            unlink(BASE_DIR.TMP_IMAGE_DIR.$tmpBigImageName);
+            
         }
 
         if (isset($postData['cropX1']) && isset($postData['cropY1']) && isset($postData['cropX2']) && isset($postData['cropY2'])) {
+            //remove old file
+            if(isset($currentData['imageSmall'])) {
+                \Modules\administrator\repository\Model::unbindFile($currentData['imageSmall'], 'standard/content_management', $widgetId);
+            }
+            
             //new small image
             $ratio = ($postData['cropX2'] - $postData['cropX1']) / ($postData['cropY2'] - $postData['cropY1']);
             $requiredWidth = round($parametersMod->getValue('standard', 'content_management', 'widget_text_photo', 'width'));
             $requiredHeight = round($requiredWidth / $ratio);
-            $smallImageName = \Library\Php\Image\Functions::crop (
+            $tmpSmallImageName = \Library\Php\Image\Functions::crop (
             $newData['imageOriginal'],
-            $destinationDir,
+            TMP_IMAGE_DIR,
             $postData['cropX1'],
             $postData['cropY1'],
             $postData['cropX2'],
@@ -69,7 +85,9 @@ class IpTextImage extends \Modules\standard\content_management\Widget{
             $requiredWidth,
             $requiredHeight
             );
-            $newData['imageSmall'] = IMAGE_DIR.$smallImageName;
+            $newData['imageSmall'] = \Modules\administrator\repository\Model::addFile(TMP_IMAGE_DIR.$tmpSmallImageName, 'standard/content_management', $widgetId);
+            unlink(BASE_DIR.TMP_IMAGE_DIR.$tmpSmallImageName);
+            
             $newData['cropX1'] = $postData['cropX1'];
             $newData['cropY1'] = $postData['cropY1'];
             $newData['cropX2'] = $postData['cropX2'];
@@ -87,5 +105,25 @@ class IpTextImage extends \Modules\standard\content_management\Widget{
         return parent::managementHtml($instanceId, $data, $layout);
     }    
 
+    
+
+    public function delete($widgetId, $data) {
+        self::_deleteImage($data, $widgetId);
+    }
+    
+    private function _deleteImage($data, $widgetId) {
+        if (!is_array($data)) {
+            return;
+        }
+        if (isset($data['imageOriginal']) && $data['imageOriginal']) {
+            \Modules\administrator\repository\Model::unbindFile($data['imageOriginal'], 'standard/content_management', $widgetId);
+        }
+        if (isset($data['imageBig']) && $data['imageBig']) {
+            \Modules\administrator\repository\Model::unbindFile($data['imageBig'], 'standard/content_management', $widgetId);
+        }
+        if (isset($data['imageSmall']) && $data['imageSmall']) {
+            \Modules\administrator\repository\Model::unbindFile($data['imageSmall'], 'standard/content_management', $widgetId);
+        }        
+    }        
 
 }
