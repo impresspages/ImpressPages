0f347c100a3ff014910f587b343b6565538fa92a
Mangirdas <mangirdas@impresspages.org>
2012-01-14 14:33:11 +0000

    Search view -> HTML beauty

--- ip_cms/modules/administrator/search/view/elements_list.php
+++ ip_cms/modules/administrator/search/view/elements_list.php
@@ -1,16 +1,18 @@
 <ul class="modAdministratorSearchList">
-    <?php foreach ($elements as $key => $element) { ?>
-    <?php 
+<?php foreach ($elements as $key => $element) { ?>
+<?php 
         $tmpTitle = $element->getPageTitle();
         if($tmpTitle == ''){ 
             $tmpTitle = $element->getButtonTitle(); 
         }
-    ?>
+?>
     <li> 
         <a class="modAdministratorSearchLink" href="<?php echo $element->getLink(); ?>"><?php echo $this->esc($tmpTitle) ?></a>
-        <?php if($this->par('administrator/search/options/show_description')){ ?>
-            <p class="modAdministratorSearchDescription"><?php echo $this->esc($element->getDescription()) ?></p>
-        <?php }?>
+<?php   if($this->par('administrator/search/options/show_description')){ ?>
+            <p class="modAdministratorSearchDescription">
+                <?php echo $this->esc($element->getDescription()) ?>
+            </p>
+<?php   }?>
     </li>
-    <?php } ?>
+<?php } ?>
 </ul>
