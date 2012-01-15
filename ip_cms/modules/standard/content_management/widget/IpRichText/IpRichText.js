64f735a73b69f8b7e0eef0cfc0c2fb6779547335
Mangirdas <mangirdas@impresspages.org>
2012-01-15 21:56:59 +0000

    Rich text widget

---
+++ ip_cms/modules/standard/content_management/widget/IpRichText/IpRichText.js
@@ -0,0 +1,32 @@
+/**
+ * @package ImpressPages
+ * @copyright Copyright (C) 2011 ImpressPages LTD.
+ * @license GNU/GPL, see ip_license.html
+ */
+
+function IpWidget_IpRichText(widgetObject) {
+    this.widgetObject = widgetObject;
+
+    this.manageInit = manageInit;
+    this.prepareData = prepareData;
+
+
+    function manageInit() {
+        var instanceData = this.widgetObject.data('ipWidget');
+        this.widgetObject.find('textarea').tinymce(ipTinyMceConfigMax);
+    }
+
+    function prepareData() {
+
+        var data = Object();
+
+        data.text = this.widgetObject.find('textarea').html();
+        $(this.widgetObject).trigger('preparedWidgetData.ipWidget', [ data ]);
+    }
+
+    
+
+};
+
+      
+
