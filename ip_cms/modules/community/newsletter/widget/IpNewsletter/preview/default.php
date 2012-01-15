d0eb0ae352663d1aa721b9513fe9eb355b600564
Mangirdas <mangirdas@impresspages.org>
2012-01-15 21:11:58 +0000

    IpNewsletter widget

---
+++ ip_cms/modules/community/newsletter/widget/IpNewsletter/preview/default.php
@@ -0,0 +1,12 @@
+<?php 
+global $site;
+$zone = $site->getZoneByModule('community', 'newsletter');
+if ($zone) {
+    
+    echo $zone->generateRegistrationBox();
+    
+} else {
+    echo 'Please create newsletter zone';
+}
+
+?>
