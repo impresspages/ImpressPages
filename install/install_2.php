75885eb9172a1e75b1e073c6563c700b12f9f441
Mangirdas <mangirdas@impresspages.org>
2012-02-20 22:28:33 +0000

    Update install text

--- install/install_2.php
+++ install/install_2.php
@@ -26,7 +26,10 @@ This program is free software: you can redistribute it and/or modify it under th
 This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 </p><p>
 You are required to keep these "Appropriate Legal Notices" intact as specified in GPL3 section 5(d) and 7(b).
-</p><p>
+</p>
+<p>&nbsp;</p>
+<p><strong>Automatic updates disclaimer</strong></p>
+<p>ImpressPages is configured to check for the updates automatically. This requires for the technical data of the website to be transfered to ImpressPages service servers. This process does not transfer any personal data or any part of the website\'s content.</p>
 
 
 <a class="button_act" href="?step=3">'.IP_ACCEPT.'</a>
