b613d9ddee7bd774ad45d14ac0ceef4e8fa056d1
Audrius Jankauskas <audrius@impresspages.org>
2012-02-28 18:06:29 +0200

    Fixing F.A.Q. widget: new functionality, layouts, styles, etc.

--- ip_cms/modules/standard/content_management/widget/IpFaq/preview/expanded.php
+++ ip_cms/modules/standard/content_management/widget/IpFaq/preview/expanded.php
@@ -1,4 +1,4 @@
-<dl class="ipwContainer collapsed">
+<dl class="ipwContainer expanded">
     <dl class="ipwQuestion"><?php echo isset($question) ? htmlspecialchars($question) : ''; ?></dl>
     <dd class="ipwAnswer">
 <?php echo isset($answer) ? $answer : ''; ?>
