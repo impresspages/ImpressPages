a28573113a1d59d27bdf33a650adcd3c6f84f73c
Mangirdas <mangirdas@impresspages.org>
2012-03-10 21:49:08 +0000

    IpForm library empty field.

---
+++ ip_libs/IpForm/Field/Blank.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @package ImpressPages
+ * @copyright   Copyright (C) 2012 ImpressPages LTD.
+ * @license GNU/GPL, see ip_license.html
+ */
+
+namespace Library\IpForm\Field;
+
+
+/**
+ * Empty field. Common usage is to display global form error. 
+ * Fore eg. for some reason the form could not be saved.
+ * The error is not specific to any of the fields.
+ * If your form could have such errors, you can put this empty 
+ * field at the top of your form and assign error message to it. 
+ * Then this error will appear above all fieldas as a global form error.
+ * 
+ */
+class Blank extends Field{
+    
+    public function render($doctype) {
+        return '';
+    }
+}
