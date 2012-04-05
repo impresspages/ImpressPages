3aac1bb82c85c7823feee9f3ab275477c22b5dbe
Mangirdas <mangirdas@impresspages.org>
2012-04-06 00:13:15 +0100

    remove console.log

--- ip_cms/modules/standard/configuration/tinymce/med.js
+++ ip_cms/modules/standard/configuration/tinymce/med.js
@@ -36,8 +36,6 @@ ipTinyMceConfigMed = {
     paste_text_use_dialog : true,    
     
     paste_preprocess : function(pl, o) {
-    console.log(pl);
-    console.log(o);
         ipTinyMceConfigPastePreprocess(pl, o, new Array('caption', 'signature', 'note'));
     }
     
