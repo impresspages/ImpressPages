86bd64acf8e052d31e4ca1945e874c4fa0a071d4
Mangirdas <mangirdas@impresspages.org>
2012-04-22 15:05:37 +0100

    user login form remove additional links. Fixes #58

--- ip_cms/modules/community/user/view/login.php
+++ ip_cms/modules/community/user/view/login.php
@@ -1,8 +1,2 @@
 <?php echo $this->renderWidget('IpTitle', array('title' => $this->par('community/user/translations/title_login'))); ?>
 <?php echo $this->renderWidget('IpUserLogin'); ?>
-<?php if (isset($passwordResetLink)){ ?>
-<a href="<?php echo $passwordResetLink; ?>"><?php echo $this->esc($this->par('community/user/translations/password_reset')); ?></a>
-<?php }?>
-<?php if (isset($registrationLink)){ ?>
-<a href="<?php echo $registrationLink; ?>"><?php echo $this->esc($this->par('community/user/translations/title_registration')); ?></a>
-<?php }?>
