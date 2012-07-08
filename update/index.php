49c8a746bc125b997af9841263425640ab511db3
Mangirdas Skripka <mangirdas@impresspages.org>
2012-07-08 23:01:27 +0100

    update comment

--- update/index.php
+++ update/index.php
@@ -7,7 +7,7 @@
  */
 
 //error_reporting(E_ALL|E_STRICT);
-//ini_set('display_errors', '0');
+//ini_set('display_errors', '1');
 
 require_once(__DIR__.'/Gui/Bootstrap.php');
 $bootstrap = new \IpUpdate\Gui\Bootstrap();
