c4a8d7b434baba9912c8772f5b3e84f1ab9f3dd2
Mangirdas Skripka <mangirdas@impresspages.org>
2012-07-21 22:56:36 +0100

    update: fix migrations from 2.0rc1 to 2.4

--- update/PhpUnit/Tests/Library/ServiceTest.php
+++ update/PhpUnit/Tests/Library/ServiceTest.php
@@ -23,13 +23,13 @@ class ServiceTest extends \IpUpdate\PhpUnit\UpdateTestCase
         
         //installl
         
-        $installation = new \IpUpdate\PhpUnit\Helper\Installation('2.0rc1');
+        $installation = new \IpUpdate\PhpUnit\Helper\Installation('2.0rc2');
         $installation->install();
 
         $service = new \IpUpdate\Library\Service($installation->getInstallationDir());
 
         $version = $service->getCurrentVersion();
-        $this->assertEquals('2.0rc1', $version);
+        $this->assertEquals('2.0rc2', $version);
         $this->assertUrlResponse($installation->getInstallationUrl(), 200);
         
         $configurationParser = new \IpUpdate\Library\Model\ConfigurationParser();
