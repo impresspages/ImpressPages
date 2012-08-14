069a3d3476149382aa373956aed8f758dd5e94e7
Mangirdas <mangirdas@impresspages.org>
2012-08-14 23:47:26 +0100

    check .htaccess file test

--- phpunit/Tests/install/installTest.php
+++ phpunit/Tests/install/installTest.php
@@ -9,6 +9,58 @@ class InstallTest extends \PhpUnit\SeleniumTestCase
 {
     public function testInstallCurrent()
     {
+        $this->prepareForInstallation();
+
+        $this->clickAndWait('css=.button_act');
+        $this->assertText ('css=h1', 'System check');
+        $this->assertNoErrors();
+
+        $this->clickAndWait('css=.button_act');
+        $this->assertText('css=h1', 'ImpressPages Legal Notices');
+        $this->assertNoErrors();
+
+        $this->clickAndWait('css=.button_act');
+        $this->assertText('css=h1', 'Database installation');
+        //$this->assertNoErrors(); //there is hidden error message
+
+        $testDbHelper = new \PhpUnit\Helper\TestDb();
+        $this->type('css=#db_server', $testDbHelper->getDbHost());
+        $this->type('css=#db_user', $testDbHelper->getDbUser());
+        $this->type('css=#db_pass', $testDbHelper->getDbPass());
+        $this->type('css=#db_db', $testDbHelper->getDbName());
+        $this->click('css=.button_act');
+        $this->waitForVisible('css=#config_site_name');
+        //$this->assertNoErrors();  //there is hidden error message
+
+
+        $this->type('css=#config_site_name', 'TestSiteName');
+        $this->type('css=#config_site_email', 'test@example.com');
+        $this->type('css=#config_login', 'admin');
+        $this->type('css=#config_pass', 'admin');
+        $this->type('css=#config_email', 'test@example.com');
+        $this->select('css=#config_timezone', 'value=Europe/London');
+        $this->clickAndWait('css=.button_act');
+        $this->assertText('css=h1', 'ImpressPages CMS successfully installed.');
+        $this->assertNoErrors();
+
+
+        $this->clickAndWait('css=#content a');
+        $this->assertNoErrors();
+        $this->assertText('css=.sitename', 'TestSiteName');
+
+    }
+
+    public function testNoHtAccess() {
+        $this->prepareForInstallation();
+
+        unlink(TEST_TMP_DIR.'.htaccess');
+        $this->clickAndWait('css=.button_act');
+        $this->assertVisible('css=span.error');
+        sleep(50);
+    }
+
+    private function prepareForInstallation()
+    {
         $tmpInstallDir = TEST_TMP_DIR;
         $testUrl = TEST_TMP_URL;
 
@@ -52,7 +104,7 @@ class InstallTest extends \PhpUnit\SeleniumTestCase
 
         file_put_contents($tmpInstallDir.'robots.txt', '');
         file_put_contents($tmpInstallDir.'ip_config.php',
-'<?php
+            '<?php
 
  if(!isset($_GET[\'install\']))
     header("location: install/?install=1");
@@ -60,45 +112,6 @@ class InstallTest extends \PhpUnit\SeleniumTestCase
 
         $this->open($testUrl);
         $this->assertNoErrors();
-
-        $this->clickAndWait('css=.button_act');
-        $this->assertText ('css=h1', 'System check');
-        $this->assertNoErrors();
-
-        $this->clickAndWait('css=.button_act');
-        $this->assertText('css=h1', 'ImpressPages Legal Notices');
-        $this->assertNoErrors();
-
-        $this->clickAndWait('css=.button_act');
-        $this->assertText('css=h1', 'Database installation');
-        //$this->assertNoErrors(); //there is hidden error message
-
-        $testDbHelper = new \PhpUnit\Helper\TestDb();
-        $this->type('css=#db_server', $testDbHelper->getDbHost());
-        $this->type('css=#db_user', $testDbHelper->getDbUser());
-        $this->type('css=#db_pass', $testDbHelper->getDbPass());
-        $this->type('css=#db_db', $testDbHelper->getDbName());
-        $this->click('css=.button_act');
-        $this->waitForVisible('css=#config_site_name');
-        //$this->assertNoErrors();  //there is hidden error message
-
-
-        $this->type('css=#config_site_name', 'TestSiteName');
-        $this->type('css=#config_site_email', 'test@example.com');
-        $this->type('css=#config_login', 'admin');
-        $this->type('css=#config_pass', 'admin');
-        $this->type('css=#config_email', 'test@example.com');
-        $this->select('css=#config_timezone', 'value=Europe/London');
-        $this->clickAndWait('css=.button_act');
-        $this->assertText('css=h1', 'ImpressPages CMS successfully installed.');
-        $this->assertNoErrors();
-
-
-        $this->clickAndWait('css=#content a');
-        $this->assertNoErrors();
-        $this->assertText('css=.sitename', 'TestSiteName');
-
-
-
     }
+
 }
