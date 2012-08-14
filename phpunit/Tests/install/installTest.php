d03c5036b98fd04bfeb6a2c7eb6f1b0810847e9e
Mangirdas <mangirdas@impresspages.org>
2012-08-14 21:13:41 +0100

    general test case

--- phpunit/Tests/install/installTest.php
+++ phpunit/Tests/install/installTest.php
@@ -9,7 +9,71 @@ class InstallTest extends \PhpUnit\SeleniumTestCase
 {
     public function testInstallCurrent()
     {
+        $tmpInstallDir = TEST_TMP_DIR;
+        $testUrl = TEST_TMP_URL;
+
+        $folders = array(
+            'audio',
+            'file',
+            'image',
+            'install',
+            'ip_cms',
+            'ip_configs',
+            'ip_libs',
+            'ip_plugins',
+            'ip_themes',
+            'update',
+            'video'
+        );
+
+        $files = array(
+            'admin.php',
+            'favicon.ico',
+            'index.php',
+            'ip_backend_frames.php',
+            'ip_backend_worker.php',
+            'ip_config.php',
+            'ip_cron.php',
+            'ip_license.html',
+            'readme.txt',
+            'robots.txt',
+            'sitemap.php',
+            '.htaccess'
+        );
+
+
         $fs = new \PhpUnit\Helper\FileSystem();
-        $fs->cpDir('');
+        foreach($folders as $folder) {
+            $fs->cpDir(CODEBASE_DIR.$folder, $tmpInstallDir.$folder);
+        }
+        foreach($files as $file) {
+            copy(CODEBASE_DIR.$file, $tmpInstallDir.$file);
+        }
+
+        file_put_contents($tmpInstallDir.'robots.txt', '');
+        file_put_contents($tmpInstallDir.'ip_config.php',
+'<?php
+
+ if(!isset($_GET[\'install\']))
+    header("location: install/?install=1");
+        ');
+
+        $this->open($testUrl);
+        $this->assertNoErrors();
+
+        $this->clickAndWait('css=.button_act');
+        $this->assertText ('css=h1', 'System check');
+        $this->assertNoErrors();
+
+        $this->clickAndWait('css=.button_act');
+        $this->assertText('css=h1', 'ImpressPages Legal Notices');
+        $this->assertNoErrors();
+
+        $this->clickAndWait('css=.button_act');
+
+
+        sleep(100000);
+
+
     }
 }
