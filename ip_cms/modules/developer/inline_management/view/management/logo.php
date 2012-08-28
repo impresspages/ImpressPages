19a2da7feabfa48996194cd1264bf30ab48fe29b
Audrius Jankauskas <audrius@impresspages.org>
2012-08-29 01:25:36 +0300

    Controls for inline logo management

--- ip_cms/modules/developer/inline_management/view/management/logo.php
+++ ip_cms/modules/developer/inline_management/view/management/logo.php
@@ -1,5 +1,4 @@
 <div class="ipModuleInlineManagement" data-cssclass="<?php echo $this->esc($cssClass) ?>">
-    <div class="ipmEdit"><span><?php echo $this->escPar('standard/configuration/admin_translations/edit'); ?></span></div>
     <div class="ipmText" <?php echo (isset($type) && $type == 'text') ? '' : 'style="display: none;"'; ?>>
         <?php echo isset($logoTextHtml) ? $logoTextHtml : '' ?>
     </div>
