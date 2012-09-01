53446b3ad2966676b6501806f2caba4ec36a800a
mangirdas <mangirdas@impresspages.org>
2012-09-01 11:48:46 +0100

    fix installation script sql

--- install/sql/structure.sql
+++ install/sql/structure.sql
@@ -303,7 +303,7 @@ DROP TABLE IF EXISTS `ip_cms_m_inline_value_language`;
 CREATE TABLE IF NOT EXISTS `ip_cms_m_inline_value_language` (
   `module` varchar(100) NOT NULL,
   `key` varchar(255) NOT NULL,
-  `langaugeId` int(11) NOT NULL,
+  `languageId` int(11) NOT NULL,
   `value` text NOT NULL,
   PRIMARY KEY (`module`,`key`,`langaugeId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
