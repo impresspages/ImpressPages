8ac903b8b1f9ae3c34e7e2e3939fbb3cc25e4749
mangirdas <mangirdas@impresspages.org>
2012-10-14 19:11:20 +0300

    fix unit test link

--- phpunit/Tests/update/Library/Migration/To2_3Test.php
+++ phpunit/Tests/update/Library/Migration/To2_3Test.php
@@ -15,7 +15,7 @@ class To2_3Test extends \PhpUnit\GeneralTestCase
     {
         $script = new \IpUpdate\Library\Migration\To2_3\Script();
         $downloadUrl = $script->getDownloadUrl();
-        $this->assertEquals(true, $downloadUrl == 'http://sourceforge.net/projects/impresspages/files/ImpressPages_2_3.zip/download' || $downloadUrl == 'http://localhost/sourceforge.net/ImpressPages_2_3.zip');
+        $this->assertEquals(true, $downloadUrl == 'http://download.impresspages.org/ImpressPages_2_3.zip' || $downloadUrl == 'http://localhost/sourceforge.net/ImpressPages_2_3.zip');
     }
 
 
