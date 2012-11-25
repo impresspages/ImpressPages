c53c75f9aa0933d69407dc49a97df2f699e97304
mangirdas <mangirdas@impresspages.org>
2012-11-26 00:43:07 +0200

    remove unused files cron

---
+++ ip_cms/modules/administrator/repository/cron.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @package	ImpressPages
+ * @copyright	Copyright (C) 2011 ImpressPages LTD.
+ * @license see ip_license.html
+ */
+namespace Modules\administrator\repository;
+
+require_once (__DIR__.'/model.php');
+
+
+class Cron{
+
+    function execute($options){
+
+        if ($options->firstTimeThisWeek) {
+            $model = new Model();
+            //repository binds files only on new uploads. Just to track new uploaded files.
+            //this function unbinds those files. If within 24h none of modules has bind new files,
+            //they are deleted
+            $repositoryAssociatedFiles = $model->findFiles('administrator/repository', 0);
+            foreach($repositoryAssociatedFiles as $file) {
+                if ($file['date'] + 60*60*24 < time()){ //older than one day
+                    \Modules\administrator\repository\Model::unbindFile($file['fileName'], 'administrator/repository', 0);
+                }
+            }
+        }
+
+    }
+
+}
