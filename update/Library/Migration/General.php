2cd86fbe20a2db6907d5894f214b337b0550d645
mangirdas <mangirdas@impresspages.org>
2012-12-13 23:50:36 +0200

    update script error handling fix

--- update/Library/Migration/General.php
+++ update/Library/Migration/General.php
@@ -70,7 +70,7 @@ abstract class General
         $answer = json_decode($jsonAnswer);
         
         if ($answer === null) {
-            throw new \Exception("Can't get version info about version ".$this->getDestinationVersion().". Server answer: ".$jsonAnswer." ", \IpUpdate\Library\UpdateException::UNKNOWN, array());
+            throw new \IpUpdate\Library\Exception("Can't get version info about version ".$this->getDestinationVersion().". Server answer: ".$jsonAnswer." ", \IpUpdate\Library\UpdateException::UNKNOWN);
         }
         
         $this->versionInfo = $answer;
