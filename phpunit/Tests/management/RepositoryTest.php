e3be1eb88e0e61549890eb86e1b2bb16ce46c7ca
mangirdas <mangirdas@impresspages.org>
2012-12-29 18:59:35 +0200

    repository test

--- phpunit/Tests/management/RepositoryTest.php
+++ phpunit/Tests/management/RepositoryTest.php
@@ -9,13 +9,15 @@ class RepositoryTestTest extends \PhpUnit\SeleniumTestCase
 {
 
  
-    public function testNewFilesUpload()
+    public function testNewFilesUploadRemoval()
     {
         $installation = new \PhpUnit\Helper\Installation();
         $installation->install();
 
         $ipActions = new \PhpUnit\Helper\IpActions($this, $installation);
         $ipActions->login();
+
+        //add file widget
         $ipActions->addWidget('IpFile');
         $this->waitForElementPresent('css=.ipAdminWidget-IpFile .ipmBrowseButton');
         $this->click('css=.ipAdminWidget-IpFile .ipmBrowseButton');
@@ -28,8 +30,10 @@ class RepositoryTestTest extends \PhpUnit\SeleniumTestCase
         $this->type("css=.plupload input", TEST_FIXTURE_DIR."Repository/testFile.txt");
         $this->waitForElementPresent('css=#ipModRepositoryTabUpload .ipmFiles .ipmFile');
         $this->click('css=#ipModRepositoryTabUpload .ipaConfirm');
-        sleep(1);
+        sleep(1); //wait for popup to close
         $this->assertElementNotPresent('css=#ipModRepositoryTabUpload');
+        $this->click('css=.ipActionWidgetCancel');
+        $this->waitForElementNotPresent('css=.ipActionWidgetCancel');
         $this->assertEquals(file_exists($installation->getConfig('BASE_DIR').$installation->getConfig('FILE_DIR').'testFile.txt'), TRUE);
 
         //check if file is automatically removed if not used
@@ -38,9 +42,33 @@ class RepositoryTestTest extends \PhpUnit\SeleniumTestCase
         \PhpUnit\Helper\Time::restoreTime(); //+7 days and 1s.
         $this->assertEquals(file_exists($installation->getConfig('BASE_DIR').$installation->getConfig('FILE_DIR').'testFile.txt'), FALSE);
 
+        //add file widget again
+        $ipActions->login(); //because we are in ip_cron now
+        $ipActions->addWidget('IpFile');
+        $this->waitForElementPresent('css=.ipAdminWidget-IpFile .ipmBrowseButton');
+        $this->click('css=.ipAdminWidget-IpFile .ipmBrowseButton');
+        $this->waitForElementPresent('css=.ipModRepositoryPopup .ipmBrowseButton');
 
+        //try to upload file
+        $this->assertEquals(file_exists($installation->getConfig('BASE_DIR').$installation->getConfig('FILE_DIR').'testFile.txt'), FALSE);
+        $this->type("css=.plupload input", TEST_FIXTURE_DIR."Repository/testFile.txt");
+        $this->waitForElementPresent('css=#ipModRepositoryTabUpload .ipmFiles .ipmFile');
+        $this->click('css=#ipModRepositoryTabUpload .ipaConfirm');
+        sleep(1); //wait for popup to close
+        $this->assertElementNotPresent('css=#ipModRepositoryTabUpload');
+        $this->click('css=.ipActionWidgetSave');
+        $this->waitForElementNotPresent('css=.ipActionWidgetCancel');
+        $this->assertEquals(file_exists($installation->getConfig('BASE_DIR').$installation->getConfig('FILE_DIR').'testFile.txt'), TRUE);
+
+
+        //check if file is NOT automatically removed
+        \PhpUnit\Helper\Time::changeTime(60*60*24*15 + 1); //+7 days and 1s.
+        $this->open($installation->getInstallationUrl().'ip_cron.php'); //cron should remove our file because it is not used by any widget yet
+        \PhpUnit\Helper\Time::restoreTime(); //+7 days and 1s.
+        $this->assertEquals(file_exists($installation->getConfig('BASE_DIR').$installation->getConfig('FILE_DIR').'testFile.txt'), TRUE);
 
     }
+
 
 
 
