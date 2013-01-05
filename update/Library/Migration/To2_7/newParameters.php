93ed976e4f199523172aab7a9e18e512c487dab2
mangirdas <mangirdas@impresspages.org>
2013-01-05 21:44:00 +0200

    default image quality setting

--- update/Library/Migration/To2_7/newParameters.php
+++ update/Library/Migration/To2_7/newParameters.php
@@ -12,3 +12,9 @@ $parameterTitle["standard"]["configuration"]["system_translations"]["add_new"] =
 $parameterValue["standard"]["configuration"]["system_translations"]["add_new"] = "Add new";
 $parameterAdmin["standard"]["configuration"]["system_translations"]["add_new"] = "0";
 $parameterType["standard"]["configuration"]["system_translations"]["add_new"] = "string";
+
+
+$parameterTitle["standard"]["configuration"]["advanced_options"]["default_image_quality"] = "Default image quality (0 - 100)";
+$parameterValue["standard"]["configuration"]["advanced_options"]["default_image_quality"] = "80";
+$parameterAdmin["standard"]["configuration"]["advanced_options"]["default_image_quality"] = "1";
+$parameterType["standard"]["configuration"]["advanced_options"]["default_image_quality"] = "integer";
