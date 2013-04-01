77de772557e12d552f590dcefea4f36f55fa62a4
Mangirdas <mangirdas@impresspages.org>
2013-04-01 18:50:44 +0300

    global order paid event

---
+++ ip_cms/includes/Ip/Event/OrderPaid.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @package ImpressPages
+ * @copyright   Copyright (C) 2011 ImpressPages LTD.
+ * @license see ip_license.html
+ */
+namespace Ip\Event;
+
+
+
+class OrderPaid extends \Ip\Event{
+    
+    const ORDER_PAID = 'global.orderPaid';
+
+
+    protected $module;
+    protected $orderId;
+
+    /**
+     * @param object $object
+     * @param string $module
+     * @param int $orderId
+     */
+    public function __construct($object, $module, $orderId) {
+        $this->module = $module;
+        $this->orderId = $orderId;
+        parent::__construct($object, self::ORDER_PAID, array());
+    }
+
+    public function getModule()
+    {
+        return $this->module;
+    }
+
+    public function getOrderId()
+    {
+        return $this->orderId;
+    }
+}
