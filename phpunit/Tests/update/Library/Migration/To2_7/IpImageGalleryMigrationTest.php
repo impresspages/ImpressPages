9c4a85d6db3f9e182a8b7417d609a269d26a1721
Mangirdas <mangirdas@impresspages.org>
2013-04-28 17:02:57 +0300

    IpImageGallery widget migration test

--- phpunit/Tests/update/Library/Migration/To2_7/IpImageGalleryMigrationTest.php
+++ phpunit/Tests/update/Library/Migration/To2_7/IpImageGalleryMigrationTest.php
@@ -5,13 +5,13 @@
  * @license   GNU/GPL, see ip_license.html
  */
 
-class IpImageMigrationTest extends \PhpUnit\MigrationTestCase
+class IpImageGalleryMigrationTest extends \PhpUnit\MigrationTestCase
 {
 
 
     public function getDataSet()
     {
-        return $this->createXMLDataSet(TEST_FIXTURE_DIR.'update/Library/Migration/To2_7/ipImage.xml');
+        return $this->createXMLDataSet(TEST_FIXTURE_DIR.'update/Library/Migration/To2_7/ipImageGallery.xml');
     }
 
 
@@ -19,11 +19,11 @@ class IpImageMigrationTest extends \PhpUnit\MigrationTestCase
     {
         $config = $this->getInstallationConfig();
 
-        $myFile = $config['BASE_DIR'].$config['FILE_DIR']."simple.gif";
+        $myFile = $config['BASE_DIR'].$config['FILE_DIR']."square_logo.jpg";
         $fh = fopen($myFile, 'w');
-        $myFile = $config['BASE_DIR'].$config['FILE_DIR']."simple_1.gif";
+        $myFile = $config['BASE_DIR'].$config['FILE_DIR']."square_logo_1.jpg";
         $fh = fopen($myFile, 'w');
-        $myFile = $config['BASE_DIR'].$config['FILE_DIR']."simple_2.gif";
+        $myFile = $config['BASE_DIR'].$config['FILE_DIR']."square_logo_2.jpg";
         $fh = fopen($myFile, 'w');
 
         $this->assertEquals(1, $this->getConnection()->getRowCount('ip_m_content_management_widget'), "Pre-Condition");
@@ -35,14 +35,14 @@ class IpImageMigrationTest extends \PhpUnit\MigrationTestCase
          * @var $sourceTable \PHPUnit_Extensions_Database_DataSet_QueryTable
          */
         $sourceTable = $this->getConnection()->createQueryTable('ip_m_content_management_widget', 'SELECT * FROM ip_m_content_management_widget');
-        $expectedTable = $this->createXMLDataSet(TEST_FIXTURE_DIR.'update/Library/Migration/To2_7/ipImageResult.xml')->getTable("ip_m_content_management_widget");
+        $expectedTable = $this->createXMLDataSet(TEST_FIXTURE_DIR.'update/Library/Migration/To2_7/ipImageGalleryResult.xml')->getTable("ip_m_content_management_widget");
 
         $this->assertTablesEqual($expectedTable, $sourceTable);
 
 
-        $this->assertEquals(true, file_exists($config['BASE_DIR'].$config['FILE_DIR']."simple.gif"));
-        $this->assertEquals(false, file_exists($config['BASE_DIR'].$config['FILE_DIR']."simple_1.gif"));
-        $this->assertEquals(false, file_exists($config['BASE_DIR'].$config['FILE_DIR']."simple_2.gif"));
+        $this->assertEquals(true, file_exists($config['BASE_DIR'].$config['FILE_DIR']."square_logo.jpg"));
+        $this->assertEquals(false, file_exists($config['BASE_DIR'].$config['FILE_DIR']."square_logo_1.jpg"));
+        $this->assertEquals(false, file_exists($config['BASE_DIR'].$config['FILE_DIR']."square_logo_2.jpg"));
     }
 
 
