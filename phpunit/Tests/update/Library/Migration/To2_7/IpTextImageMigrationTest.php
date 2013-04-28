022e8257e6ac7b83b1cc257d2dbfe61b2e2f575e
Mangirdas <mangirdas@impresspages.org>
2013-04-28 15:49:08 +0300

    IpTextImage widget migration test

---
+++ phpunit/Tests/update/Library/Migration/To2_7/IpTextImageMigrationTest.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @package   ImpressPages
+ * @copyright Copyright (C) 2012 ImpressPages LTD.
+ * @license   GNU/GPL, see ip_license.html
+ */
+
+class IpTextImageMigrationTest extends \PhpUnit\MigrationTestCase
+{
+
+
+    public function getDataSet()
+    {
+        return $this->createXMLDataSet(TEST_FIXTURE_DIR.'update/Library/Migration/To2_7/ipTextImage.xml');
+    }
+
+
+    public function testIpImageMigration()
+    {
+        $config = $this->getInstallationConfig();
+
+        $myFile = $config['BASE_DIR'].$config['FILE_DIR']."lunch.JPG";
+        $fh = fopen($myFile, 'w');
+        $myFile = $config['BASE_DIR'].$config['FILE_DIR']."lunch_1.JPG";
+        $fh = fopen($myFile, 'w');
+        $myFile = $config['BASE_DIR'].$config['FILE_DIR']."lunch.png";
+        $fh = fopen($myFile, 'w');
+
+        $this->assertEquals(1, $this->getConnection()->getRowCount('ip_m_content_management_widget'), "Pre-Condition");
+        $migrationScript = new \IpUpdate\Library\Migration\To2_7\Script();
+        $migrationScript->migrateWidgets($config);
+
+
+        /**
+         * @var $sourceTable \PHPUnit_Extensions_Database_DataSet_QueryTable
+         */
+        $sourceTable = $this->getConnection()->createQueryTable('ip_m_content_management_widget', 'SELECT * FROM ip_m_content_management_widget');
+        $expectedTable = $this->createXMLDataSet(TEST_FIXTURE_DIR.'update/Library/Migration/To2_7/ipTextImageResult.xml')->getTable("ip_m_content_management_widget");
+
+        $this->assertTablesEqual($expectedTable, $sourceTable);
+
+
+        $this->assertEquals(true, file_exists($config['BASE_DIR'].$config['FILE_DIR']."lunch.JPG"));
+        $this->assertEquals(false, file_exists($config['BASE_DIR'].$config['FILE_DIR']."lunch_1.JPG"));
+        $this->assertEquals(false, file_exists($config['BASE_DIR'].$config['FILE_DIR']."lunch.png"));
+    }
+
+
+
+}
