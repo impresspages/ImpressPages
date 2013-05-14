1a37992c03e8f09eab6ea83e0b9a1ba74e8d8715
Mangirdas <mangirdas@impresspages.org>
2013-05-15 00:13:00 +0300

    put wizard files only in administration mode

--- ip_cms/modules/administrator/wizard/system.php
+++ ip_cms/modules/administrator/wizard/system.php
@@ -11,15 +11,17 @@ namespace Modules\administrator\wizard;
 class System{
 
     function init(){
-        global $site;
+        $site = \Ip\ServiceLocator::getSite();
 
-        // loading required Javascript libraries
-        $site->addJavascript(BASE_URL.LIBRARY_DIR.'js/jquery-tools/jquery.tools.ui.tooltip.js',2);
-        $site->addJavascript(BASE_URL.LIBRARY_DIR.'js/jquery-tools/jquery.tools.ui.overlay.js',2);
-        $site->addJavascript(BASE_URL.LIBRARY_DIR.'js/jquery-tools/jquery.tools.toolbox.expose.js',2);
-        $site->addJavascript(BASE_URL.MODULE_DIR.'administrator/wizard/public/jquery.simulate.js',2);
-        // loading module's elements
-        $site->addCSS(BASE_URL.MODULE_DIR.'administrator/wizard/public/wizard.css',2);
-        $site->addJavascript(BASE_URL.MODULE_DIR.'administrator/wizard/public/wizard.js',2);
+        if ($site->managementState()) {
+            // loading required Javascript libraries
+            $site->addJavascript(BASE_URL.LIBRARY_DIR.'js/jquery-tools/jquery.tools.ui.tooltip.js',2);
+            $site->addJavascript(BASE_URL.LIBRARY_DIR.'js/jquery-tools/jquery.tools.ui.overlay.js',2);
+            $site->addJavascript(BASE_URL.LIBRARY_DIR.'js/jquery-tools/jquery.tools.toolbox.expose.js',2);
+            $site->addJavascript(BASE_URL.MODULE_DIR.'administrator/wizard/public/jquery.simulate.js',2);
+            // loading module's elements
+            $site->addCSS(BASE_URL.MODULE_DIR.'administrator/wizard/public/wizard.css',2);
+            $site->addJavascript(BASE_URL.MODULE_DIR.'administrator/wizard/public/wizard.js',2);
+        }
     }
 }
