a03bc3a95370d7e74053f129ebe5a566bdbb1636
Algimantas Stancelis <algimantas@impresspages.org>
2013-07-11 13:26:48 +0300

    Added img_filename parameter

--- ip_cms/modules/market/image_download/controller.php
+++ ip_cms/modules/market/image_download/controller.php
@@ -26,25 +26,32 @@ class Controller extends \Ip\Controller
 
         // download file to tmp folder
         $img_url = $_GET['img_url'];
-        $img_url_path = parse_url($img_url, PHP_URL_PATH);
-        $img_url_filename = basename($img_url_path);
-        $img_tmp_path = BASE_DIR . TMP_FILE_DIR . $img_url_filename;
 
-        // TODO add to library a method to doanload files safely
+        if (!empty($_GET['img_filename'])) {
+            // XTODO validate
+            $img_filename = $_GET['img_filename'];
+        } else {
+            $img_url_path = parse_url($img_url, PHP_URL_PATH);
+            $img_filename = basename($img_url_path);
+        }
+
+        $img_tmp_path = BASE_DIR . TMP_FILE_DIR . $img_filename;
+        // XTODO add to library a method to download files safely
+        // XTODO if there is no extension, detect by headers
         file_put_contents($img_tmp_path, fopen($img_url, 'r'));
-        // TODO check $http_response_header
+        // XTODO check $http_response_header
 
         $destination = BASE_DIR.FILE_REPOSITORY_DIR;
-        $img_new_filename = \Library\Php\File\Functions::genUnoccupiedName($img_tmp_path, $destination);
-        copy($img_tmp_path, $destination . $img_new_filename);
+        $img_real_filename = \Library\Php\File\Functions::genUnoccupiedName($img_tmp_path, $destination);
+        copy($img_tmp_path, $destination . $img_real_filename);
 
-        \Modules\administrator\repository\Model::bindFile(FILE_DIR . $img_new_filename, 'administrator/repository', 0);
+        \Modules\administrator\repository\Model::bindFile(FILE_DIR . $img_real_filename, 'administrator/repository', 0);
 
         unlink($img_tmp_path);
 
         $browser_model = \Modules\administrator\repository\BrowserModel::instance();
 
-        $file = $browser_model->getFile($img_new_filename);
+        $file = $browser_model->getFile($img_real_filename);
 
         $site->setOutput(json_encode($file));
     }
