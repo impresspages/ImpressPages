16131efa8284d10cc3145dd1abb61133d7682224
Mangirdas <mangirdas@impresspages.org>
2013-07-26 15:19:43 +0300

    make market iframe larger

--- ip_cms/modules/standard/design/view/designdashboard.php
+++ ip_cms/modules/standard/design/view/designdashboard.php
@@ -3,7 +3,14 @@
     <?php echo $this->esc($themeVersion); ?>
     <img width="200" src="<?php echo $this->esc($themePreviewImage) ?>" alt="<?php echo addslashes($themeName); ?>" />
 </div>
-<div id="ipModuleDesignContainer" data-marketurl="<?php echo addslashes($marketUrl) ?>">
+<style>
+    #ipModuleDesignContainer iframe{
+        width: 100%;
+        height: 800px;
+        outline: 1px solid black;
+    }
+</style>
+<div style="width: 900px; height: 800px;" id="ipModuleDesignContainer" data-marketurl="<?php echo addslashes($marketUrl) ?>">
 
 </div data->
 <!--<iframe style="width: 900px; height: 500px; clear: both;" src="--><?php //echo addslashes($marketUrl) ?><!--">-->
