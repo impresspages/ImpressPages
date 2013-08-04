a4f33954ec267cee3e6a368f098ab725216cae9c
Mangirdas <mangirdas@impresspages.org>
2013-08-04 15:57:29 +0300

    request class updates

--- ip_cms/includes/Ip/Request.php
+++ ip_cms/includes/Ip/Request.php
@@ -16,10 +16,22 @@ namespace Ip;
 class Request{
 
     protected $_SERVER = array();
+    protected $_POST = array();
+    protected $_GET = array();
+    protected $_REQUEST = array();
+
 
     public function __construct()
     {
         $this->_SERVER = $_SERVER;
+        $this->_POST = $_POST;
+        $this->_GET = $_GET;
+        $this->_REQUEST = $_REQUEST;
+    }
+
+    public function isGet()
+    {
+        return $this->getMethod() == 'GET';
     }
 
     public function isPost()
@@ -31,7 +43,37 @@ class Request{
      * get request method  'GET', 'HEAD', 'POST', 'PUT'.
      * @return string
      */
-    public function getMethod() {
+    public function getMethod()
+    {
         return $this->_SERVER['REQUEST_METHOD'];
     }
+
+
+    public function paramsGet($name = null, $default = null)
+    {
+        return $this->getParam($name, $this->_GET, $default);
+    }
+
+    public function paramsPost($name = null, $default = null)
+    {
+        return $this->getParam($name, $this->_POST, $default);
+    }
+
+    public function paramsRequest($name = null, $default = null)
+    {
+        return $this->getParam($name, $this->_REQUEST, $default);
+    }
+
+
+    protected function getParam($name, $values, $default)
+    {
+        if ($name === null) {
+            return $values;
+        }
+        if (!isset($values[$name])) {
+            return $default;
+        }
+        return $values[$name];
+    }
+
 }
