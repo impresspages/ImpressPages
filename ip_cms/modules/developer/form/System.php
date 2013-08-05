1b694f046bd0b28db5c7658ba793d075caddc6ee
Mangirdas <mangirdas@impresspages.org>
2013-08-05 14:49:45 +0300

    farbtastic color picker initializatin

--- ip_cms/modules/developer/form/System.php
+++ ip_cms/modules/developer/form/System.php
@@ -16,5 +16,7 @@ class System
         $site = \Ip\ServiceLocator::getSite();
         $site->addJavascript(BASE_URL.LIBRARY_DIR.'js/jquery/jquery.js');
         $site->addJavascript(BASE_URL.MODULE_DIR.'developer/form/public/form.js');
+        $site->addJavascript(BASE_URL.LIBRARY_DIR.'js/farbtastic/farbtastic.js');
+        $site->addCSS(BASE_URL.LIBRARY_DIR.'js/farbtastic/farbtastic.css');
     }
 }
