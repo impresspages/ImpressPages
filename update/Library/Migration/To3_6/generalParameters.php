82d35031c52ef6898441400a43d491bb7f6e3439
Mangirdas <mangirdas@impresspages.org>
2013-08-13 11:15:34 +0300

    add delete parameter on update

--- update/Library/Migration/To3_6/generalParameters.php
+++ update/Library/Migration/To3_6/generalParameters.php
@@ -5,3 +5,7 @@ $languageShort = "EN"; //Short description
 $languageLong = "English"; //Long title
 $languageUrl = "en";
 
+$parameterTitle["standard"]["configuration"]["admin_translations"]["delete"] = "Delete";
+$parameterValue["standard"]["configuration"]["admin_translations"]["delete"] = "Delete";
+$parameterAdmin["standard"]["configuration"]["admin_translations"]["delete"] = "0";
+$parameterType["standard"]["configuration"]["admin_translations"]["delete"] = "string";
