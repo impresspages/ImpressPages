bb1926bdd44ceadf4c6f4d9469be2ffaa9100dcd
Audrius Jankauskas <audrius@impresspages.org>
2013-08-13 17:59:46 +0300

    Styles for theme options box

--- ip_cms/modules/standard/design/view/configBox.php
+++ ip_cms/modules/standard/design/view/configBox.php
@@ -1,10 +1,18 @@
-<div class="ipModuleDesignConfig">
-    <div class="ipmFormContainer">
-        <?php echo $form->render(); ?>
-    </div>
-    <div class="ipmFormActions">
-        <a href="#" class="ipsSave">{{Save}}</a>
-        <a href="#" class="ipsDefault">{{Restore Defaults}}</a>
-        <a href="#" class="ipsCancel">{{Cancel}}</a>
+<div class="ip ipModuleDesignConfig">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title"><i class="icon-cogs"></i> {{Theme options}}</h4>
+            </div>
+            <div class="modal-body ipsForm">
+                <?php echo $form->render(); ?>
+            </div>
+            <div class="modal-footer">
+                <a href="#" class="btn btn-success btn-xs ipsSave">{{Save}}</a>
+                <a href="#" class="btn btn-default btn-xs ipsDefault">{{Restore Defaults}}</a>
+                <a href="#" class="btn btn-default btn-xs ipsCancel">{{Cancel}}</a>
+            </div>
+        </div>
     </div>
 </div>
+
