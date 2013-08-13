69c6079b88ae6a7c641e5e9762ec349ab1e8fa79
Mangirdas <mangirdas@impresspages.org>
2013-08-13 11:25:01 +0300

    missing parameters

--- update/Library/Migration/To3_6/generalParameters.php
+++ update/Library/Migration/To3_6/generalParameters.php
@@ -5,3 +5,19 @@ $languageShort = "EN"; //Short description
 $languageLong = "English"; //Long title
 $languageUrl = "en";
 
+//missed in previous update
+
+$parameterTitle["standard"]["configuration"]["admin_translations"]["delete"] = "Delete";
+$parameterValue["standard"]["configuration"]["admin_translations"]["delete"] = "Delete";
+$parameterAdmin["standard"]["configuration"]["admin_translations"]["delete"] = "0";
+$parameterType["standard"]["configuration"]["admin_translations"]["delete"] = "string";
+
+$parameterTitle["administrator"]["repository"]["admin_translations"]["confirm_delete"] = "Confirm delete";
+$parameterValue["administrator"]["repository"]["admin_translations"]["confirm_delete"] = "Are you sure you want to delete selected files?";
+$parameterAdmin["administrator"]["repository"]["admin_translations"]["confirm_delete"] = "1";
+$parameterType ["administrator"]["repository"]["admin_translations"]["confirm_delete"] = "string";
+
+$parameterTitle["administrator"]["repository"]["admin_translations"]["delete_warning"] = "Delete warning";
+$parameterValue["administrator"]["repository"]["admin_translations"]["delete_warning"] = "Some of the selected files cannot be deleted because they are used.";
+$parameterAdmin["administrator"]["repository"]["admin_translations"]["delete_warning"] = "1";
+$parameterType ["administrator"]["repository"]["admin_translations"]["delete_warning"] = "string";
