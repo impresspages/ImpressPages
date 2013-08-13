99f8a38b514254b50802dc049aff0583dde952ff
Mangirdas <mangirdas@impresspages.org>
2013-08-13 15:53:05 +0300

    secure error handler from throwing errors by itself

---
+++ ip_themes/keko/blank.php
@@ -0,0 +1,6 @@
+<?php 
+
+if (!defined('FRONTEND')) exit;  
+
+echo $site->generateContent();
+
---
+++ ip_themes/keko/install/parameters.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * These parameters are used to synchronize layout styles with automated image cropping
+ * */
+
+//language description
+$languageCode = "en"; //RFC 4646 code
+$languageShort = "EN"; //Short description
+$languageLong = "English"; //Long title
+$languageUrl = "en";
+
+
+    // Widget: IpImage
+    $parameterValue["standard"]["content_management"]["widget_image"]["width"] = "645"; // content
+    $parameterValue["standard"]["content_management"]["widget_image"]["height"] = "600"; // content
+    $parameterValue["standard"]["content_management"]["widget_image"]["big_width"] = "800"; // lightbox
+    $parameterValue["standard"]["content_management"]["widget_image"]["big_height"] = "600"; // lightbox
+
+    // Widget: IpImageGallery
+    $parameterValue["standard"]["content_management"]["widget_image_gallery"]["width"] = "198"; // content
+    $parameterValue["standard"]["content_management"]["widget_image_gallery"]["height"] = "150"; // content
+    $parameterValue["standard"]["content_management"]["widget_image_gallery"]["big_width"] = "800"; // lightbox
+    $parameterValue["standard"]["content_management"]["widget_image_gallery"]["big_height"] = "600"; // lightbox
+
+    // Widget: IpLogoGallery
+    $parameterValue["standard"]["content_management"]["widget_logo_gallery"]["width"] = "143";
+    $parameterValue["standard"]["content_management"]["widget_logo_gallery"]["height"] = "143";
+
+    // Widget: IpTextImage
+    $parameterValue["standard"]["content_management"]["widget_text_image"]["width"] = "262"; // content
+    $parameterValue["standard"]["content_management"]["widget_text_image"]["height"] = "600"; // content
+    $parameterValue["standard"]["content_management"]["widget_text_image"]["big_width"] = "800"; // lightbox
+    $parameterValue["standard"]["content_management"]["widget_text_image"]["big_height"] = "600"; // lightbox
