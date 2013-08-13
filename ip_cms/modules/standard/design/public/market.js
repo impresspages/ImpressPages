349962aef837dfe584b35500d8dd2bc316a07ea6
Mangirdas <mangirdas@impresspages.org>
2013-08-13 09:57:51 +0300

    installTheme action

--- ip_cms/modules/standard/design/public/market.js
+++ ip_cms/modules/standard/design/public/market.js
@@ -52,6 +52,13 @@ var ipDesignThemeMarket = new function () {
                     handle: function (action, data) {
                         console.log('handle(' + action + ')');
                         switch (action) {
+                            case 'installTheme':
+                                var fakeOrder = {
+                                    images: [],
+                                    themes: [data]
+                                }
+                                processOrder(fakeOrder);
+                                break;
                             case 'processOrder':
                                 processOrder(data);
                                 break;
