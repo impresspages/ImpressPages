68504f1d71a484a87ac2d582f8f0f8441b8f2fe7
Algimantas Stancelis <algimantas@impresspages.org>
2013-08-16 17:59:59 +0300

    Ugly caching

--- ip_cms/modules/standard/design/Controller.php
+++ ip_cms/modules/standard/design/Controller.php
@@ -24,10 +24,18 @@ class Controller extends \Ip\Controller
         header('HTTP/1.0 200 OK');
         header('Content-Type: text/css');
 
+        if ($this->isLessCached($less)) {
+            echo file_get_contents(BASE_DIR . THEME_DIR . THEME . '/css/compiled.css');
+            exit();
+        }
+
         require_once BASE_DIR . LIBRARY_DIR . 'php/leafo/lessphp/lessc.inc.php';
         $lessc = new \lessc();
         $lessc->setImportDir(BASE_DIR . THEME_DIR . THEME . '/less');
-        echo $lessc->compile($less);
+        $css = $lessc->compile($less);
+        echo $css;
+        flush();
+        file_put_contents(BASE_DIR . THEME_DIR . THEME . '/css/compiled.css', $css);
         exit();
     }
 
@@ -41,5 +49,36 @@ class Controller extends \Ip\Controller
         }
 
         return $less;
+    }
+
+    protected function isLessCached($less)
+    {
+        // TODOX check weather variables were changed
+        // check filemtime()
+
+        //$files = scandir(BASE_DIR . THEME_DIR . THEME . '/less');
+        //$files = glob(BASE_DIR . THEME_DIR . THEME . '/less/*.less');
+
+        $items = glob(BASE_DIR . THEME_DIR . THEME . '/less/*');
+
+        for ($i = 0; $i < count($items); $i++) {
+
+            if (is_dir($items[$i])) {
+                $add = glob($items[$i] . "/*");
+                $items = array_merge($items, $add);
+            }
+        }
+
+        $compileTime = filemtime(BASE_DIR . THEME_DIR . THEME . '/css/compiled.css');
+
+        foreach ($items as $path) {
+            if (preg_match('/[.]less$/', $path)) {
+                if (filemtime($path) > $compileTime) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
