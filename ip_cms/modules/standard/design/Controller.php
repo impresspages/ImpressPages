18e75e66356f0bec528639bdcd47024130c3086b
Algimantas Stancelis <algimantas@impresspages.org>
2013-08-19 15:35:54 +0300

    Refactoring

--- ip_cms/modules/standard/design/Controller.php
+++ ip_cms/modules/standard/design/Controller.php
@@ -15,84 +15,9 @@ class Controller extends \Ip\Controller
         $request = ServiceLocator::getRequest();
         $file = $request->getQuery('file');
 
-        $model = Model::instance();
-
-        $theme = $model->getTheme(THEME);
-
-        $options = $theme->getOptions();
-
-        $configModel = ConfigModel::instance();
-        $config = $configModel->getAllConfigValues(THEME);
-
-        $less = "@import '{$file}.less'; " . $this->generateLessVariables($options, $config);
-
-        header('HTTP/1.0 200 OK');
-        header('Content-Type: text/css');
-
-        if ($this->isLessCached($less)) {
-            echo file_get_contents(BASE_DIR . THEME_DIR . THEME . '/css/compiled.css');
-            exit();
-        }
-
-        require_once BASE_DIR . LIBRARY_DIR . 'php/leafo/lessphp/lessc.inc.php';
-        $lessc = new \lessc();
-        $lessc->setImportDir(BASE_DIR . THEME_DIR . THEME . '/less');
-        $css = $lessc->compile($less);
-        echo $css;
-        flush();
-        file_put_contents(BASE_DIR . THEME_DIR . THEME . '/css/compiled.css', $css);
-        exit();
+        $lessCompiler = LessCompiler::instance();
+        $lessCompiler->serve(THEME, 'theme.less');
     }
 
-    protected function generateLessVariables($options, $config)
-    {
-        $less = '';
 
-        foreach ($options as $option)
-        {
-            $rawValue = array_key_exists($option['name'], $config) ? $config[$option['name']] : $option['default'];
-
-            switch ($option['type']) {
-                case 'color':
-                    $lessValue = $rawValue;
-                    break;
-                case 'range':
-                    $lessValue = $rawValue;
-                    break;
-                default:
-                    $lessValue = json_encode($rawValue);
-            }
-
-            $less .= "\n@{$option['name']}: {$lessValue};";
-        }
-
-        return $less;
-    }
-
-    protected function isLessCached($less)
-    {
-        // TODOX check weather variables were changed
-
-        $items = glob(BASE_DIR . THEME_DIR . THEME . '/less/*');
-
-        for ($i = 0; $i < count($items); $i++) {
-
-            if (is_dir($items[$i])) {
-                $add = glob($items[$i] . "/*");
-                $items = array_merge($items, $add);
-            }
-        }
-
-        $compileTime = filemtime(BASE_DIR . THEME_DIR . THEME . '/css/compiled.css');
-
-        foreach ($items as $path) {
-            if (preg_match('/[.]less$/', $path)) {
-                if (filemtime($path) > $compileTime) {
-                    return false;
-                }
-            }
-        }
-
-        return true;
-    }
 }
