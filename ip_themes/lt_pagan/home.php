f4d409e734d614977205af073ec622c35767489d
Mangirdas <mangirdas@impresspages.org>
2013-08-22 16:28:51 +0300

    add new elements to home page

--- ip_themes/lt_pagan/home.php
+++ ip_themes/lt_pagan/home.php
@@ -8,21 +8,33 @@
 <?php echo $this->subview('_head.php'); ?>
         <div class="main grid_7 right suffix_1">
             <div class="breadcrumb">
-               <?php echo $this->generateBlock('ipBreadcrumb'); ?>
+               <?php echo $this->generateBlock('home', true); ?>
             </div>
-            <?php echo $site->generateBlock('main'); ?>
         </div>
-        <div class="side grid_3 left">
-            <nav><?php /* add class="collapse" to <nav> to hide second level by default */ ?>
-                <?php
-                    //first argument is unique name of this menu within your theme. Choose anything you like. Next argument is zone name. They don't have to be equal.
-                    echo $this->generateMenu('left', 'menu2');
-                    echo $this->generateMenu('left', 'menu3');
-                ?>
-            </nav>
-            <aside>
-                <?php echo $this->generateBlock('side', true); ?>
-            </aside>
+        <div>
+            <div class="grid_3 left">
+                <?php echo $this->generateManagedString('boxTitle1', 'h2', 'Some title'); ?>
+                <?php echo $this->generateManagedImage('boxImage1', THEME_DIR.THEME.'/img/header.jpg', array('width' => '940'), 'banner'); ?>
+                <?php echo $this->generateManagedText('boxText1', 'div', '<p>Lorem ipsum dolor sit amet</p>'); ?>
+            </div>
+            <div class="grid_3 left">
+                <?php echo $this->generateManagedString('boxTitle2', 'h2', 'Some title'); ?>
+                <?php echo $this->generateManagedImage('boxImage2', THEME_DIR.THEME.'/img/header.jpg', array('width' => '940'), 'banner'); ?>
+                <?php echo $this->generateManagedText('boxText2', 'div', '<p>Lorem ipsum dolor sit amet</p>'); ?>
+            </div>
+            <div class="grid_3 left">
+                <?php echo $this->generateManagedString('boxTitle3', 'h2', 'Some title'); ?>
+                <?php echo $this->generateManagedImage('boxImage3', THEME_DIR.THEME.'/img/header.jpg', array('width' => '940'), 'banner'); ?>
+                <?php echo $this->generateManagedText('boxText3', 'div', '<p>Lorem ipsum dolor sit amet</p>'); ?>
+            </div>
+
         </div>
-        <div class="clear"></div>
+        <div>
+            <div class="grid_4 left">
+                <?php echo $site->generateBlock('home1', true); ?>
+            </div>
+            <div class="grid_4 left">
+                <?php echo $site->generateBlock('home2', true); ?>
+            </div>
+        </div>
 <?php echo $this->subview('_foot.php'); ?>
