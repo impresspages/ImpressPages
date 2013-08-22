f718159c88b891f8d426977d708d7bcc80f46c87
Mangirdas <mangirdas@impresspages.org>
2013-08-22 15:47:19 +0300

    add design tab

--- install/sql/data.sql
+++ install/sql/data.sql
@@ -69,7 +69,7 @@ INSERT INTO `ip_cms_module` (`id`, `group_id`, `row_number`, `name`, `admin`, `t
 (441, 336, 14, 'form', 0, 'Form', 0, '1.00', 1),
 (442, 336, 0, 'inline_management', 1, 'Inline Management', 0, '1.00', 1),
 (443, 323, 0, 'wizard', 1, 'Wizard', 0, '1.00', 1),
-(444, 324, 8, 'design', 1, 'Design', 0, '1.00', 1);
+(444, 324, 8, 'design', 1, 'Design', 1, '1.00', 1);
 
 -- Dumping data for table--
 
