f71c3b5760f93d221a7153359a5fdf8b75d02700
Algimantas Stancelis <algimantas@impresspages.org>
2013-08-23 13:27:01 +0300

    Added hidden option type

--- ip_cms/modules/standard/design/LessCompiler.php
+++ ip_cms/modules/standard/design/LessCompiler.php
@@ -61,6 +61,7 @@ class LessCompiler
                 case 'color':
                     $lessValue = $rawValue;
                     break;
+                case 'hidden':
                 case 'range':
                     $lessValue = $rawValue;
                     if (!empty($option['units'])) {
