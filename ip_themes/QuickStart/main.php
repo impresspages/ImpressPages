7ffa11b059853d347b691668dd8e2003135f29b9
Mangirdas <mangirdas@impresspages.org>
2013-08-26 15:33:22 +0300

    QuickStart theme

---
+++ ip_themes/QuickStart/main.php
@@ -0,0 +1,25 @@
+<?php echo $this->doctypeDeclaration(); ?>
+<html<?php echo $this->htmlAttributes(); ?>>
+<head>
+    <?php
+    $site->addCss(BASE_URL.THEME_DIR.THEME.'/site.css');
+    $site->addCss(BASE_URL.THEME_DIR.THEME.'/ip_content.css');
+    $site->addCss(BASE_URL.LIBRARY_DIR.'js/colorbox/themes/2/colorbox.css');
+    echo $site->generateHead();
+    ?>
+</head>
+<body>
+    <div class="topmenu">
+        <?php echo $this->generateMenu('top', 'menu1'); ?> <!--first argument is unique name of this menu within your theme. Choose anything you like. Next argument is zone name.-->
+    </div>
+    <div class="content">
+        <?php echo $this->generateBlock('main'); ?>
+    </div>
+    <?php
+        $site->addJavascript(BASE_URL.LIBRARY_DIR.'js/jquery/jquery.js');
+        $site->addJavascript(BASE_URL.LIBRARY_DIR.'js/colorbox/jquery.colorbox.js');
+        $site->addJavascript(BASE_URL.THEME_DIR.THEME.'/site.js');
+        echo $site->generateJavascript();
+    ?>
+</body>
+</html>
