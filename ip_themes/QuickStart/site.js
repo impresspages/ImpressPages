02e7862c92c06f9f77ab54ec1ff0c424f77fcacf
Audrius Jankauskas <audrius@impresspages.org>
2013-08-27 12:52:23 +0300

    Minimizing and optimizing QuickStart theme

--- ip_themes/QuickStart/site.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    // Enabling lightbox (colorbox) support for images
-    jQuery('.ipWidget-IpImageGallery li a, .ipWidget-IpImage a').colorbox({
-        rel:'ipwImage',
-        maxWidth:'90%',
-        maxHeight:'90%'
-    });
-});
