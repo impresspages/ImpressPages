541e9feab6a68d1947ef6ab8888a194ff359b31e
Audrius Jankauskas <audrius@impresspages.org>
2013-08-28 16:46:56 +0300

    relative paths for LESS

--- ip_themes/LTpagan/_header.php
+++ ip_themes/LTpagan/_header.php
@@ -16,9 +16,9 @@
             fileAsync: true,   // load imports async when in a page under
             // a file protocol
             poll: 1000,         // when in watch mode, time in ms between polls
-            functions: {}      // user functions, keyed by name
+            functions: {},      // user functions, keyed by name
 //            dumpLineNumbers: "mediaQuery", // or "mediaQuery" or "all"
-//            relativeUrls: false,// whether to adjust url's to be relative
+            relativeUrls: true // whether to adjust url's to be relative
             // if false, url's are already relative to the
             // entry less file
             //rootpath: ":/a.com/"// a path to add on to the start of every url
