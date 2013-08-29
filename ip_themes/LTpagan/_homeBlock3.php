86b120e751fa68d24e5fef91dc0bd6d6cd5a106f
Mangirdas <mangirdas@impresspages.org>
2013-08-29 12:55:45 +0300

    0 - 6 block in main page

---
+++ ip_themes/LTpagan/_homeBlock3.php
@@ -0,0 +1,9 @@
+<div class="grid_4 left">
+    <?php echo $site->generateBlock('home1', true); ?>
+</div>
+<div class="grid_4 left">
+    <?php echo $site->generateBlock('home2', true); ?>
+</div>
+<div class="grid_4 left">
+    <?php echo $site->generateBlock('home3', true); ?>
+</div>
--- ip_themes/LTpagan/home.php
+++ ip_themes/LTpagan/home.php
@@ -6,32 +6,13 @@
  */
 ?>
 <?php echo $this->subview('_header.php'); ?>
-        <div>
-            <div class="grid_4 left">
-                <?php echo $this->generateManagedString('boxTitle1', 'h2', 'Some title'); ?>
-                <?php echo $this->generateManagedImage('boxImage1', THEME_DIR.THEME.'/img/image1.png', array('width' => $this->getThemeOption('boxWidth'), 'height' => '175'), 'boxImage'); ?>
-                <?php echo $this->generateManagedText('boxText1', 'div', '<p>Lorem ipsum dolor sit amet</p>'); ?>
-            </div>
-            <div class="grid_4 left">
-                <?php echo $this->generateManagedString('boxTitle2', 'h2', 'Some title'); ?>
-                <?php echo $this->generateManagedImage('boxImage2', THEME_DIR.THEME.'/img/image2.png', array('width' => $this->getThemeOption('boxWidth'), 'height' => '175'), 'boxImage'); ?>
-                <?php echo $this->generateManagedText('boxText2', 'div', '<p>Lorem ipsum dolor sit amet</p>'); ?>
-            </div>
-            <div class="grid_4 left">
-                <?php echo $this->generateManagedString('boxTitle3', 'h2', 'Some title'); ?>
-                <?php echo $this->generateManagedImage('boxImage3', THEME_DIR.THEME.'/img/image3.png', array('width' => $this->getThemeOption('boxWidth'), 'height' => '175'), 'boxImage'); ?>
-                <?php echo $this->generateManagedText('boxText3', 'div', '<p>Lorem ipsum dolor sit amet</p>'); ?>
-            </div>
-            <div class="clear"></div>
-        </div>
-        <div>
-            <div class="grid_6 left">
-                <?php echo $site->generateBlock('home1', true); ?>
-            </div>
-            <div class="grid_6 left">
-                <?php echo $site->generateBlock('home2', true); ?>
-            </div>
-            <div class="clear"></div>
-        </div>
-<div class="clear"></div>
+
+<div>
+    <?php echo $this->subview('_homeBlock'.$this->getThemeOption('homeContentBlocks', 0).'.php')->render() ?>
+    <div class="clear"></div>
+</div>
+<div>
+    <?php echo $this->generateBlock('main'); ?>
+</div>
+
 <?php echo $this->subview('_footer.php'); ?>
