878b8d3429410e8f146d7872b048bcc937812b20
Mangirdas <mangirdas@impresspages.org>
2013-09-04 23:32:29 +0300

    put just one line of block on main page

--- ip_themes/LTpagan/_homeBlock4.php
+++ ip_themes/LTpagan/_homeBlock4.php
@@ -1,13 +1,12 @@
-<div class="grid_6 ">
+<div class="grid_3 ">
     <?php echo $site->generateBlock('home1', true); ?>
 </div>
-<div class="grid_6 ">
+<div class="grid_3 ">
     <?php echo $site->generateBlock('home2', true); ?>
 </div>
-<div class="clear"></div>
-<div class="grid_6 ">
+<div class="grid_3 ">
     <?php echo $site->generateBlock('home3', true); ?>
 </div>
-<div class="grid_6 ">
+<div class="grid_3 ">
     <?php echo $site->generateBlock('home4', true); ?>
 </div>
