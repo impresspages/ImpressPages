51d2522872829933b0ece1496e9e7bc4b0ffbdf8
Audrius Jankauskas <audrius@impresspages.org>
2013-09-12 11:28:59 +0300

    Adding configuration options to Blank theme

--- ip_themes/Blank/setup/options.js
+++ ip_themes/Blank/setup/options.js
@@ -1,1 +1,16 @@
-var ipDesignOptions = {};
+var ipDesignOptions = {
+    ipTextColor: function (value) {
+        ipDesign.reloadLessFiles(['ip_content', 'theme']);
+    },
+    ipLinkColor: function (value) {
+        ipDesign.reloadLessFiles(['ip_content', 'theme']);
+    },
+    bodyBackgroundColor: function (value) {
+        'use strict';
+        $('body').css('background-color', value);
+    },
+    ipBackgroundColor: function (value) {
+        'use strict';
+        $('.theme').css('background-color', value);
+    }
+};
