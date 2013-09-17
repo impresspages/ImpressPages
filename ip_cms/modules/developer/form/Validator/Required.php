e8afb2235c676b1b7121c80ffbbda31368cfa2aa
Algimantas Stancelis <algimantas@impresspages.org>
2013-09-17 10:41:35 +0300

    Fix validator rule

--- ip_cms/modules/developer/form/Validator/Required.php
+++ ip_cms/modules/developer/form/Validator/Required.php
@@ -9,18 +9,18 @@ namespace Modules\developer\form\Validator;
 
 
 class Required extends Validator {
-    
+
     public function validate($values, $valueKey) {
-        if (empty($values[$valueKey])) {
+        if (!array_key_exists($valueKey, $values) || in_array($values[$valueKey], array(null, false, '', array()), true)) {
             $parametersMod = \Ip\ServiceLocator::getParametersMod();
             return $parametersMod->getValue("developer", "form", "error_messages", "required");
         } else {
             return false;
         }
     }
-    
+
     public function jtoolsArgs() {
         return 'required="required"';
     }
-    
+
 }
