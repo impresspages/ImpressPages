a33ea9f77615c978f0828651196a4fc29fab8254
Mangirdas <mangirdas@impresspages.org>
2013-09-20 15:53:06 +0300

    fix file input error

--- ip_cms/modules/developer/form/view/field/File.php
+++ ip_cms/modules/developer/form/view/field/File.php
@@ -1,8 +1,8 @@
 <div class="ipmFileContainer <?php echo $classes ?>" data-inputname='<?php echo addslashes($inputName) ?>'>
-    <div style="display: none;" class="ipmHiddenInput"><!-- div that hides input field. It is needed for jQuery Tools to position error message -->
-        <input type="text" name="<?php echo addslashes($inputName) ?>" />
-    </div>
     <a <?php echo $attributesStr ?> class="ipmFileAddButton"  href="#" ><?php echo $this->escPar('developer/form/translations/upload'); ?></a>
+    <div>
+        <input class="ipmControlBlank" type="text" name="<?php echo addslashes($inputName) ?>" /> <!-- It is needed for jQuery Tools to position error message -->
+    </div>
     <div class="ipmFiles">
 
     </div>
