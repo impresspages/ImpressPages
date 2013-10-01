85355f3e280f2e574b78420d55b0d562860e9990
Algimantas Stancelis <algimantas@impresspages.org>
2013-10-01 14:59:10 +0300

    Refactoring

--- ip_cms/modules/standard/menu_management/manager.php
+++ ip_cms/modules/standard/menu_management/manager.php
@@ -7,37 +7,44 @@
 namespace Modules\standard\menu_management;
 
 
-if (!defined('BACKEND')) exit;
+if (!defined('BACKEND')) {
+    exit;
+}
 
 
+class Manager
+{
 
-class Manager {
-
-    function __construct() {
+    function __construct()
+    {
     }
 
 
-    function manage() {
+    function manage()
+    {
         global $cms;
 
-        require_once (__DIR__.'/template.php');
+        require_once(__DIR__ . '/template.php');
 
-        $data = array (
-      'securityToken' =>  $cms->session->securityToken(),
-      'moduleId' => $cms->curModId,
-      'postURL' => $cms->generateWorkerUrl(),
-      'imageDir' => BASE_URL.MODULE_DIR.'standard/menu_management/img/'
-      );
+        $data = array(
+            'securityToken' => $cms->session->securityToken(),
+            'moduleId' => $cms->curModId,
+            'postURL' => $cms->generateWorkerUrl(),
+            'imageDir' => BASE_URL . MODULE_DIR . 'standard/menu_management/img/'
+        );
 
-      $content = Template::content($data);
+        $site = \Ip\ServiceLocator::getSite();
+        $site->addCss(BASE_URL . MODULE_DIR . 'standard/menu_management/menu_management.css');
+        $site->addCss(BASE_URL . MODULE_DIR . 'standard/menu_management/jquery-ui/jquery-ui.css');
+        $site->addJavascript(BASE_URL . LIBRARY_DIR . 'js/default.js');
+        $site->addJavascript(BASE_URL . LIBRARY_DIR . 'js/jquery/jquery.js');
+        $site->addJavascript(BASE_URL . MODULE_DIR . 'standard/menu_management/jstree/jquery.cookie.js');
+        $site->addJavascript(BASE_URL . MODULE_DIR . 'standard/menu_management/jstree/jquery.hotkeys.js');
+        $site->addJavascript(BASE_URL . MODULE_DIR . 'standard/menu_management/jstree/jquery.jstree.js');
+        $site->addJavascript(BASE_URL . MODULE_DIR . 'standard/menu_management/menu_management.js');
+        $site->addJavascript(BASE_URL . MODULE_DIR . 'standard/menu_management/jquery-ui/jquery-ui.js');
 
-
-
-      $answer = Template::addLayout($content);
-
-
-
-      return $answer;
+        return Template::content($data);
     }
 
 }
