c35dbc426171276943884e187b4140f10bd048f1
Algimantas Stancelis <algimantas@impresspages.org>
2013-10-03 15:25:27 +0300

    Cleanup

--- update/migrations.sql
+++ update/migrations.sql
@@ -1,3 +1,2 @@
 -- 2013-10-03
-ALTER TABLE  `ip3_content_element` ADD  `action` VARCHAR( 255 ) NOT NULL ,
-ADD  `symlink` VARCHAR( 255 ) NOT NULL ;
+ALTER TABLE  `ip3_content_element` ADD  `action` VARCHAR( 255 ) NOT NULL;
