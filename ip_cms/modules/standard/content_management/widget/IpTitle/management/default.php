7aabc3cd8e05af9ee935adadba269983c8dbd088
chris vigelius <me@cv.gd>
2013-10-12 14:50:08 +0200

    Allow setting id in title widget, for relative links ('#section')

--- ip_cms/modules/standard/content_management/widget/IpTitle/management/default.php
+++ ip_cms/modules/standard/content_management/widget/IpTitle/management/default.php
@@ -1,1 +1,4 @@
-<input name="title" class="ipAdminInput" value="<?php echo htmlspecialchars(isset($title) ? $title : '' ); ?>" />
+<label class="ipAdminLabel"><?php echo $this->escPar('standard/content_management/widget_title/title') ?><br/>
+<input name="title" required="required" class="ipAdminInput" value="<?php echo htmlspecialchars(isset($title) ? $title : '' ); ?>" /></label>
+<label class="ipAdminLabel">Id (optional, for relative links)<br/>
+<input name="id" class="ipAdminInput" value="<?php echo htmlspecialchars(isset($id) ? $id : '' ); ?>" /></label>
