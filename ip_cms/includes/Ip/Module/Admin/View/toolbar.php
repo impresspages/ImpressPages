f203e09352fdf220db354c8b3e3be6b9498dc2a5
Mangirdas <mangirdas@impresspages.org>
2013-10-16 14:56:53 +0300

    implement logout

--- ip_cms/includes/Ip/Module/Admin/View/toolbar.php
+++ ip_cms/includes/Ip/Module/Admin/View/toolbar.php
@@ -1,4 +1,4 @@
 <div class="ipAdminToolbar">
     <a href="#" class="ipsAdminMenu">Menu</a>
-    <a href="#" class="right"><?php echo $this->escPar('standard/configuration/system_translations/logout') ?></a>
+    <a href="<?php echo BASE_URL ?>?sa=Admin.logout" class="right"><?php echo $this->escPar('standard/configuration/system_translations/logout') ?></a>
 </div>
