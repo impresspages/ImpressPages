b3b9f89cb9827d20918ace4ceb6350b42c8a0855
Mangirdas <mangirdas@impresspages.org>
2013-10-16 10:43:55 +0300

    logout link

--- ip_cms/includes/Ip/Module/Admin/View/toolbar.php
+++ ip_cms/includes/Ip/Module/Admin/View/toolbar.php
@@ -1,3 +1,4 @@
 <div class="ipAdminToolbar">
     <a href="#" class="ipsAdminMenu">Menu</a>
+    <a href="#" class="right"><?php echo $this->escPar('standard/configuration/system_translations/logout') ?></a>
 </div>
