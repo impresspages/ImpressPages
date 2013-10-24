ea39c03365218f3a7f664e37f5e33735274c2dd6
Algimantas Stancelis <algimantas@impresspages.org>
2013-10-24 14:39:09 +0300

    Documentation

---
+++ phpunit/Tests/InternalRequest/ExperimentTest.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @package   ImpressPages
+ */
+
+namespace Tests\InternalRequest;
+
+
+class ExperimentTest extends \PhpUnit\GeneralTestCase
+{
+    public function setup()
+    {
+//        parent::setup();
+//        $config = include __DIR__ . '/ipConfig-default.php';
+//        \Ip\Config::init($config);
+    }
+
+    public function testRenderHomepage()
+    {
+//        $applicationController = new \Ip\Core\ApplicationController();
+//        $controller->handler($request);
+//
+//        $response = $application->handleRequest($request);
+    }
+}
