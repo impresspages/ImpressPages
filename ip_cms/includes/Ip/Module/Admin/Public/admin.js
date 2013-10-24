551dd0e935baeb9862573649be7e832f817bd910
Mangirdas <mangirdas@impresspages.org>
2013-10-24 14:33:54 +0300

    fix menu javascript events

--- ip_cms/includes/Ip/Module/Admin/Public/admin.js
+++ ip_cms/includes/Ip/Module/Admin/Public/admin.js
@@ -1,29 +1,52 @@
 var ipAdmin = new function () {
     "use strict";
+    var $adminMenu;
+    var $container;
 
     this.init = function () {
         $('body').prepend($(ipAdminToolbar));
-        var $container = $('.ipsAdminToolbarContainer'); // the most top element physically creates a space
-        var $toolbar = $('.ipsAdminToolbar'); // Administration Panel that stays always visible
-        var $adminMenu = $('.ipsAdminMenuBlock').height($(window).height());
-        $container.height($toolbar.outerHeight()); // setting the height to container
+        $container = $('.ipsAdminToolbarContainer'); // the most top element physically creates a space
 
-        $('.ipsAdminMenu').on('hover', function (e) {
-            e.preventDefault();
-            $adminMenu.show('slide', {direction: 'left'}, 1000);
-        });
+        $adminMenu = $('.ipsAdminMenuBlock');
 
-        $adminMenu.on('mouseleave', function (e) {
-            e.preventDefault();
-            $adminMenu.hide('slide', {direction: 'left'}, 1000);
-        });
+        $('.ipsAdminMenu').on('mouseenter', function (e) { showAdminMenu(); });
+
+        $adminMenu.on('mouseleave', function (e) { hideAdminMenu(); });
 
         //prevent session expire
         if (typeof(ipAdminSessionRefresh) !== 'undefined') {
             setInterval(refreshSession, ipAdminSessionRefresh * 1000);
         }
 
+        $(window).on('resize', fixLayout);
+        fixLayout();
     };
+
+
+    var showAdminMenu = function () {
+        var newWidth = 200;
+        $adminMenu.width(0);
+        $adminMenu.show();
+        $adminMenu.animate({
+            width: newWidth + 'px'
+        }, 200);
+    };
+
+    var hideAdminMenu = function () {
+        $('body').removeClass('ipAdminNoScroll');
+        var newWidth = 0;
+        $adminMenu.width('auto');
+        $adminMenu.animate({
+            width: newWidth + 'px'
+        }, 200, function(){$(this).hide();});
+    };
+
+    var fixLayout = function () {
+        var $toolbar = $('.ipsAdminToolbar'); // Administration Panel that stays always visible
+        $adminMenu.height($(window).height());
+        $container.height($toolbar.outerHeight()); // setting the height to container
+    }
+
 
     var refreshSession = function () {
         $.ajax({
