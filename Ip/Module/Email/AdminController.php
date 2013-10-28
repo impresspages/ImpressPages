b757a39b47d0fd64add49dd750afd7140c5f3122
Mangirdas <mangirdas@impresspages.org>
2013-10-28 16:20:50 +0200

    email queue field titles

--- Ip/Module/Email/AdminController.php
+++ Ip/Module/Email/AdminController.php
@@ -22,7 +22,7 @@ class AdminController{
         $element = new \Ip\Lib\StdMod\Element\Text(array(
             'dbField' => 'to'
         ));
-        $element->name = $parametersMod->getValue('administrator', 'email_queue', 'admin_translations', 'to');
+        $element->title = $parametersMod->getValue('administrator', 'email_queue', 'admin_translations', 'to');
         $element->showOnList = true;
         $element->searchable = true;
         $element->sortable = true;
@@ -31,7 +31,7 @@ class AdminController{
         $element = new \Ip\Lib\StdMod\Element\Text(array(
             'dbField' => 'from'
         ));
-        $element->name = $parametersMod->getValue('administrator', 'email_queue', 'admin_translations', 'from');
+        $element->title = $parametersMod->getValue('administrator', 'email_queue', 'admin_translations', 'from');
         $element->showOnList = true;
         $element->searchable = true;
         $element->sortable = true;
@@ -41,39 +41,40 @@ class AdminController{
         $element = new \Ip\Lib\StdMod\Element\Text(array(
             'dbField' => 'subject'
         ));
-        $element->name = $parametersMod->getValue('administrator', 'email_queue', 'admin_translations', 'subject');
+        $element->title = $parametersMod->getValue('administrator', 'email_queue', 'admin_translations', 'subject');
         $element->showOnList = true;
         $element->searchable = true;
         $element->sortable = true;
         $elements[] = $element;
-//
-//        $element = new \Ip\Lib\StdMod\Element\Bool(array(
-//            'dbField' => 'immediate'
-//        ));
-//        $element->name = $parametersMod->getValue('administrator', 'email_queue', 'admin_translations', 'immediate');
-//        $element->showOnList = true;
-//        $element->read_only = true;
-//        $element->searchable = true;
-//        $element->sortable = true;
-//        $elements[] = $element;
+
+
+        $element = new \Ip\Lib\StdMod\Element\Bool(array(
+            'dbField' => 'immediate'
+        ));
+        $element->title = $parametersMod->getValue('administrator', 'email_queue', 'admin_translations', 'immediate');
+        $element->showOnList = true;
+        $element->read_only = true;
+        $element->searchable = true;
+        $element->sortable = true;
+        $elements[] = $element;
 
 
         $element = new \Ip\Lib\StdMod\Element\Text(array(
             'dbField' => 'send'
         ));
-        $element->name = $parametersMod->getValue('administrator', 'email_queue', 'admin_translations', 'send');
+        $element->title = $parametersMod->getValue('administrator', 'email_queue', 'admin_translations', 'send');
         $element->showOnList = true;
         $element->searchable = true;
         $element->sortable = true;
         $elements[] = $element;
 
 
-
+//TODOX refactor email preview and attachment elements
 //
 //        $element = new element_email(array(
 //            'dbField' => 'id'
 //        ));
-//        $element->name = $parametersMod->getValue('administrator', 'email_queue', 'admin_translations', 'email');
+//        $element->title = $parametersMod->getValue('administrator', 'email_queue', 'admin_translations', 'email');
 //        $element->showOnList = true;
 //        $element->read_only = true;
 //        $element->searchable = true;
@@ -82,7 +83,7 @@ class AdminController{
 //        $element = new element_attachment(array(
 //            'dbField' => 'id'
 //        ));
-//        $element->name = $parametersMod->getValue('administrator', 'email_queue', 'admin_translations', 'attachments');
+//        $element->title = $parametersMod->getValue('administrator', 'email_queue', 'admin_translations', 'attachments');
 //        $element->showOnList = true;
 //        $element->read_only = true;
 //        $element->searchable = true;
@@ -91,7 +92,7 @@ class AdminController{
 
         $area0 = new \Ip\Lib\StdMod\Area();
         $area0->dbTable = "m_administrator_email_queue";
-        $area0->name = $parametersMod->getValue('administrator', 'email_queue', 'admin_translations', 'email_queue');
+        $area0->title = $parametersMod->getValue('administrator', 'email_queue', 'admin_translations', 'email_queue');
         $area0->dbPrimaryKey= "id";
         $area0->elements = $elements;
         $area0->searchable = true;
