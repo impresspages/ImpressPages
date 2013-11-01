6c5fa43afae87c7471352c7699f79d838539f7b0
Algimantas Stancelis <algimantas@impresspages.org>
2013-11-01 13:44:03 +0200

    Translation functions

--- phpunit/Tests/Translation/TranslationTest.php
+++ phpunit/Tests/Translation/TranslationTest.php
@@ -16,33 +16,9 @@ class TranslationTest extends \PHPUnit_Framework_TestCase
     public function testDefaultTestEnvironment()
     {
         \PhpUnit\Helper\TestEnvironment::initCode();
+        \Ip\Translator::init();
 
         $this->assertEquals('non existent string', __('non existent string', 'ip'));
         $this->assertEquals('{{keyword}}', _k('keyword', 'ip'));
     }
-}
-
-function __($text, $domain)
-{
-    return $text;
-}
-
-function _x($text, $context, $domain)
-{
-    return $text;
-}
-
-function _n($single, $plural, $number, $domain)
-{
-
-}
-
-function _nx($single, $plural, $number, $context, $domain)
-{
-
-}
-
-function _k($text, $domain)
-{
-    return '{{' . $text .'}}';
 }
