bb8c456940c591fc7510dad8fc4b41e6d620a4cc
Algimantas Stancelis <algimantas@impresspages.org>
2013-11-04 16:12:55 +0200

    Use translation function

--- Ip/Module/Install/migrate.php
+++ Ip/Module/Install/migrate.php
@@ -6,6 +6,9 @@ $translations = include __DIR__ . '/migrate-en.php';
 $translations['IP_FINISH_MESSAGE'] = 'FINISH_MESSAGE';
 $translations['IP_DB_DB'] = 'DATABASE_NAME';
 $translations['IP_STEP_CHECK_LONG'] = 'SYSTEM_CHECK_LONG';
+$translations["'Yes'"] = "'Yes'";
+$translations["'No'"] = "'No'";
+$translations["'Warning'"] = "'Warning'";
 
 // Remove already translated strings:
 foreach ($translations as $id => $translation)
@@ -18,10 +21,11 @@ foreach ($translations as $id => $translation)
 // Add slashes:
 foreach ($translations as $id => $translation)
 {
-    $translations[$id] = addslashes($translation);
+    $translations[$id] = "__('" . addslashes($translation) . "', 'ipInstall')";
 }
 
-$files = glob(__DIR__ . '/languages/*.php');
+$files = glob(__DIR__ . '/view/*.php');
+//$files = array('Model.php');
 
 foreach ($files as $file) {
     $contents = file_get_contents($file);
--- Ip/Module/Install/view/step3.php
+++ Ip/Module/Install/view/step3.php
@@ -1,19 +1,19 @@
-<h1><?php echo IP_STEP_DB_LONG ?></h1>
+<h1><?php echo __('Database installation', 'ipInstall') ?></h1>
 
-<div id="errorAllFields" class="noDisplay"><p class="error"><?php echo IP_DB_ERROR_ALL_FIELDS ?></p></div>
-<div id="errorConnect" class="noDisplay"><p class="error"><?php echo IP_DB_ERROR_CONNECT ?></p></div>
-<div id="errorDb" class="noDisplay"><p class="error"><?php echo IP_DB_ERROR_DB ?></p></div>
-<div id="errorQuery" class="noDisplay"><p class="error"><?php echo IP_DB_ERROR_QUERY ?></p></div>
-<div id="errorLongPrefix" class="noDisplay"><p class="error"><?php echo IP_DB_ERROR_LONG_PREFIX ?></p></div>
-<div id="errorIncorrectPrefix" class="noDisplay"><p class="error"><?php echo IP_DB_ERROR_INCORRECT_PREFIX ?></p></div>
+<div id="errorAllFields" class="noDisplay"><p class="error"><?php echo __('Please fill in all fields', 'ipInstall') ?></p></div>
+<div id="errorConnect" class="noDisplay"><p class="error"><?php echo __('Can\'t connect to database', 'ipInstall') ?></p></div>
+<div id="errorDb" class="noDisplay"><p class="error"><?php echo __('Specified database does not exists', 'ipInstall') ?></p></div>
+<div id="errorQuery" class="noDisplay"><p class="error"><?php echo __('Unknown SQL error', 'ipInstall') ?></p></div>
+<div id="errorLongPrefix" class="noDisplay"><p class="error"><?php echo __('Prefix can\'t be longer than 7 symbols', 'ipInstall') ?></p></div>
+<div id="errorIncorrectPrefix" class="noDisplay"><p class="error"><?php echo __('Prefix can\'t contain any special characters and should sart with letter', 'ipInstall') ?></p></div>
 <form onsubmit="return false">
-    <p><strong><?php echo IP_DB_SERVER ?></strong><input id="db_server" type="text" name="server" value="<?php echo $_SESSION['db_server'] ?>"></p>
-    <p><strong><?php echo IP_DB_USER ?></strong><input id="db_user" type="text" name="db_user" value="<?php echo $_SESSION['db_user'] ?>"></p>
-    <p><strong><?php echo IP_DB_PASS ?></strong><input id="db_pass" type="password" name="db_pass" value="<?php echo $_SESSION['db_pass'] ?>"></p>
-    <p><strong><?php echo IP_DB_DB ?></strong><input id="db_db" type="text" name="db" value="<?php echo $_SESSION['db_db'] ?>"></p>
-    <p><strong><?php echo IP_DB_PREFIX ?></strong><input id="db_prefix" maxlength="7" type="text" name="prefix" value="<?php echo $_SESSION['db_prefix'] ?>"></p>
+    <p><strong><?php echo __('Database Host (eg. localhost or 127.0.0.1)', 'ipInstall') ?></strong><input id="db_server" type="text" name="server" value="<?php echo $_SESSION['db_server'] ?>"></p>
+    <p><strong><?php echo __('User name', 'ipInstall') ?></strong><input id="db_user" type="text" name="db_user" value="<?php echo $_SESSION['db_user'] ?>"></p>
+    <p><strong><?php echo __('User password', 'ipInstall') ?></strong><input id="db_pass" type="password" name="db_pass" value="<?php echo $_SESSION['db_pass'] ?>"></p>
+    <p><strong><?php echo __('DATABASE_NAME', 'ipInstall') ?></strong><input id="db_db" type="text" name="db" value="<?php echo $_SESSION['db_db'] ?>"></p>
+    <p><strong><?php echo __('Table prefix (use underscore to separate prefix).', 'ipInstall') ?></strong><input id="db_prefix" maxlength="7" type="text" name="prefix" value="<?php echo $_SESSION['db_prefix'] ?>"></p>
 
 </form>
-<p><?php echo IP_DB_DATA_WARNING ?></p>
-<a class="button_act" href="#"><?php echo IP_NEXT ?></a>
-<a class="button" href="?step=2"><?php echo IP_BACK ?></a>
+<p><?php echo __('Attention!!! All old tables with the same prefix will be deleted!', 'ipInstall') ?></p>
+<a class="button_act" href="#"><?php echo __('Next', 'ipInstall') ?></a>
+<a class="button" href="?step=2"><?php echo __('Back', 'ipInstall') ?></a>
