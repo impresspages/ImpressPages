1a5053ffa45421fe4114b18dbb55103c3e7eb9d3
Algimantas Stancelis <algimantas@impresspages.org>
2013-11-05 15:17:16 +0200

    Tests

--- install/ip_config-template.php
+++ install/ip_config-template.php
@@ -23,9 +23,9 @@ return array(
     // END DB
 
     // GLOBAL
-    /*OK*/'BASE_DIR' => '/var/www/ip4.x/', //root DIR with trainling slash at the end. If you have moved your site to another place, change this line to correspond your new domain.
+    /*OK*/'BASE_DIR' => '', //root DIR with trainling slash at the end. If you have moved your site to another place, change this line to correspond your new domain.
     /*OK*/'CORE_DIR' => '',
-    /*OK*/'BASE_URL' => 'http://local.ip4.x.org/', //root url with trainling slash at the end. If you have moved your site to another place, change this line to correspond your new domain.
+    /*OK*/'BASE_URL' => 'http://localhost/', //root url with trainling slash at the end. If you have moved your site to another place, change this line to correspond your new domain.
     /*OK*/'FILE_DIR' => 'file/', //uploded files directory
     /*OK*/'TMP_FILE_DIR' => 'file/tmp/', //temporary files directory
     /*OK*/'FILE_REPOSITORY_DIR' => 'file/repository/', //files repository.
--- phpunit/config.php
+++ phpunit/config.php
@@ -9,7 +9,6 @@ define ('TEST_CODEBASE_DIR', dirname(__DIR__) . '/');
 define ('TEST_FIXTURE_DIR', TEST_BASE_DIR . 'Fixture/');
 define ('TEST_TMP_DIR', TEST_BASE_DIR . 'tmp/');
 
-
 define ('TEST_TMP_URL', $isTravis ? 'http://localhost/phpunit/tmp/' : 'http://localhost/ip4.x/phpunit/tmp/');
 define ('TEST_UNWRITABLE_DIR', '/var/tmp/testDir'); //root owned empty dir which can't be writable by Apache process and can't be chmoded. Used by making symbolic links and emulating unwritable dirs in such way.
 
