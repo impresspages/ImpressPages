007f969f1a3bb5564c8479fff5c0c38a95eecada
Mangirdas <mangirdas@impresspages.org>
2013-11-10 11:59:10 +0200

    website title config

---
+++ Ip/Form/Field/Number.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @package ImpressPages
+ *
+ */
+
+namespace Ip\Form\Field;
+
+
+class Number extends Field{
+
+    public function render($doctype) {
+        $attributesStr = '';
+
+        return '<input '.$this->getAttributesStr($doctype).' class="ipmControlInput '.implode(' ',$this->getClasses()).'" name="'.htmlspecialchars($this->getName()).'" '.$this->getValidationAttributesStr($doctype).' type="number" value="'.htmlspecialchars($this->getDefaultValue()).'" />';
+    }
+
+    /**
+     * CSS class that should be applied to surrounding element of this field. Extending classes should specify their value.
+     */
+    public function getTypeClass() {
+        return 'number';
+    }
+
+}
