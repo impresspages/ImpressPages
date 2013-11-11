d68d86dd7eabb3418a82d81c3bb8031a78d9f944
Algimantas <algimantas@impresspages.org>
2013-11-11 11:09:50 +0200

    Merge

--- Ip/Module/User/view/profile.php
+++
@@ -1,5 +0,0 @@
-<?php echo $this->renderWidget('IpTitle', array('title' => $this->par('User.title_profile'))); ?>
-<?php if ($justUpdated) { ?>
-    <?php echo $this->renderWidget('IpText', array('text' => $this->par('User.profile_updated'))); ?>
-<?php } ?>
-<?php echo $this->renderWidget('IpUserProfile'); ?>
