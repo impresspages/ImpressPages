043148ba43de744837622f8910755195f14eaacd
Mangirdas <mangirdas@impresspages.org>
2013-11-11 16:05:30 +0200

    move translation methods to sugar file

--- Ip/Sugar.php
+++ Ip/Sugar.php
@@ -15,3 +15,27 @@ function ipSetOption($option, $value)
 {
     \Ip\ServiceLocator::getOptions()->setOption($option, $value);
 }
+
+
+
+
+
+function __($text, $domain)
+{
+    return \Ip\Translator::translate($text, $domain);
+}
+
+function _n($singular, $plural, $number, $domain)
+{
+    return \Ip\Translator::translatePlural($singular, $plural, $number, $domain);
+}
+
+function _x($text, $context, $domain)
+{
+    return $text;
+}
+
+function _nx($single, $plural, $number, $context, $domain)
+{
+
+}
