b4a6258ad985e0cad42de7716fcc44e59c43f3a5
Mangirdas <mangirdas@impresspages.org>
2013-11-15 17:07:02 +0200

    replace escPar with new translations

--- Ip/Form/view/field/File.php
+++ Ip/Form/view/field/File.php
@@ -1,5 +1,5 @@
 <div class="ipmFileContainer <?php echo $classes ?>" data-inputname='<?php echo addslashes($inputName) ?>'>
-    <a <?php echo $attributesStr ?> class="ipmFileAddButton"  href="#" ><?php echo $this->escPar('Form.upload'); ?></a>
+    <a <?php echo $attributesStr ?> class="ipmFileAddButton"  href="#" ><?php echo $this->esc(__('Upload', 'ipAdmin')) ?></a>
     <div>
         <input class="ipmControlBlank" type="text" name="<?php echo addslashes($inputName) ?>" /> <!-- It is needed for jQuery Tools to position error message -->
     </div>
--- Ip/Module/Breadcrumb/view/breadcrumb.php
+++ Ip/Module/Breadcrumb/view/breadcrumb.php
@@ -1,4 +1,4 @@
-<a href="<?php echo $homeUrl; ?>"><?php echo $this->escPar('Config.home'); ?></a>
+<a href="<?php echo $homeUrl; ?>"><?php echo $this->esc(__('Home', 'ipPublic')); ?></a>
 <?php foreach ($breadcrumbElements as $key => $element) { ?>
     <?php echo $separator; ?>
 <a href="<?php echo $element->getLink(); ?>" title="<?php echo htmlspecialchars($element->getPageTitle()); ?>"><?php echo htmlspecialchars($element->getButtonTitle()); ?></a>
