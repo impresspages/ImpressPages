7b2ecc22f57ece1e6b298eb431b2525723cf5719
Mangirdas <mangirdas@impresspages.org>
2013-11-17 00:21:49 +0200

    remove utf8 encode function from controller

--- Ip/Controller.php
+++ Ip/Controller.php
@@ -44,27 +44,4 @@ class Controller{
 
 
 
-    /**
-    *
-    *  Returns $dat encoded to UTF8
-    * @param mixed $dat array or string
-    */
-    private function utf8Encode($dat)
-    {
-        if (is_string($dat)) {
-            if (mb_check_encoding($dat, 'UTF-8')) {
-                return $dat;
-            } else {
-                return utf8_encode($dat);
-            }
-        }
-        if (is_array($dat)) {
-            $answer = array();
-            foreach($dat as $i=>$d) {
-                $answer[$i] = $this->utf8Encode($d);
-            }
-            return $answer;
-        }
-        return $dat;
-    }
 }
