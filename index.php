0df9e5beed0ab578a8165da82ac315ed73875501
Mangirdas <mangirdas@impresspages.org>
2013-11-16 21:56:17 +0200

    nicer bootstrap

--- index.php
+++ index.php
@@ -19,19 +19,12 @@ if((PHP_MAJOR_VERSION < 5) || (PHP_MAJOR_VERSION == 5 && PHP_MINOR_VERSION < 3))
     echo 'Your PHP version is: '.PHP_MAJOR_VERSION.'.'.PHP_MINOR_VERSION.'. To run ImpressPages CMS you need PHP >= 5.3.*';
     exit;
 }
-
-$config = require (__DIR__.'/ip_config.php');
-
-require_once $config['BASE_DIR'] . $config['CORE_DIR'] . 'Ip/Config.php';
-\Ip\Config::init($config);
-
-require_once \Ip\Config::getCore('CORE_DIR') . 'Ip/autoloader.php';
-
-//TODOX remove
 ini_set('display_errors', 1);
 
+require_once('Ip/Core/Application.php');
+
 try {
-    $application = new \Ip\Core\Application();
+    $application = new \Ip\Core\Application(__DIR__ . '/ip_config.php');
     $application->init();
     $application->run();
 } catch (\Exception $e) {
