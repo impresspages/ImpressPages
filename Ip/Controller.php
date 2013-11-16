fc7fb8965e5ca9671dd499766262e9b39ea0be5f
Mangirdas <mangirdas@impresspages.org>
2013-11-17 00:22:17 +0200

    remove adminView function from controller

--- Ip/Controller.php
+++ Ip/Controller.php
@@ -24,24 +24,5 @@ class Controller{
     
 
 
-    /**
-     * Wrap content into admin layout view. Use when generating administration pages.
-     * @param string $content
-     * @return View
-     */
-    public function createAdminView($content)
-    {
-        if (is_object($content) && get_class($content) == 'Ip\View') {
-            $content = $content->render();
-        }
-
-        $variables = array(
-            'content' => $content
-        );
-        $view = \Ip\View::create(\Ip\Config::coreModuleFile('Config/view/adminLayout.php'), $variables);
-        return $view;
-    }
-
-
 
 }
