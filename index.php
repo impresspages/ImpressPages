7d747ffd0c40794c73b28ea47ee61e3b0e11e443
Mangirdas <mangirdas@impresspages.org>
2013-11-16 21:59:13 +0200

    fine tune index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 
 if((PHP_MAJOR_VERSION < 5) || (PHP_MAJOR_VERSION == 5 && PHP_MINOR_VERSION < 3)) {
-    echo 'Your PHP version is: '.PHP_MAJOR_VERSION.'.'.PHP_MINOR_VERSION.'. To run ImpressPages CMS you need PHP >= 5.3.*';
+    echo 'Your PHP version is: '.PHP_MAJOR_VERSION.'.'.PHP_MINOR_VERSION.'. To run ImpressPages CMS you need PHP >= 5.3.3';
     exit;
 }
 ini_set('display_errors', 1);
