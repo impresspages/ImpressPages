649d28a0f7cefdfdd9eae38d5268b25555480cf2
Algimantas <algimantas@impresspages.org>
2013-11-18 17:30:25 +0200

    DbException class

---
+++ Ip/DbException.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Ip;
+
+/**
+ * Purpose of this exception is to show error on the line db method was called.
+ * @package Ip
+ */
+class DbException extends \PDOException
+{
+    public function __construct($message = "", $code = 0, \PDOException $previous = null)
+    {
+        $this->message = $previous->message;
+        $this->code = $previous->code;
+        $this->file = $previous->file;
+        $this->line = $previous->line;
+        $this->trace = $previous->getTrace();
+        $this->previous = $previous;
+
+        $backtrace = debug_backtrace();
+
+        $dbFile = dirname(__FILE__) . '/Db.php';
+
+        foreach ($backtrace as $info) {
+            if ($info['file'] != $dbFile) {
+                $this->file = $info['file'];
+                $this->line = $info['line'];
+                break;
+            }
+        }
+    }
+}
