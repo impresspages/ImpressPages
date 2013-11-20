fd800b26b06bf3f1fa687e76608fdc62f7605e4f
Algimantas <algimantas@impresspages.org>
2013-11-20 13:20:03 +0200

    Remove UrlChanged event class

--- Ip/Module/System/System.php
+++ Ip/Module/System/System.php
@@ -17,13 +17,13 @@ class System{
             ipAddJavascript(ipConfig()->coreModuleUrl('System/public/system.js'), 0);
         }
 
-        ipDispatcher()->bind(\Ip\Event\UrlChanged::URL_CHANGED, __NAMESPACE__ .'\System::urlChanged');
+        ipDispatcher()->bind('site.urlChanged', __NAMESPACE__ .'\System::urlChanged');
         ipDispatcher()->bind('Cron.execute', array($this, 'executeCron'));
     }
     
-    public static function urlChanged (\Ip\Event\UrlChanged $event)
+    public static function urlChanged($info)
     {
-        \Ip\Internal\DbSystem::replaceUrls($event->getOldUrl(), $event->getNewUrl());
+        \Ip\Internal\DbSystem::replaceUrls($info['oldUrl'], $info['newUrl']);
     }
     
     public function executeCron(\Ip\Event $e)
