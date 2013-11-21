7caf6addcec342be769504e25087d69f77bc04f2
Algimantas <algimantas@impresspages.org>
2013-11-21 10:45:33 +0200

    Default logger

---
+++ Ip/Module/Log/Logger.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Ip;
+
+class Logger extends \Psr\Log\AbstractLogger
+{
+    /**
+     * Logs with an arbitrary level.
+     *
+     * @param mixed $level
+     * @param string $message
+     * @param array $context
+     * @return null
+     */
+    public function log($level, $message, array $context = array())
+    {
+        
+    }
+}
