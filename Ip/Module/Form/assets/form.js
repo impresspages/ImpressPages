2ce249fe9dfd87ce6c133206a68113ebfeb7185b
Mangirdas <mangirdas@impresspages.org>
2013-11-21 15:35:45 +0200

    fix file upload

--- Ip/Module/Form/assets/form.js
+++ Ip/Module/Form/assets/form.js
@@ -14,21 +14,16 @@ $(document).ready(function () {
 var ipModuleForm = new function () {
     "use strict";
     this.init = function () {
-        alert($('.ipsModuleForm .ipsFileContainer').length );
         //if interactive file upload input found, load file upload javascript
         if ($('.ipsModuleForm .ipsFileContainer').length && (typeof(plupload) === "undefined")) {
-            //TODOX new path
-            alert(ip.baseUrl + ip.libraryDir + 'js/plupload/plupload.full.js');
-            $('body').append($('<script type="text/javascript" src="' + ip.baseUrl + ip.libraryDir + 'js/plupload/plupload.full.js"></script>'));
-            $('body').append($('<script type="text/javascript" src="' + ip.baseUrl + ip.moduleDir + 'developer/form/public/file.js"></script>'));
+            $('body').append($('<script type="text/javascript" src="' + Form_pluploadJsFile + '"></script>'));
+            $('body').append($('<script type="text/javascript" src="' + Form_fileFieldJs + '"></script>'));
         }
 
         if ($('.ipsModuleForm .ipmType-color').length && !jQuery().spectrum) {
-            //TODOX new path
-            $('body').append('<script type="text/javascript" src="' + ip.baseUrl + ip.libraryDir + 'js/spectrum/spectrum.min.js"></script>');
-            $('body').append('<script type="text/javascript" src="' + ip.baseUrl + ip.moduleDir + 'developer/form/public/color.js"></script>');
-            $('head').append('<link rel="stylesheet" href="' + ip.baseUrl + ip.libraryDir + 'js/spectrum/spectrum.css" type="text/css" />');
+            $('body').append('<script type="text/javascript" src="' + Form_spectrumJsFile + '"></script>');
+            $('body').append('<script type="text/javascript" src="' + Form_colorFieldJs + '"></script>');
+            $('head').append('<link rel="stylesheet" href="' + Form_spectrumCssFile + '" type="text/css" />');
         }
-
     };
 };
