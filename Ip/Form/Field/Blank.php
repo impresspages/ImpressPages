1361e75e5ea4d2018ab70f76c960540a6184404b
Audrius Jankauskas <audrius@impresspages.org>
2013-11-21 15:26:07 +0200

    cleanup

--- Ip/Form/Field/Blank.php
+++ Ip/Form/Field/Blank.php
@@ -19,12 +19,9 @@ namespace Ip\Form\Field;
 class Blank extends Field{
     
     public function render($doctype) {
-        return '<input '.$this->getAttributesStr($doctype).' class="hidden '.implode(' ',$this->getClasses()).'" name="'.htmlspecialchars($this->getName()).'"  '.$this->getValidationAttributesStr($doctype).' type="text" value="'.htmlspecialchars($this->getDefaultValue()).'" />';
+        return '<input '.$this->getAttributesStr($doctype).' class="'.implode(' ',$this->getClasses()).'" name="'.htmlspecialchars($this->getName()).'"  '.$this->getValidationAttributesStr($doctype).' type="hidden" value="'.htmlspecialchars($this->getDefaultValue()).'" />';
     }
-    
-    /**
-    * CSS class that should be applied to surrounding element of this field. By default empty. Extending classes should specify their value.
-    */
+
     public function getTypeClass() {
         return 'blank';
     }    
--- Ip/Form/Field/Hidden.php
+++ Ip/Form/Field/Hidden.php
@@ -10,24 +10,18 @@ namespace Ip\Form\Field;
 class Hidden extends Field{
     
     public function render($doctype) {
-        $attributesStr = '';
-
-        return '<input '.$this->getAttributesStr($doctype).' class="hidden '.implode(' ',$this->getClasses()).'" name="'.htmlspecialchars($this->getName()).'" '.$this->getValidationAttributesStr($doctype).' type="hidden" value="'.htmlspecialchars($this->getDefaultValue()).'" />';
+        return '<input '.$this->getAttributesStr($doctype).' class="'.implode(' ',$this->getClasses()).'" name="'.htmlspecialchars($this->getName()).'" '.$this->getValidationAttributesStr($doctype).' type="hidden" value="'.htmlspecialchars($this->getDefaultValue()).'" />';
     }
-    
+
     public function getLayout() {
         return self::LAYOUT_BLANK;
     }
-    
+
     public function getType() {
         return self::TYPE_SYSTEM;
-    }    
-    
-    /**
-    * CSS class that should be applied to surrounding element of this field. By default empty. Extending classes should specify their value.
-    */
+    }
+
     public function getTypeClass() {
         return 'hidden';
-    }    
-    
+    }
 }
