d322a0cd57f7cd41d0f0b7c5f2c19bd8a6c5a9e2
Audrius Jankauskas <audrius@impresspages.org>
2013-11-22 17:34:45 +0200

    Form file upload styles

--- Ip/Form/view/field/File.php
+++ Ip/Form/view/field/File.php
@@ -1,17 +1,16 @@
 <div class="file <?php echo $classes ?> ipsFileContainer" data-inputname='<?php echo addslashes($inputName) ?>'>
-    <a <?php echo $attributesStr ?> class="ipmFileAddButton"  href="#" ><?php _e('Upload', 'ipAdmin') ?></a>
-    <div>
-        <input class="ipmControlBlank" type="text" name="<?php echo addslashes($inputName) ?>" /> <!-- It is needed for jQuery Tools to position error message -->
-    </div>
-    <div class="ipmFiles">
-
-    </div>
-    <div class="ipmFileTemplate ipmFile ipgHide">
-        <div class="ipmRemove"></div>
+    <a <?php echo $attributesStr ?> class="btn btn-default ipmFileAddButton"  href="#" ><?php _e('Upload', 'ipAdmin') ?></a>
+    <input type="hidden" name="<?php echo addslashes($inputName) ?>" /> <!-- It is needed for jQuery Tools to position error message -->
+    <div class="ipmFiles"></div>
+    <div class="well ipmFileTemplate ipmFile ipgHide">
+        <button type="button" class="close ipsRemove" aria-hidden="true">&times;</button>
         <div class="ipmUploadError"></div>
         <div class="ipmFileName"></div>
-        <div class="ipmFileProgress">
+        <!-- div class="ipmFileProgress">
             <div class="ipmFileProgressValue"></div>
+        </div -->
+        <div class="progress progress-striped active ipmFileProgress">
+            <div class="progress-bar ipmFileProgressValue"></div>
         </div>
     </div>
 </div>
