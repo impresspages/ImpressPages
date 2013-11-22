79951db2b9109bbeabf22f2bcbf96819a93cd817
Algimantas <algimantas@impresspages.org>
2013-11-22 16:08:40 +0200

    Fixed install tests

--- phpunit/Helper/Installation.php
+++ phpunit/Helper/Installation.php
@@ -95,7 +95,7 @@ class Installation
         $config = array();
         $config['SESSION_NAME'] = 'ses' . rand();
         $config['BASE_DIR'] = $this->getInstallationDir();
-        $config['BASE_URL'] = $this->getInstallationUrl();
+        $config['BASE_URL'] = substr($this->getInstallationUrl(), strlen('http://'));
         $config['timezone'] = $this->getSiteTimeZone();
         $config['db'] = array(
             'hostname' => $this->getDbHost(),
