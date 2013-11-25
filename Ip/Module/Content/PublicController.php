db08dd85a4246a0b00dd30c65d39bf4b20f45671
Mangirdas <mangirdas@impresspages.org>
2013-11-25 13:08:43 +0200

    fix error 404

--- Ip/Module/Content/PublicController.php
+++ Ip/Module/Content/PublicController.php
@@ -14,7 +14,7 @@ class PublicController extends \Ip\Controller
 
         if (
             ipContent()->getLanguageUrl() != ipContent()->getCurrentLanguage()->getUrl() ||
-            !$currentPage
+            $currentPage instanceof \Ip\Page404
         ) {
             return new \Ip\Response\PageNotFound();
         }
