921b345047c95b2f3ab0387fcdcc0cbd8db58bea
Mangirdas Skripka <mangirdas@impresspages.org>
2013-11-27 23:52:21 +0200

    Fix text/image widget

--- Ip/Module/Content/Widget/IpTextImage/view/default.php
+++ Ip/Module/Content/Widget/IpTextImage/view/default.php
@@ -1,5 +1,5 @@
 <?php if (isset($imageSmall) && $imageSmall != ''){ ?>
-<img class="ipwImage" src="<?php echo ipConfig()->baseUrl($imageSmall) ?>" alt="<?php echo isset($title) ? htmlspecialchars($title) : ''; ?>" />
+<img class="ipwImage" src="<?php echo esc($imageSmall) ?>" alt="<?php echo isset($title) ? esc($title) : ''; ?>" />
 <?php } ?>
 <div class="ipwText">
 <?php echo isset($text) ? $text : ''; ?>
