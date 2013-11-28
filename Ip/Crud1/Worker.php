26cf105290d01ddc0b41adcadc69fb641013714c
Mangirdas Skripka <mangirdas@impresspages.org>
2013-11-28 17:47:23 +0200

    new crud sceleton

---
+++ Ip/Crud1/Worker.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @package   ImpressPages
+ */
+
+namespace Ip\Crud1;
+
+
+class Worker {
+
+    protected $config = null;
+
+    /**
+     * @param array $config
+     */
+    public function __construct($config)
+    {
+        $this->config = $config;
+    }
+
+    public function handleAction()
+    {
+
+
+    }
+
+}
