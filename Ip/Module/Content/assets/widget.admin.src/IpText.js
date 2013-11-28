4e575cbfecc2f2a763dec463c70077ff36835fa8
Mangirdas Skripka <mangirdas@impresspages.org>
2013-11-28 19:03:59 +0200

    reformat widget js

--- Ip/Module/Content/assets/widget.admin.src/IpText.js
+++ Ip/Module/Content/assets/widget.admin.src/IpText.js
@@ -5,6 +5,7 @@
  */
 
 function IpWidget_IpText(widgetObject) {
+    "use strict";
     this.widgetObject = widgetObject;
 
     this.manageInit = manageInit;
@@ -13,7 +14,14 @@ function IpWidget_IpText(widgetObject) {
 
     function manageInit() {
         var instanceData = this.widgetObject.data('ipWidget');
-        this.widgetObject.find('textarea').tinymce(ipTinyMceConfigMin);
+        //this.widgetObject.find('textarea').tinymce(ipTinyMceConfigMin);
+
+        //this.widgetObject.find('h1.ipwTitle').attr('contenteditable', 'true');
+        this.widgetObject.find('.ipsContent').attr('contenteditable', 'true');
+
+
+            //$('.ipWidget-IpText .ipsContent').tinymce(ipTinyMceConfigMin);
+
     }
 
     function prepareData() {
