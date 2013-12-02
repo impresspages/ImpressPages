4bb0de160e36b2658f7a204f104f5c7593d6d483
Mangirdas Skripka <mangirdas@impresspages.org>
2013-12-02 14:38:24 +0200

    fix widget admin translations

--- Ip/Module/Content/view/widget_controls.php
+++ Ip/Module/Content/view/widget_controls.php
@@ -1,5 +1,5 @@
 <div class="ipAdminWidgetControls">
-    <a href="#" class="ipaButton ipActionWidgetMove"><span>Move</span></a>
-    <a href="#" class="ipaButton ipActionWidgetDelete"><span><?php _e('Delete', 'ipAdmin') ?></span></a>
+    <a href="#" class="ipaButton ipActionWidgetMove"><span><?php __('Move', 'ipAdmin') ?></span></a>
+    <a href="#" class="ipaButton ipActionWidgetDelete"><span><?php __('Delete', 'ipAdmin') ?></span></a>
 </div>
 <div class="ipAdminWidgetMoveIcon"></div>
