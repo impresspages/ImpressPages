c1a55424bca0301772fff872672f1ad4a67078a8
Mangirdas Skripka <mangirdas@impresspages.org>
2013-12-02 10:09:27 +0200

    contact form translations

--- Ip/Module/Content/Widget/IpForm/snippet/popup.php
+++ Ip/Module/Content/Widget/IpForm/snippet/popup.php
@@ -30,8 +30,8 @@
                     </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
+                    <button type="button" class="btn btn-default" data-dismiss="modal"><?php echo __('Cancel', 'ipAdmin') ?></button>
+                    <button type="button" class="btn btn-primary"><?php echo __('Confirm', 'ipAdmin') ?></button>
                 </div>
             </div><!-- /.modal-content -->
         </div><!-- /.modal-dialog -->
