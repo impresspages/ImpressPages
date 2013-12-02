6e3096750a06837d0581f4b5b1c07123e92554d3
Mangirdas Skripka <mangirdas@impresspages.org>
2013-12-02 14:36:09 +0200

    remove manage icon

--- Ip/Module/Content/view/widget_controls.php
+++ Ip/Module/Content/view/widget_controls.php
@@ -1,5 +1,4 @@
 <div class="ipAdminWidgetControls">
-    <a href="#" class="ipaButton ipActionWidgetManage"><span><?php _e('Edit', 'ipAdmin') ?></span></a>
     <a href="#" class="ipaButton ipActionWidgetMove"><span>Move</span></a>
     <a href="#" class="ipaButton ipActionWidgetDelete"><span><?php _e('Delete', 'ipAdmin') ?></span></a>
 </div>
