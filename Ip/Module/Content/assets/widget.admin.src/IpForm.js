260b9dbea4352e18f04314718c79b2ad083832b3
Mangirdas Skripka <mangirdas@impresspages.org>
2013-12-02 10:20:11 +0200

    contact form admin cleanup

--- Ip/Module/Content/assets/widget.admin.src/IpForm.js
+++ Ip/Module/Content/assets/widget.admin.src/IpForm.js
@@ -12,6 +12,8 @@ function IpWidget_IpForm() {
 
     this.data = null;
     this.modal = null;
+    this.container = null;
+    this.addButton = null;
 
 
     this.init = function($widgetObject, data) {
@@ -24,12 +26,15 @@ function IpWidget_IpForm() {
 
     var openPopup = function ()
     {
-        var $modal = $('#ipWidgetFormPopup');
-        $modal.modal();
-        this.modal = $modal;
+        this.modal = $('#ipWidgetFormPopup');
+        this.addButton = this.modal.find(".ipaFieldAdd");
+        this.container = this.modal.find('.ipWidget_ipForm_container');
+        this.modal.modal({});
+
+        this.modal.on('hidden.bs.modal', $.proxy(cleanup, this));
 
         var instanceData = this.data;
-        var container = $modal.find('.ipWidget_ipForm_container');
+
         var options = new Object;
         if (instanceData['fields']) {
             options.fields = instanceData.fields;
@@ -37,23 +42,27 @@ function IpWidget_IpForm() {
             options.fields = new Array();
         }
 
-        options.fieldTemplate = $modal.find('.ipaFieldTemplate');
-        console.log('types');
-        console.log(instanceData);
+        options.fieldTemplate = this.modal.find('.ipaFieldTemplate');
 
-        options.optionsPopup = $modal.find(".ipaFieldOptionsPopup").ipWidget_ipForm_options({fieldTypes : instanceData.fieldTypes});
-        container.ipWidget_ipForm_container(options);
-        $modal.find(".ipaFieldAdd").click($.proxy(addField, this));
+        options.optionsPopup = this.modal.find(".ipaFieldOptionsPopup").ipWidget_ipForm_options({fieldTypes : instanceData.fieldTypes});
+        this.container.ipWidget_ipForm_container(options);
+
+
+        this.addButton.on('click', $.proxy(addField, this));
         var customTinyMceConfig = ipTinyMceConfigMin;
         customTinyMceConfig.height = 100;
-        $modal.find(".ipWidgetIpFormSuccess").tinymce(customTinyMceConfig);
+        this.modal.find(".ipWidgetIpFormSuccess").tinymce(customTinyMceConfig);
     };
 
 
+    var cleanup = function() {
+        this.container.html('');
+        this.addButton.off();
+    }
     
     var addField = function (e) {
-        var $container = this.modal.find('.ipWidget_ipForm_container');
-        $container.ipWidget_ipForm_container('addField');
+
+        this.container.ipWidget_ipForm_container('addField');
     };
     
 
