572a5df4e9c2b0a063deacf320935fdebd5ff39d
Mangirdas Skripka <mangirdas@impresspages.org>
2013-12-02 17:49:52 +0200

    paste as plain text in title widget

--- Ip/Module/Content/assets/widget.admin.src/IpText.js
+++ Ip/Module/Content/assets/widget.admin.src/IpText.js
@@ -8,12 +8,12 @@ function IpWidget_IpText() {
     "use strict";
 
     this.init = function($widgetObject) {
-        var customTinyMceConfig = ipTinyMceConfigMin;
+        var customTinyMceConfig = ipTinyMceConfigMin();
         customTinyMceConfig.setup = function(ed, l) {ed.on('change', function(e) {
             $widgetObject.save({text: $widgetObject.find('.ipsContent').html()});
         })};
 
-        $widgetObject.find('.ipsContent').tinymce(ipTinyMceConfigMin);
+        $widgetObject.find('.ipsContent').tinymce(customTinyMceConfig);
     };
 
 
