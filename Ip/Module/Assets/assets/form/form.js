29672ca89515cfa42d956bd801ddd2399112b394
marijusimpresspages <marijus@impresspages.org>
2013-12-03 11:58:24 +0200

    Fixed file upload.

--- Ip/Module/Assets/assets/form/form.js
+++ Ip/Module/Assets/assets/form/form.js
@@ -17,8 +17,10 @@ var ipModuleForm = new function () {
         //if interactive file upload input found, load file upload javascript
         if ($('.ipsModuleForm .ipsFileContainer').length && (typeof(plupload) === "undefined")) {
             $('body').append($('<script type="text/javascript" src="' + Form_pluploadJsFile + '"></script>'));
-            $('body').append($('<script type="text/javascript" src="' + Form_fileFieldJs + '"></script>'));
         }
+
+        //TODOX check if not loaded
+        $('body').append($('<script type="text/javascript" src="' + Form_fileFieldJs + '"></script>'));
 
         if ($('.ipsModuleForm .ipmType-color').length && !jQuery().spectrum) {
             $('body').append('<script type="text/javascript" src="' + Form_spectrumJsFile + '"></script>');
