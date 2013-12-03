6b9a7485d4f59ead60cd395c728ce7f1da929078
marijusimpresspages <marijus@impresspages.org>
2013-12-03 11:43:20 +0200

    Added import functionality.

--- Plugin/ImportExport/AdminController.php
+++ Plugin/ImportExport/AdminController.php
@@ -2,103 +2,178 @@
 namespace Plugin\importExport;
 
 
+use Ip\Form\Exception;
+
 class AdminController extends \Ip\Controller
 {
+
+    private $zonesForImporting = Array(),
+            $importLog = Array();
+
     public function index()
     {
 
+        $form = new \Ip\Form();
+
+        if (isset($_REQUEST['startImport'])) {
+
+            switch ($_REQUEST['startImport']) {
+                case 'import':
+                    ipAddPluginAsset('ImportExport', 'importExport.js');
+                    $this->startImport();
+                    return new \Ip\Response\Json($this->importLog);
+                default:
+                    return $this->showWaitMessage();
+            }
+        }
+
+        $field = new \Ip\Form\Field\Submit(
+            array(
+                'name' => 'submit', //html "name" attribute
+                'label' => 'submit', //field label that will be displayed next to input field
+                'defaultValue' => 'Import site widget content from file'
+            ));
+        $form->addField($field);
+
+
+        $field = new \Ip\Form\Field\File(
+            array(
+                'name' => 'siteFile', //html "name" attribute
+                'label' => 'ZIP file:', //field label that will be displayed next to input field
+            ));
+        $form->addField($field);
+
+        $field = new \Ip\Form\Field\Hidden(
+            array(
+                'name' => 'startImport',
+                'defaultValue' => 'import'
+            )
+        );
+
+        $form->addField($field);
+        $formHtml = $form->render();
+
+        return $formHtml;
+
+    }
+
+    private function showWaitMessage()
+    {
+        $data = Array();
+        $renderedHtml = \Ip\View::create('View/view_wait.php', $data)->render();
+        return $renderedHtml;
+    }
+
+    private function startImport()
+    {
         $this->extractZip();
         $this->importZones();
 
 
+        $zones = ipContent()->getZones();
 
-        $zoneData['id'] = 111;
-        $zoneData['name'] = 'TestName';
-        $zoneData['template'] = 'testTemplate';
-        $zoneData['title'] = 'Title';
-        $zoneData['url'] = 'URL';
-        $zoneData['keywords'] = '';
-        $zoneData['description'] = 'Test description';
-
-        ipAddPluginAsset('ImportExport', 'importExport.js');
+        $parentId = 0;
+        $recursive = true;
 
 
         try {
 
-            $zones = \Ip\Module\Pages\Db::getZones();
+            foreach ($this->zonesForImporting as $zone) {
 
-            foreach ($zones as $zone){
+                $zoneName = $zone['nameForImporting'];
 
+                $this->addLogRecord('ZONE NAME:' . $zoneName);
 
-                $zoneName = $zone['name'];
+                $zoneObj = ipContent()->getZone($zoneName);
 
-                $zone = ipContent()->getZone($zoneName);
+                $zoneId = $zoneObj->getId();
 
-
+                $recursive = true;
 
                 $languages = \Ip\Module\Pages\Db::getLanguages();
 
+
                 foreach ($languages as $key => $language) {
+
                     $language_id = $language['id'];
-                    $parentPageId = \Ip\Module\Pages\Db::rootContentElement($zone->getId(), $language_id);
 
-
-                    if ($parentPageId === false) {
-                        trigger_error("Can't find root zone element.");
-
-                        return false;
-                    }
-
-print_r($language);
-
-
-
-                    $parentPage = $zone->getPage($parentPageId);
-
-                    $pageId = \Ip\Module\Content\Service::addPage($zoneName, $parentPageId);
-                    $revisionId = \Ip\Revision::createRevision($zoneName, $pageId, true);
-
-                    $widgetName = 'IpText';
-                    $content['text'] = '<p>Demushke</p>';
-
-                    $instanceId = \Ip\Module\Content\Service::addWidget(
-                        $widgetName,
-                        $zoneName,
-                        $pageId,
-                        'main',
-                        $revisionId,
-                        null
+                    $directory = ipConfig()->fileDirFile(
+                        'ImportExport/archive/' . $language['url'] . '_' . $language_id . '/' . $zone['nameInFile']
                     );
-                    \Ip\Module\Content\Service::addWidgetContent($instanceId, $content, $layout = 'default');
 
-                    $this->importWidgets($language['d_short']);
 
+
+                    if (file_exists($directory) || is_dir($directory)) {
+
+                        $this->addLogRecord("<br>Processing:" . $directory);
+
+                        $parentPageId = \Ip\Module\Pages\Db::rootContentElement($zoneId, $language_id);
+
+                        $this->addZonePages($directory, $parentPageId, $recursive, $zoneName, $language);
+
+
+                    } else {
+                        $this->addLogRecord("Skipping:" . $directory);
+                    }
                 }
             }
         } catch (\Exception $e) {
-            echo $e;
+            $this->addLogRecord("Skipping:" . $e);
         }
 
-        return 'test';
+        $this->addLogRecord('Finished importing');
+        return true;
     }
 
 
     private function importZones()
     {
 
-        $string = file_get_contents( ipConfig()->fileDirFile('ImportExport/archive/zones.json'));
-        $json_a = json_decode($string, true);
-        foreach ($json_a as $zone) {
-            $zoneName = $zone['name'];
+        $this->zonesForImporting = Array();
+
+        $string = file_get_contents(ipConfig()->fileDirFile('ImportExport/archive/zones.json'));
+        $zoneList = json_decode($string, true);
+
+        foreach ($zoneList as $zone) {
+
+            $curZoneName = $zone['name'];
+            $prefix = 'imported_';
+            $suffix = '';
+            while (ipContent()->getZone($prefix . $curZoneName . $suffix)) {
+                $suffix = $suffix + 1;
+            }
+
+            $zoneName = $prefix . $zone['name'] . $suffix;
             $zoneTitle = $zone['title'];
             $zoneDescription = $zone['description'];
             $zoneUrl = $zone['url'];
             $associatedModule = 'Content';
             $defaultLayout = 'main.php';
 
-            \Ip\Module\Pages\Service::addZone($zoneTitle, $zoneName, $associatedModule, $defaultLayout, null, $zoneDescription, $zoneUrl );
+            $this->zonesForImporting[] = Array(
+                'nameInFile' => $zone['name'],
+                'nameForImporting' => $zoneName,
+                'title' => $zoneTitle,
+                'description' => $zoneDescription,
+                'url' => $zoneUrl,
+                'associatedModule' => $associatedModule,
+                'layout' => $defaultLayout
+            );
 
-            $zone = ipContent()->getZone($zoneName);
+
+            try {
+                \Ip\Module\Pages\Service::addZone(
+                    $zoneTitle,
+                    $zoneName,
+                    $associatedModule,
+                    $defaultLayout,
+                    null,
+                    $zoneDescription,
+                    $zoneUrl
+                );
+            } catch (\Exception $e) {
+                throw new \Exception($e);
+            }
 
         }
         return true;
@@ -118,22 +193,181 @@ print_r($language);
             }
 
         } catch (\Exception $e) {
-            echo $e;
+            $this->addLogRecord($e);
         }
     }
 
-    private function importWidgets($languageDir){
-        $path = realpath(ipConfig()->fileDirFile('ImportExport'));
+    private function importWidgets($fileName, $pageId, $zoneName, $language)
+    {
 
-        $Regex = new RegexIterator($Iterator, '/^.+\.json$/i', RecursiveRegexIterator::GET_MATCH);
+        $language_id = $language['id'];
+        $languageDir = $language['url'];
 
-        $objects = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($path), \RecursiveIteratorIterator::SELF_FIRST);
-        foreach($objects as $name => $object){
-            $string = file_get_contents( $name);
-            $json_a = json_decode($string, true);
-            print $name."<Br>";
+
+        $zone = ipContent()->getZone($zoneName);
+
+        $parentPageId = \Ip\Module\Pages\Db::rootContentElement($zone->getId(), $language_id);
+
+
+        if ($parentPageId === false) {
+            trigger_error("Can't find root zone element.");
+
+            return false;
+        }
+
+        $parentPage = $zone->getPage($parentPageId);
+
+
+        //TODO get page data from JSON
+        $buttonTitle = basename($fileName, ".json");
+        $url = $buttonTitle;
+
+        $revisionId = \Ip\Revision::createRevision($zoneName, $pageId, true);
+
+        $string = file_get_contents($fileName);
+
+        $position = 0;
+
+        $pageData = json_decode($string, true);
+
+        if (isset($pageData['widgets'])) {
+
+            $widgetData = $pageData['widgets'];
+
+            foreach ($widgetData as $widgetKey => $widgetValue) {
+
+                $blockId = 'main';
+
+                if (isset($widgetValue['type'])) {
+                    $widgetName = $widgetValue['type'];
+
+                    //TODO Testing
+                    $processWidget = false;
+
+                    $layout = 'default';
+
+                    switch ($widgetName) {
+                        case 'IpSeparator':
+                            $content = null;
+                            $processWidget = true;
+                            break;
+                        case 'IpTable':
+                            $content['text'] = $widgetValue['text'];
+                            $processWidget = true;
+                            break;
+                        case 'IpText':
+                            $content['text'] = $widgetValue['text'];
+                            $processWidget = true;
+                            break;
+                        case 'IpTextImage': // Import IpTextImage as IpText
+                            $widgetName = 'IpText';
+                            $content['text'] = $widgetValue['text'];
+                            $processWidget = true;
+                            break;
+                        case 'IpTitle':
+                            $content['title'] = $widgetValue['title'];
+                            $processWidget = true;
+                            break;
+                        case 'IpHtml':
+                            $content['html'] = $widgetValue['html'];
+                            if (!isset($widgetValue['layout'])) {
+                                $layout = 'escape'; // default layout for code examples
+                            }
+
+                            $processWidget = true;
+                            break;
+                        default:
+                            $content = null;
+                            break;
+                    }
+
+                    if ($processWidget) {
+                        $position++;
+                        $instanceId = \Ip\Module\Content\Service::addWidget(
+                            $widgetName,
+                            $zoneName,
+                            $pageId,
+                            $blockId,
+                            $revisionId,
+                            $position
+                        );
+
+                        \Ip\Module\Content\Service::addWidgetContent($instanceId, $content, $layout);
+
+                    } else {
+                        $this->addLogRecord('ERROR:' . $widgetName . " not supported");
+                    }
+                }
+
+            }
+
+
         }
     }
+
+
+    private function addZonePages($directory, $parentId, $recursive, $zoneName, $language)
+    {
+        $array_items = array();
+        if ($handle = opendir($directory)) {
+            while (false !== ($file = readdir($handle))) {
+                if ($file != "." && $file != "..") {
+                    if (is_dir($directory . "/" . $file)) {
+
+                        if ($recursive) {
+
+                            $string = file_get_contents($directory . "/" . $file . ".json");
+                            $pageData = json_decode($string, true);
+
+                            $pageSettings = $pageData['settings'];
+                            $buttonTitle = $pageSettings['button_title'];
+                            $pageTitle = $pageSettings['page_title'];
+                            $url = $pageSettings['url'];
+
+                            $pageId = \Ip\Module\Content\Service::addPage(
+                                $zoneName,
+                                $parentId,
+                                $buttonTitle,
+                                $pageTitle,
+                                $url
+                            );
+                            $this->addZonePages($directory . "/" . $file, $pageId, $recursive, $zoneName, $language);
+                        }
+
+                    } else {
+                        $fileFullPath = $directory . "/" . $file;
+                        if (!is_dir(preg_replace("/\\.[^.\\s]{3,4}$/", "", $fileFullPath))) {
+                            $string = file_get_contents($fileFullPath);
+
+                            $pageData = json_decode($string, true);
+                            $pageSettings = $pageData['settings'];
+                            $buttonTitle = $pageSettings['button_title'];
+                            $pageTitle = $pageSettings['page_title'];
+                            $url = $pageSettings['url'];
+
+
+                            $pageId = \Ip\Module\Content\Service::addPage(
+                                $zoneName,
+                                $parentId,
+                                $buttonTitle,
+                                $pageTitle,
+                                $url
+                            );
+                            $this->importWidgets($fileFullPath, $pageId, $zoneName, $language);
+                        }
+                    }
+                }
+            }
+            closedir($handle);
+        }
+        return $array_items;
+    }
+
+    private function addLogRecord($msg)
+    {
+        $this->importLog[] = $msg;
+    }
+
 
 
 }
