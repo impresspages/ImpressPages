b2ddfaaba98265c5a6ea19b19748f5ee4921f420
Mangirdas Skripka <mangirdas@impresspages.org>
2013-12-04 21:24:36 +0200

    move bootstrap.js to Ip module

---
+++ Ip/Module/Ip/assets/functions.js
@@ -0,0 +1,15 @@
+
+function ipFileUrl(path)
+{
+    return ip.baseUrl + path;
+}
+
+function ipThemeUrl(path)
+{
+    return ipFileUrl('Theme/' + ip.theme + '/' + path);
+}
+
+function ipHomeUrl()
+{
+    return ip.homeUrl;
+}
