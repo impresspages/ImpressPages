c0724e710640023c003910278431a25baec1f3ca
Algimantas <algimantas@impresspages.org>
2013-12-05 17:17:29 +0200

    Extract translation strings

--- phpunit/tools/translations.php
+++ phpunit/tools/translations.php
@@ -1,10 +1,27 @@
 <?php
 
+/**
+ * requires PHP 5.4, gettext, nodejs and nvm install po2json
+ *
+ */
+
 $rootDir = dirname(dirname(__DIR__));
 
-chdir($rootDir . '/Ip');
+$dirs = array(
+    'Ip',
+    'Plugin',
+    'Theme'
+);
 
-`find . -iname "*.php" > {$rootDir}/phpunit/tools/tmp_gettext_files.txt`;
+chdir($rootDir);
+
+if (file_exists("$rootDir/phpunit/tools/tmp_gettext_files.txt")) {
+    unlink("$rootDir/phpunit/tools/tmp_gettext_files.txt");
+}
+
+foreach ($dirs as $dir) {
+    `find ./$dir -iname "*.php" >> {$rootDir}/phpunit/tools/tmp_gettext_files.txt`;
+}
 
 `xgettext -f {$rootDir}/phpunit/tools/tmp_gettext_files.txt -L PHP --from-code=utf-8 --keyword=__:1,2c --keyword=_e:1,2c -o {$rootDir}/phpunit/tools/all.po --omit-header`;
 `rm {$rootDir}/phpunit/tools/tmp_gettext_files.txt`;
@@ -15,7 +32,6 @@ chdir($rootDir . '/phpunit/tools');
 `rm all.po`;
 
 $all = json_decode(file_get_contents(__DIR__ . '/all.json'), true);
-`rm all.json`;
 
 $t = array();
 
@@ -28,17 +44,16 @@ foreach ($all as $key => $values) {
 
     $parts = explode($delimiter, $key, 2);
     if (count($parts) != 2) {
-        //* TODOX remove
-        var_export($key);
-        echo __FILE__ . ':' . (__LINE__ - 2);
+        echo "error encountered on key: $key";
         exit();
-        //*/
     }
 
     list($domain, $id) = $parts;
 
     $domains[$domain][$id] = $id;
 }
+
+`rm all.json`;
 
 foreach ($domains as $domain => $messageList) {
     file_put_contents(__DIR__ . '/' . $domain . '-en.json', json_encode($messageList, JSON_PRETTY_PRINT)); // JSON_PRETTY_PRINT is only for PHP 5.4 and above
