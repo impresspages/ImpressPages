98dc5c868063b83b8a932e34014a74214b6e3670
Mangirdas Skripka <mangirdas@impresspages.org>
2013-12-05 23:12:38 +0200

    make ipSlot echo output by default

--- Theme/Blank/_footer.php
+++ Theme/Blank/_footer.php
@@ -6,7 +6,7 @@
 ?>
 <footer class="clearfix">
     <div class="col_12">
-        <?php echo ipSlot('Ip.string', array('id' => 'themeName', 'tag' => 'p', 'default' => __('Theme "Blank"', 'theme-Blank', false), 'class' => 'left')) ?>
+        <?php ipSlot('Ip.string', array('id' => 'themeName', 'tag' => 'p', 'default' => __('Theme "Blank"', 'theme-Blank', false), 'class' => 'left')) ?>
         <div>
             <?php echo sprintf(__('Drag & drop with %s', 'theme-Blank'), '<a href="http://www.impresspages.org">' . __('ImpressPages CMS', 'theme-Blank') . '</a>'); ?>
         </div>
