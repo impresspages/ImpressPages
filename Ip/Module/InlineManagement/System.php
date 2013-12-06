2ddfabe756d755f520ed42693263a248623adb15
Mangirdas Skripka <mangirdas@impresspages.org>
2013-12-06 15:18:28 +0200

    compress inline management js

--- Ip/Module/InlineManagement/System.php
+++ Ip/Module/InlineManagement/System.php
@@ -16,18 +16,23 @@ class System
             ipAddCss(ipFileUrl('Ip/Module/InlineManagement/assets/inline_management.css'));
 
 
-            ipAddJavascript(ipFileUrl('Ip/Module/InlineManagement/assets/inlineManagementControls.js'));
-            ipAddJavascript(ipFileUrl('Ip/Module/InlineManagement/assets/inlineManagementLogo.js'));
-            ipAddJavascript(ipFileUrl('Ip/Module/InlineManagement/assets/inlineManagementString.js'));
-            ipAddJavascript(ipFileUrl('Ip/Module/InlineManagement/assets/inlineManagementText.js'));
-            ipAddJavascript(ipFileUrl('Ip/Module/InlineManagement/assets/inlineManagementImage.js'));
+            if (ipConfig()->getRaw('DEBUG_MODE')) {
+                ipAddJavascript(ipFileUrl('Ip/Module/InlineManagement/assets/src/inlineManagement.js'));
+                ipAddJavascript(ipFileUrl('Ip/Module/InlineManagement/assets/src/inlineManagementControls.js'));
+                ipAddJavascript(ipFileUrl('Ip/Module/InlineManagement/assets/src/inlineManagementImage.js'));
+                ipAddJavascript(ipFileUrl('Ip/Module/InlineManagement/assets/src/inlineManagementLogo.js'));
+                ipAddJavascript(ipFileUrl('Ip/Module/InlineManagement/assets/src/inlineManagementString.js'));
+                ipAddJavascript(ipFileUrl('Ip/Module/InlineManagement/assets/src/inlineManagementText.js'));
+                ipAddJavascript(ipFileUrl('Ip/Module/InlineManagement/assets/src/jquery.fontselector.js'));
+            } else {
+                ipAddJavascript(ipFileUrl('Ip/Module/InlineManagement/assets/inlineManagement.min.js'));
+            }
 
             $response = \Ip\ServiceLocator::response();
             if (method_exists($response, 'addJavascriptContent')) {
                 $response->addJavascriptContent('controls', \Ip\View::create('view/management/controls.php')->render());
             }
 
-            ipAddJavascript(ipFileUrl('Ip/Module/InlineManagement/assets/inlineManagement.js'));
 
             ipAddJavascript(ipFileUrl('Ip/Module/Ip/assets/js/plupload/plupload.full.js'));
             ipAddJavascript(ipFileUrl('Ip/Module/Ip/assets/js/plupload/plupload.browserplus.js'));
@@ -35,8 +40,6 @@ class System
 
             ipAddJavascript(ipFileUrl('Ip/Module/Upload/assets/jquery.ip.uploadImage.js'));
             ipAddJavascript(ipFileUrl('Ip/Module/Upload/assets/jquery.ip.uploadFile.js'));
-
-            ipAddJavascript(ipFileUrl('Ip/Module/InlineManagement/assets/jquery.fontselector.js'));
 
             ipAddCss(ipFileUrl('Ip/Module/Ip/assets/js/jquery-colorpicker/colorpicker.css'));
             ipAddJavascript(ipFileUrl('Ip/Module/Ip/assets/js/jquery-colorpicker/colorpicker.js'));
