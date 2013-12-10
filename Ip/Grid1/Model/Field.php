a0dfabbfef1075a2de8f49d2eced0cb7346c7c9d
Mangirdas Skripka <mangirdas@impresspages.org>
2013-12-10 17:58:08 +0200

    refactor Log and Email module to new Grid1

---
+++ Ip/Grid1/Model/Field.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package   ImpressPages
+ */
+
+namespace Ip\Grid1\Model;
+
+
+abstract class Field
+{
+    public abstract function preview($data);
+
+    public abstract function createField();
+    public abstract function createQuery($postData);
+
+    public abstract function updateField();
+    public abstract function updateQuery($postData);
+
+    public abstract function searchField();
+    public abstract function searchQuery($postData);
+}
---
+++ Ip/Grid1/Model/Field/Text.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @package   ImpressPages
+ */
+
+namespace Ip\Grid1\Model\Field;
+
+
+class Text extends \Ip\Grid1\Model\Field
+{
+    protected $field = '';
+    public function __construct($config)
+    {
+        if (empty($config['field'])) {
+            throw new \Ip\CoreException('\'field\' option required for text field');
+        }
+        $this->field = $config['field'];
+    }
+
+    public function preview($recordData)
+    {
+        return esc($recordData[$this->field]);
+    }
+
+    public function createField()
+    {
+    }
+    public function createQuery($postData)
+    {
+    }
+
+    public function updateField()
+    {
+    }
+    public function updateQuery($postData)
+    {
+    }
+
+
+    public function searchField()
+    {
+    }
+    public function searchQuery($postData)
+    {
+    }
+}
