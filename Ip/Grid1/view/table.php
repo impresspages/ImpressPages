f672157dccb1ca7e327326a9eb24d07b089ade0b
Mangirdas Skripka <mangirdas@impresspages.org>
2013-12-10 15:03:23 +0200

    Grid1 print field header

--- Ip/Grid1/view/table.php
+++ Ip/Grid1/view/table.php
@@ -1,11 +1,24 @@
-<table>
-    <?php foreach($data as $row){ ?>
-        <tr>
-            <?php foreach($row as $field){ ?>
-            <td>
-                <?php echo esc($field) ?>
-            </td>
+<div class="ip">
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <?php if (!empty($data[0])) foreach($data[0] as $key => $field){ ?>
+                    <th>
+                        <?php echo esc($key) ?>
+                    </th>
+                <?php } ?>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($data as $row){ ?>
+                <tr>
+                    <?php foreach($row as $field){ ?>
+                    <td>
+                        <?php echo esc($field) ?>
+                    </td>
+                    <?php } ?>
+                </tr>
             <?php } ?>
-        </tr>
-    <?php } ?>
-</table>
+        </tbody>
+    </table>
+</div>
