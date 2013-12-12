8ebb1953b615a7808c7284e9eff1806a778222f1
Algimantas <algimantas@impresspages.org>
2013-12-12 16:30:45 +0200

    IpHtml widget

--- Ip/Module/Content/Widget/IpHtml/look/default.php
+++ Ip/Module/Content/Widget/IpHtml/look/default.php
@@ -1,1 +1,6 @@
-<?php echo isset($html) ? $html : ''; ?>
+<?php if (ipContent()->isManagementState()) { ?>
+    <div class="ipsContainer"><?php echo isset($html) ? $html : ''; ?></div>
+<?php } else { ?>
+    <?php echo isset($html) ? $html : ''; ?>
+<?php } ?>
+
