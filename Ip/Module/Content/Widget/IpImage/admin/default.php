cd03797480648f3eeca0f0819cacdeee69df04e7
Mangirdas Skripka <mangirdas@impresspages.org>
2013-12-12 16:06:49 +0200

    remove admin folder in widgets

--- Ip/Module/Content/Widget/IpImage/admin/default.php
+++
@@ -1,5 +0,0 @@
-<div class="ipaImage"></div>
-<div class="ipaOptions">
-    <label class="ipAdminLabel"><?php _e('Title', 'ipAdmin') ?></label>
-    <input type="text" class="ipAdminInput ipaImageTitle" name="title" value="<?php echo htmlspecialchars(isset($title) ? $title : ''); ?>" />
-</div>
