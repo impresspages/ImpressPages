43f8c889858a1815d9b8aac1e1076eed48442b24
Mangirdas Skripka <mangirdas@impresspages.org>
2013-12-12 16:06:49 +0200

    remove admin folder in widgets

--- Ip/Module/Content/Widget/IpColumns/admin/default.php
+++
@@ -1,4 +0,0 @@
-<p><?php _e('Number of columns', 'ipAdmin'); ?> <input type="number" name="columns" value="<?php echo (int) $columns ?>"/></p>
-<form>
-    <input name="baseId" type="hidden" value="<?php echo esc($baseId) ?>"/>
-</form>
