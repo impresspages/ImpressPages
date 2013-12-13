b1cc5e3ada444c804eaed646ffdeb88e10d42eb8
Algimantas <algimantas@impresspages.org>
2013-12-13 11:20:56 +0200

    Autoformat

--- Ip/Grid1/Controller.php
+++ Ip/Grid1/Controller.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @package		ImpressPages
+ * @package        ImpressPages
  */
 
 namespace Ip\Grid1;
@@ -10,7 +10,8 @@ namespace Ip\Grid1;
  * Some function to speed up ecommerce products development
  * @package Library\Php\Ecommerce
  */
-abstract class Controller extends \Ip\Controller{
+abstract class Controller extends \Ip\Controller
+{
     public function index()
     {
         ipAddJavascript(ipFileUrl('Ip/Module/Ip/assets/grid1/src/grid1.js'));
@@ -33,7 +34,7 @@ abstract class Controller extends \Ip\Controller{
     public function grid()
     {
         $worker = new Worker($this->config());
-        $result =  $worker->handleMethod(ipRequest());
+        $result = $worker->handleMethod(ipRequest());
         return new \Ip\Response\JsonRpc($result);
     }
 
