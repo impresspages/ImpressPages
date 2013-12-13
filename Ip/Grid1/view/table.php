a31583a0fa51fcf68d26d088ac708a6888bf3862
Algimantas <algimantas@impresspages.org>
2013-12-13 18:23:26 +0200

    Pagination

--- Ip/Grid1/view/table.php
+++ Ip/Grid1/view/table.php
@@ -25,33 +25,20 @@
     <?php if ($totalPages > 1) { ?>
 
         <ul class="pagination">
-            <?php
-            $firstPage = max(1, $currentPage - 5);
-            if ($firstPage < 3) {
-                $firstPage = 1;
-            }
-            $lastPage = min($totalPages, $firstPage + 20);
-            if ($lastPage >= $totalPages - 2) {
-                $lastPage = $totalPages;
-            }
-            ?>
             <?php if ($currentPage > 1) { ?>
                 <li><a href="#" class="ipsAction" data-method="init" data-params="<?php echo esc(json_encode(array('page' => $currentPage - 1))) ?>">&laquo;</a></li>
             <?php } else { ?>
                 <li class="disabled"><a href="#">&laquo;</a></li>
             <?php } ?>
-            <?php if ($firstPage > 1) { ?>
-                <li><a href="#"class="ipsAction" data-method="init" data-params="<?php echo esc(json_encode(array('page' => 1))) ?>">1</a></li>
-                <li class="disabled"><a href="#">..</a></li>
+
+            <?php foreach ($pages as $page) { ?>
+                <?php if (is_numeric($page)) { ?>
+                    <li <?php if ($page == $currentPage) { ?>class="active"<?php } ?>><a href="#" class="ipsAction" data-method="init" data-params="<?php echo esc(json_encode(array('page' => $page))) ?>"><?php echo esc($page) ?></a></li>
+                <?php } elseif (isset($page['page'])) { ?>
+                    <li <?php if ($page['page'] == $currentPage) { ?>class="active"<?php } ?>><a href="#" class="ipsAction" data-method="init" data-params="<?php echo esc(json_encode(array('page' => $page['page']))) ?>"><?php echo esc(isset($page['text']) ? $page['text'] : $page['page']) ?></a></li>
+                <?php } ?>
             <?php } ?>
 
-            <?php for ($i = $firstPage; $i < $lastPage; $i++) { ?>
-                <li <?php if ($i == $currentPage) { ?>class="active"<?php } ?>><a href="#"class="ipsAction" data-method="init" data-params="<?php echo esc(json_encode(array('page' => $i))) ?>"><?php echo $i ?></a></li>
-            <?php } ?>
-            <?php if ($lastPage < $totalPages) { ?>
-                <li class="disabled"><a href="#">..</a></li>
-                <li><a href="#" class="ipsAction" data-method="init" data-params="<?php echo esc(json_encode(array('page' => $totalPages))) ?>"><?php echo $totalPages ?></a></li>
-            <?php } ?>
             <?php if ($currentPage < $totalPages) { ?>
                 <li><a href="#" class="ipsAction" data-method="init" data-params="<?php echo esc(json_encode(array('page' => $currentPage + 1))) ?>">&raquo;</a></li>
             <?php } else { ?>
