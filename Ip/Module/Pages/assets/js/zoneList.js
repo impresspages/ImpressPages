a4559b23f77979ebba89e2b10c64c792ca7b8983
Mangirdas Skripka <mangirdas@impresspages.org>
2013-12-15 23:53:06 +0200

    select zone

--- Ip/Module/Pages/assets/js/zoneList.js
+++ Ip/Module/Pages/assets/js/zoneList.js
@@ -1,3 +1,10 @@
 function ZoneList($scope) {
     $scope.zones = zoneList;
+
+    $scope.activate = function(zone) {
+        $.each($scope.zones, function (key, value) {
+            value.active = false;
+        })
+        zone.active = 1;
+    }
 }
--- Ip/Module/Pages/view/layout.php
+++ Ip/Module/Pages/view/layout.php
@@ -1,23 +1,23 @@
 <div class="ip" ng-app>
-    <div ng-controller="LanguageList" class="languageList">
+    <div ng-controller="LanguageList" class="languageList navBlock">
         <ul>
             <li ng-repeat="language in languages">
                 <a href="#">{{language.abbreviation}}</a>
             </li>
         </ul>
     </div>
-    <div ng-controller="ZoneList" class="zoneList">
-        <ul>
-            <li ng-repeat="zone in zones">
-                <a href="#">{{zone.title}}</a>
-            </li>
-        </ul>
-    </div>
-    <div ng-controller="pageTree" class="pageTree">
-        <ul>
-            <li ng-repeat="zone in zones">
-                <a href="#">{{zone.title}}</a>
-            </li>
-        </ul>
+    <div ng-controller="ZoneList">
+        <div class="zoneList navBlock">
+            <ul>
+                <li ng-repeat="zone in zones">
+                    <a class="title" href="#" ng-click="activate(zone)">{{zone.title}}</a>
+                </li>
+            </ul>
+        </div>
+        <div class="ipsTree pageTree navBlock">
+            <div ng-repeat="zone in zones" class="tree" ng-show="zone.active">
+                tree {{zone.title}}
+            </div>
+        </div>
     </div>
 </div>
