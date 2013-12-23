45d00c559cf22bd69250c9e288467d46f1027ac0
Algimantas <algimantas@impresspages.org>
2013-12-23 13:26:00 +0200

    Cleanup

--- Ip/Internal/Admin/Service.php
+++ Ip/Internal/Admin/Service.php
@@ -7,19 +7,6 @@ namespace Ip\Internal\Admin;
 
 class Service
 {
-    /**
-     * Injects admin html into old backend modules.
-     *
-     * @deprecated
-     * @param string $html
-     * @return mixed
-     */
-    public static function injectAdminHtml($html)
-    {
-        $system = new System();
-        return $system->injectAdminHtml($html);
-    }
-
     public static function isSafeMode()
     {
         return \Ip\Internal\Admin\Model::isSafeMode();
