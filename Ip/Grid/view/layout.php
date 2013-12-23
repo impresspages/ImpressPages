5f8f331491e1d91185dea0575418d2f0a8294701
Algimantas <algimantas@impresspages.org>
2013-12-23 11:51:11 +0200

    Refactored pagination to other module

--- Ip/Grid/view/layout.php
+++ Ip/Grid/view/layout.php
@@ -1,5 +1,5 @@
 <div class="ip scopeGrid">
     <?php echo $this->subview('actions.php'); ?>
     <?php echo $this->subview('table.php'); ?>
-    <?php echo $this->subview('pagination.php'); ?>
+    <?php echo $pagination->render(ipFile('Ip/Grid/view/pagination.php')); ?>
 </div>
