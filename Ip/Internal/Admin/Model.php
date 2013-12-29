8ccd7dbae7f7af6495c3af4fcb1e6b5ffda18d5d
Algimantas <algimantas@impresspages.org>
2013-12-29 14:48:07 +0200

    Cleanup

--- Ip/Internal/Admin/Model.php
+++ Ip/Internal/Admin/Model.php
@@ -67,55 +67,6 @@ class Model{
         return false;
     }
 
-    protected function getOldModules($managed = null, $userId = null) {
-        global $cms;
-        $groups = array();
-        $sql = "select g.name as g_name, g.id, g.translation from `".DB_PREF."module_group` g
-  	where 1 order by row_number";
-
-        $rs = ip_deprecated_mysql_query($sql);
-        if($rs) {
-            while($lock = ip_deprecated_mysql_fetch_assoc($rs)) {
-                if(!isset($groups[$lock['translation']])) { //if exist two groups with the same translation this array can be identified already
-                    $groups[$lock['translation']] = array();
-                }
-
-                if ($managed === null) {
-                    $managedSql = '';
-                } else {
-                    if ($managed) {
-                        $managedSql = ' and managed ';
-                    } else {
-                        $managedSql = ' and not managed ';
-                    }
-                }
-
-                if($userId === null) {
-                    $sql2 = "select m.name as m_name, m.id, m.translation, core from
-          `".DB_PREF."module` m where m.group_id = '".$lock['id']."' ".$managedSql."
-           order by row_number";
-                }else {
-                    $sql2 = "select m.name as m_name, m.id, m.translation, core from
-          `".DB_PREF."user_to_mod` um,`".DB_PREF."module` m where
-          um.userId = '".ip_deprecated_mysql_real_escape_string($userId)."' and um.module_id = m.id and
-          m.group_id = '".$lock['id']."' ".$managedSql." order by row_number";
-                }
-                $rs2 = ip_deprecated_mysql_query($sql2);
-                if($rs2) {
-                    while($lock2 = ip_deprecated_mysql_fetch_assoc($rs2)) {
-                        $lock2['g_name'] =  $lock['g_name'];
-                        $lock2['g_id'] =  $lock['id'];
-                        $groups[$lock['translation']][] = $lock2;
-                    }
-                    if(sizeof($groups[$lock['translation']]) == 0) {
-                        unset($groups[$lock['translation']]);
-                    }
-                }else trigger_error($sql." ".ip_deprecated_mysql_error());
-            }
-        }else trigger_error($sql." ".ip_deprecated_mysql_error());
-        return $groups;
-    }
-
     public function getUserId(){
         if(isset($_SESSION['backend_session']['userId']))
             return $_SESSION['backend_session']['userId'];
