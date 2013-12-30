e05ee4186c79afa02f3399310cdf8905cf2f3407
Mangirdas Skripka <mangirdas@impresspages.org>
2013-12-30 16:13:07 +0200

    remove DB_PREF usage

--- Ip/Internal/Languages/Db.php
+++ Ip/Internal/Languages/Db.php
@@ -60,29 +60,7 @@ class Db {
         return $answer;
     }
 
-    public static function deleteRootZoneElement($language) {
-        $zones = Db::getZones();
-        foreach($zones as $key => $zone) {
-
-            $sql = "delete `".DB_PREF."content_element`.*, `".DB_PREF."zone_to_content`.* from `".DB_PREF."content_element`, `".DB_PREF."zone_to_content` where
-      `".DB_PREF."zone_to_content`.zone_id = ".$zone['id']." and `".DB_PREF."zone_to_content`.element_id = `".DB_PREF."content_element`.id and `".DB_PREF."zone_to_content`.language_id = '".ip_deprecated_mysql_real_escape_string($language)."'";
-            $rs = ip_deprecated_mysql_query($sql);
-            if(!$rs) {
-                trigger_error($sql." ".ip_deprecated_mysql_error());
-            }
-
-            $sql2 = "delete from " . ipTable('zone_parameter') . " where language_id = '".ip_deprecated_mysql_real_escape_string($language)."'";
-            $rs2 = ip_deprecated_mysql_query($sql2);
-            if(!$rs2)
-            trigger_error($sql2." ".ip_deprecated_mysql_error());
-
-        }
-
-
-    }
-
-
-
+    
     public static function newUrl($preferredUrl) {
         $suffix = '';
         $url = ipDb()->select('id', 'language', array('url' => $preferredUrl . $suffix));
