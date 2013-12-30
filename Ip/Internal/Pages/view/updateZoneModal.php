039355d5a0aa1b5fdeafe565ceff3f2e7232657b
Mangirdas Skripka <mangirdas@impresspages.org>
2013-12-30 15:56:27 +0200

    cleanup content service

--- Ip/Internal/Pages/view/updateZoneModal.php
+++ Ip/Internal/Pages/view/updateZoneModal.php
@@ -5,7 +5,10 @@
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                 <h4 class="modal-title"><?php _e('Update menu zone', 'ipAdmin') ?></h4>
             </div>
-            <div class="modal-body ipsBody">
+            <div class="modal-body">
+                <button class="btn btn-default ipsDelete" role="button">
+                    <i class="fa fa-delete"></i><?php _e('Delete', 'ipAdmin') ?></button>
+                <div class="ipsBody"></div>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal"><?php _e('Cancel', 'ipAdmin') ?></button>
