639b4071da20185f29b5511dc52ba9e0e0a6973a
Mangirdas Skripka <mangirdas@impresspages.org>
2013-12-31 18:08:54 +0200

    remove unused model functions

--- Ip/Internal/Languages/Db.php
+++ Ip/Internal/Languages/Db.php
@@ -29,31 +29,7 @@ class Db {
         return false;
     }
 
-    public static function getLanguageByUrl($url) {
-        $sql = "select * from " . ipTable('language') . " where `url` = '".ip_deprecated_mysql_real_escape_string($url)."' ";
-        $rs = ip_deprecated_mysql_query($sql);
-        if($rs) {
-            if($lock = ip_deprecated_mysql_fetch_assoc($rs)){
-                return $lock;
-            }
-        }else {
-            trigger_error($sql." ".ip_deprecated_mysql_error());
-        }
-        return false;
-    }
 
-    public static function getZones() {
-        $answer = array();
-        $sql = "select * from " . ipTable('zone') . " where 1 order by row_number";
-        $rs = ip_deprecated_mysql_query($sql);
-        if($rs) {
-            while($lock = ip_deprecated_mysql_fetch_assoc($rs))
-            $answer[] = $lock;
-        }else {
-            trigger_error($sql." ".ip_deprecated_mysql_error());
-        }
-        return $answer;
-    }
 
     
     public static function newUrl($preferredUrl) {
