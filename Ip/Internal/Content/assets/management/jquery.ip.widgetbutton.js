fc235b6c6c62bd0119ba94717b2125b89fed24c4
Mangirdas Skripka <mangirdas@impresspages.org>
2014-01-02 09:10:15 +0200

    widget side droppable

--- Ip/Internal/Content/assets/management/jquery.ip.widgetbutton.js
+++ Ip/Internal/Content/assets/management/jquery.ip.widgetbutton.js
@@ -34,7 +34,47 @@
                             }
                         },
                         helper : 'clone',
-                        stop: function(event, ui) { }
+                        stop: function(event, ui) {
+                            console.log('Drop');
+                            console.log(ui);
+                            if (draggableOnDroppable) {
+                                //jQuery-ui droppable is buggy and fire fake drop event. So we better handle stop event on draggable. This is just for widget side drops
+                                $(draggableOnDroppable).css('backgroundColor', 'black');
+                            }
+
+                        },
+                        start: function (event, ui) {
+                            $('.ipBlock').each(function(key, value) {
+                                var $droppable = $('<div style="">DROPPABLE</div>');
+                                $('body').append($droppable);
+
+                                $droppable.css('position', 'absolute');
+                                $droppable.css('left', $(value).offset().left - $droppable.width() + 'px');
+                                $droppable.css('top', $(value).offset().top + 100 + 'px');
+                                $droppable.droppable({
+                                    accept: ".ipActionWidgetButton, .ipWidget",
+                                    activeClass: "ui-state-hover",
+                                    hoverClass: "ui-state-active",
+                                    over: function(event,ui) {
+                                        draggableOnDroppable = this;
+                                    },
+                                    out: function(event, ui) {
+                                        draggableOnDroppable = false;
+                                    },
+                                    drop: function( event, ui ) {
+                                        $( this )
+                                            .addClass( "ui-state-highlight" )
+                                            .html( "Dropped!" );
+                                        console.log(this);
+                                        console.log(event);
+                                        console.log(ui);
+                                    }
+                                });
+
+
+                            });
+                            console.log('start');
+                        }
                     });
                     
                     $this.data('ipAdminWidgetButton', {
