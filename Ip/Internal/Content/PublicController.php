d227f5e7a4b84bccf86b58f063a2a124ef35da4e
Mangirdas Skripka <mangirdas@impresspages.org>
2014-01-02 10:02:48 +0200

    remove unused js code

--- Ip/Internal/Content/PublicController.php
+++ Ip/Internal/Content/PublicController.php
@@ -74,7 +74,6 @@ class PublicController extends \Ip\Controller
         if (ipConfig()->isDebugMode()) {
             ipAddJs(ipFileUrl('Ip/Internal/Content/assets/management/ipContentManagement.js'));
             ipAddJs(ipFileUrl('Ip/Internal/Content/assets/management/jquery.ip.contentManagement.js'));
-            ipAddJs(ipFileUrl('Ip/Internal/Content/assets/management/jquery.ip.pageOptions.js'));
             ipAddJs(ipFileUrl('Ip/Internal/Content/assets/management/jquery.ip.widgetbutton.js'));
             ipAddJs(ipFileUrl('Ip/Internal/Content/assets/management/jquery.ip.layoutModal.js'));
             ipAddJs(ipFileUrl('Ip/Internal/Content/assets/management/jquery.ip.block.js'));
