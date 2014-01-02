b7430b7509707a3981ead14e4ff39ad39a3b0cf5
Algimantas <algimantas@impresspages.org>
2014-01-02 16:48:06 +0200

    Travis debug

--- phpunit/travis-nginx-install.sh
+++ phpunit/travis-nginx-install.sh
@@ -47,7 +47,6 @@ server {
 	location ~ \.php {
 		fastcgi_pass	unix:$PHP_FPM_SOCK;
 		include			fastcgi_params;
-		fastcgi_param  SCRIPT_FILENAME    $document_root$fastcgi_script_name;
 	}
 }
 " | sudo tee $NGINX_CONF > /dev/null
