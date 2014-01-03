5388869a4409f30db26d63ab24770833e750ea6e
Algimantas <algimantas@impresspages.org>
2014-01-03 17:05:55 +0200

    Fix exit codes

--- phpunit/travis-tests.sh
+++ phpunit/travis-tests.sh
@@ -2,8 +2,10 @@
 
 if [ $TEST_GROUP = "other" ]
 then
-./vendor/bin/phpunit --exclude-group Sauce,smoke,ignoreOnTravis --colors Tests/
+  ./vendor/bin/phpunit --exclude-group Sauce,smoke,ignoreOnTravis --colors Tests/
+  exit $?
 else
-./vendor/bin/phpunit --group $TEST_GROUP --colors Tests/
+  ./vendor/bin/phpunit --group $TEST_GROUP --colors Tests/
+  exit $?
 fi
 
