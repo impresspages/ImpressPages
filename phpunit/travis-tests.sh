2c140614d59d01f9d500321115fb8efef078a405
Algimantas <algimantas@impresspages.org>
2014-01-03 16:24:25 +0200

    Travis tests

--- phpunit/travis-tests.sh
+++ phpunit/travis-tests.sh
@@ -1,3 +1,9 @@
 #!/usr/bin
 
-./vendor/bin/phpunit --group $1 --colors Tests/
+if [ $TEST_GROUP = "other" ]
+then
+./vendor/bin/phpunit --exclude-group Sauce,smoke,ignoreOnTravis --colors Tests/
+else
+./vendor/bin/phpunit --group $TEST_GROUP --colors Tests/
+fi
+
