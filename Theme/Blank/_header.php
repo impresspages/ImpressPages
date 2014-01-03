0cfbe62744f1c25f7eec1e94f91cb226c95c5007
Mangirdas Skripka <mangirdas@impresspages.org>
2014-01-03 10:36:21 +0200

    make charset the first line in head of blank theme

--- Theme/Blank/_header.php
+++ Theme/Blank/_header.php
@@ -7,9 +7,9 @@
 <?php echo ipDoctypeDeclaration(); ?>
 <html<?php echo ipHtmlAttributes(); ?>>
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php ipAddCss(ipThemeUrl('assets/theme.css')); ?>
-    <?php echo ipHead(); //todox: does this print charset=UTF8? ?>
+    <?php echo ipHead(); ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
 <div class="theme clearfix">
