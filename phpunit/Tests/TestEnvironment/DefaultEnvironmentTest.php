99eded96afec356d6f576759771e6781246bf4b7
Algimantas <algimantas@impresspages.org>
2014-01-06 12:07:14 +0200

    Test environment

--- phpunit/Tests/TestEnvironment/DefaultEnvironmentTest.php
+++ phpunit/Tests/TestEnvironment/DefaultEnvironmentTest.php
@@ -12,6 +12,8 @@ class DefaultEnvironmentTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(realpath(TEST_CODEBASE_DIR), ipConfig()->getRaw('BASE_DIR'));
 
         $this->assertTrue(file_exists(ipFile('file/repository/')), 'file/repository doesn\'t exist.');
+
+        $this->assertNotEmpty(ipDispatcher(), 'Dispatcher not loaded.');
     }
 
 
