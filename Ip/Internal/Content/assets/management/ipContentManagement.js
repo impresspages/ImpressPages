9f8c38dba840b0eac0ee9c15c1f5b89bc302e791
Mangirdas Skripka <mangirdas@impresspages.org>
2014-01-07 10:07:42 +0200

    reformat content management js

--- Ip/Internal/Content/assets/management/ipContentManagement.js
+++ Ip/Internal/Content/assets/management/ipContentManagement.js
@@ -1,33 +1,31 @@
 /**
-* @package ImpressPages
-*
-*/
+ * @package ImpressPages
+ *
+ */
 
 
-var ipContentManagement;
 
-
-(function($){
+(function ($) {
     "use strict";
 
 
-    ipContentManagement = new function() {
+    var ipContentManagement = new function () {
 
         var widgetOnDroppable = false;
 
 
-        $(document).ready(function() {
+        $(document).ready(function () {
             var $ipObject = $(document);
 
             $ipObject.bind('initFinished.ipContentManagement', ipAdminPanelInit);
             $ipObject.bind('initFinished.ipContentManagement', ipAdminWidgetsScroll);
-            $(window).bind('resizeEnd',                        ipAdminWidgetsScroll);
+            $(window).bind('resizeEnd', ipAdminWidgetsScroll);
             $ipObject.bind('initFinished.ipContentManagement', ipAdminWidgetsSearch);
 
             $ipObject.ipContentManagement();
 
             // case insensitive search
-            ip.jQuery.expr[':'].icontains = function(a, i, m) {
+            ip.jQuery.expr[':'].icontains = function (a, i, m) {
                 return ip.jQuery(a).text().toUpperCase().indexOf(m[3].toUpperCase()) >= 0;
             };
 
@@ -41,9 +39,11 @@ var ipContentManagement;
 
         });
 
-        $(window).resize(function() {
-            if(this.resizeTO) { clearTimeout(this.resizeTO); }
-            this.resizeTO = setTimeout(function() {
+        $(window).resize(function () {
+            if (this.resizeTO) {
+                clearTimeout(this.resizeTO);
+            }
+            this.resizeTO = setTimeout(function () {
                 $(this).trigger('resizeEnd');
             }, 100);
         });
@@ -57,7 +57,7 @@ var ipContentManagement;
          *
          *
          */
-        var ipAdminWidgetsScroll = function() {
+        var ipAdminWidgetsScroll = function () {
             var $scrollable = $('.ipAdminWidgetsContainer'); // binding object
             $scrollable.scrollable({
                 items: 'li', // items are <li> elements; on scroll styles will be added to <ul>
@@ -67,14 +67,16 @@ var ipContentManagement;
             var itemWidth = scrollableAPI.getItems().eq(0).outerWidth(true);
             var containerWidth = scrollableAPI.getRoot().width() + 24; // adding left side compensation
             var scrollBy = Math.floor(containerWidth / itemWidth); // define number of items to scroll
-            if(scrollBy < 1) { scrollBy = 1; } // setting the minimum
+            if (scrollBy < 1) {
+                scrollBy = 1;
+            } // setting the minimum
             $('.ipAdminWidgets .ipaRight, .ipAdminWidgets .ipaLeft').unbind('click'); // unbind if reinitiating dynamically
             scrollableAPI.begin(); // move to scroller to default position (beginning)
-            $('.ipAdminWidgets .ipaRight').click(function(event){
+            $('.ipAdminWidgets .ipaRight').click(function (event) {
                 event.preventDefault();
                 scrollableAPI.move(scrollBy);
             });
-            $('.ipAdminWidgets .ipaLeft').click(function(event){
+            $('.ipAdminWidgets .ipaLeft').click(function (event) {
                 event.preventDefault();
                 scrollableAPI.move(-scrollBy);
             });
@@ -89,24 +91,26 @@ var ipContentManagement;
          *
          *
          */
-        var ipAdminWidgetsSearch = function() {
+        var ipAdminWidgetsSearch = function () {
             var $input = $('.ipAdminWidgetsSearch .ipaInput');
             var $button = $('.ipAdminWidgetsSearch .ipaButton');
             var $widgets = $('.ipAdminWidgetsContainer li');
 
-            $input.focus(function(){
-                if( this.value == this.defaultValue ){
+            $input.focus(function () {
+                if (this.value == this.defaultValue) {
                     this.value = '';
-                };
-            }).blur(function(){
-                    if( this.value == '' ){
+                }
+                ;
+            }).blur(function () {
+                    if (this.value == '') {
                         this.value = this.defaultValue;
-                    };
-                }).keyup(function(){
+                    }
+                    ;
+                }).keyup(function () {
                     var value = this.value;
-                    $widgets.css('display',''); // restate visibility
-                    if (value && value != this.defaultValue ) {
-                        $widgets.not(':icontains(' + value + ')').css('display','none');
+                    $widgets.css('display', ''); // restate visibility
+                    if (value && value != this.defaultValue) {
+                        $widgets.not(':icontains(' + value + ')').css('display', 'none');
                         $button.addClass('ipaClear');
                     } else {
                         $button.removeClass('ipaClear');
@@ -114,7 +118,7 @@ var ipContentManagement;
                     ipAdminWidgetsScroll(); // reinitiate scrollable
                 });
 
-            $button.click(function(event){
+            $button.click(function (event) {
                 event.preventDefault();
                 var $this = $(this);
                 if ($this.hasClass('ipaClear')) {
@@ -133,15 +137,15 @@ var ipContentManagement;
          *
          *
          */
-        var ipAdminPanelInit = function() {
+        var ipAdminPanelInit = function () {
             var $container = $('.ipAdminPanelContainer'); // the most top element physically creates a space
             var $panel = $('.ipAdminPanel'); // Administration Panel that stays always visible
             $container.height($panel.height()); // setting the height to container
-            $panel.css('top',$('.ipsAdminToolbarContainer').outerHeight()); // move down to leave space for top toolbar
+            $panel.css('top', $('.ipsAdminToolbarContainer').outerHeight()); // move down to leave space for top toolbar
         }
 
-        var ipStartWidgetDrag = function(event, ui) {
-            $('.ipWidget').each(function(key, value) {
+        var ipStartWidgetDrag = function (event, ui) {
+            $('.ipWidget').each(function (key, value) {
                 //left placeholder
                 var $droppable = $('<div class="ipsWidgetDropPlaceholder" style="width: 10px; background-color: #000;"></div>');
                 $('body').append($droppable);
@@ -167,20 +171,20 @@ var ipContentManagement;
                 accept: ".ipActionWidgetButton, .ipWidget",
                 activeClass: "ui-state-hover",
                 hoverClass: "ui-state-active",
-                over: function(event,ui) {
+                over: function (event, ui) {
                     widgetOnDroppable = $(this);
                 },
-                out: function(event, ui) {
+                out: function (event, ui) {
                     widgetOnDroppable = false;
                 },
-                drop: function( event, ui ) {
+                drop: function (event, ui) {
                     //this method on jQuery-ui is buggy and fires fake drop events. So we better handle stop event on draggable. This is just for widget side drops.
                 }
             });
 
         }
 
-        var ipStopWidgetDrag = function(event, ui) {
+        var ipStopWidgetDrag = function (event, ui) {
 
             if (widgetOnDroppable) {
                 var targetWidgetInstanceId = widgetOnDroppable.data('instanceId');
@@ -196,7 +200,7 @@ var ipContentManagement;
             // todo
         }
 
-        var ipAddWidgetToSide = function(widgetName, targetWidgetInstanceId, leftOrRight) {
+        var ipAddWidgetToSide = function (widgetName, targetWidgetInstanceId, leftOrRight) {
 
             console.log(widgetName);
             console.log(targetWidgetInstanceId);
