7e743be9327a745419dfb77621762986378093a3
Algimantas <algimantas@impresspages.org>
2014-01-07 11:14:59 +0200

    Exception data

--- Ip/Exception.php
+++ Ip/Exception.php
@@ -8,15 +8,24 @@
 namespace Ip;
 
 
-/**
- * IpCmsException class
- */
 class Exception extends \Exception
 {
     // Redefine the exception so message isn't optional
-    public function __construct($message, $code = 0, \Exception $previous = null)
+    /**
+     * @var array|null
+     */
+    protected $data;
+
+    public function __construct($message, $data = null, $code = 0, \Exception $previous = null)
     {
+        $this->data = $data;
+
         // make sure everything is assigned properly
         parent::__construct($message, $code, $previous);
+    }
+
+    public function getData()
+    {
+        return $this->data;
     }
 }
