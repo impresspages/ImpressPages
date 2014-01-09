f0a4b7c3a726b8ed53a85644283d7dca4f86844a
Algimantas <algimantas@impresspages.org>
2014-01-09 19:12:35 +0200

    Removed debug code

--- Ip/Db.php
+++ Ip/Db.php
@@ -57,11 +57,6 @@ class Db
             $this->pdoConnection->exec("SET time_zone='$offset';");
             $this->pdoConnection->exec("SET CHARACTER SET " . $dbConfig['charset']);
         } catch (\PDOException $e) {
-            //* TODOX remove
-            var_export($dbConfig);
-            echo __FILE__ . ':' . (__LINE__ - 2);
-            exit();
-            //*/
             throw new \Ip\Exception\Db("Can't connect to database. Stack trace hidden for security reasons");
             //PHP traces all details of error including DB password. This could be a disaster on live server. So we hide that data.
         }
