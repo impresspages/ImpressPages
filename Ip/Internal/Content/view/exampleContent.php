988a3d585a96ca3648fed578f30c391a528bdc15
Mangirdas Skripka <mangirdas@impresspages.org>
2014-01-10 12:08:10 +0200

    fix example content

--- Ip/Internal/Content/view/exampleContent.php
+++ Ip/Internal/Content/view/exampleContent.php
@@ -14,7 +14,7 @@
         <li>And many more.</li>
     </ul>
 ')); ?>
-<?php echo ipRenderWidget('IpTitle', array('title' => 'For titles always use "Title" widget'), 'level2'); ?>
+<?php echo ipRenderWidget('IpTitle', array('title' => 'For titles always use "Title" widget', 'level' => 2)); ?>
 
 <?php echo ipRenderWidget('IpImage', array('imageSmall' => 'Ip/Internal/Content/img/example_image.jpg')); ?>
 
@@ -23,4 +23,4 @@
     <p>Add any widget to work with different types of content. There are many of them: title, text, separator, text with image, image, image gallery, logo gallery, file, table, HTML code, F.A.Q., Contact form.</p>
 ')); ?>
 
-<?php echo ipRenderWidget('IpTitle', array('title' => 'Have a great experience to get most out of your website!'), 'level3'); ?>
+<?php echo ipRenderWidget('IpTitle', array('title' => 'Have a great experience to get most out of your website!', 'level' => 3)); ?>
