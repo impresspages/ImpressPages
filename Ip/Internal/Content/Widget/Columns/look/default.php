f53813010293c955da62acddbbbbdf635f339b2e
Audrius Jankauskas <audrius@impresspages.org>
2014-01-13 17:43:32 +0200

    removing negative margins from Columns widget

--- Ip/Internal/Content/Widget/Columns/look/default.php
+++ Ip/Internal/Content/Widget/Columns/look/default.php
@@ -1,5 +1,5 @@
 <?php
-/**
+/** todox: make description related to real variables
  * preview / layout template for multicolumn widget. This template needs to render
  * the required markup for columns, and generate a unique block inside of each
  * column. To make block unique, use $baseId which is provided by controller.
@@ -11,9 +11,8 @@
  *    <div class="span8"><?php echo ipBlock('multicol'.$baseId.'right') ?></div>
  *  </div>
  *
- * The following code based on <table> is ONLY included to support templates which do
- * not have a grid system. Templates which do have a proper grid system should really
- * override this.
+ * The following code is ONLY included to support templates which do not have a grid system.
+ * Templates which do have a proper grid system should really override this.
  *
  */
 
@@ -24,7 +23,7 @@ if (empty($columns)) {
 ?>
 
 <?php for($i = 1; $i <= $columns; $i++) { ?>
-    <div class="ipwCol" style="width:<?php echo (100 / $columns) ?>%;" style="width: 25%; vertical-align: top;">
+    <div class="ipwCol<?php if($i==1){echo ' first';}?><?php if($i==$columns){echo ' last';}?>" style="width:<?php echo (100 / $columns) ?>%;">
         <?php echo ipBlock('multicol'.$widgetId.'_'.$i)->exampleContent(' ')->render($revisionId); ?>
     </div>
 <?php } ?>
