c1f1ed6239277789540ac07ee59788d4f9842d99
Mangirdas Skripka <mangirdas@impresspages.org>
2014-01-15 17:17:19 +0200

    remove blocks from blank theme

--- Theme/Blank/home.php
+++ Theme/Blank/home.php
@@ -5,16 +5,6 @@
  */
 ?>
 <?php echo $this->subview('_header.php'); ?>
-    <?php
-    if (ipGetThemeOption('homeBlocks', 0) > 0) {
-        echo $this->subview('_homeBlocks.php');
-    }
-    ?>
-    <?php
-    if (ipGetThemeOption('homeBlocks2', 0) > 0) {
-        echo $this->subview('_homeBlocks2.php');
-    }
-    ?>
     <div class="main col_12">
         <?php echo ipBlock('main')->exampleContent(' '); ?>
     </div>
