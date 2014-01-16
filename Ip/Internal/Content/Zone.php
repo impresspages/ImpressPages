654197a1f6f30151923fcea320a3b7dad54f55b1
marijusimpresspages <marijus@impresspages.org>
2014-01-16 16:11:19 +0200

    Removed setModifyFrequency method and modify_track database records

--- Ip/Internal/Content/Zone.php
+++ Ip/Internal/Content/Zone.php
@@ -194,7 +194,6 @@ class Zone extends \Ip\Zone
         $newPage->setText($dbPage['cached_text']);
         $newPage->setLastModified($dbPage['last_modified']);
         $newPage->setCreatedOn($dbPage['created_on']);
-        $newPage->setModifyFrequency($dbPage['modify_frequency']);
         $newPage->setVisible($dbPage['visible']);
         if ($firstLevel) {
             $newPage->setParentId(null);
