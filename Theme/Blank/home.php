81ff91a64408055913427f394984f0fccc4bae94
Audrius Jankauskas <audrius@impresspages.org>
2014-01-16 13:58:22 +0200

    todox for default theme

--- Theme/Blank/home.php
+++ Theme/Blank/home.php
@@ -5,6 +5,7 @@
  */
 ?>
 <?php echo $this->subview('_header.php'); ?>
+<?php // todox: removed home blocks; loosing examples how front page can be built; 3 columns is now impossible to build ?>
     <div class="main col_12">
         <?php echo ipBlock('main')->exampleContent(' '); ?>
     </div>
