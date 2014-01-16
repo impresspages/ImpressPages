f6ef8b5408271cff217475040147aacdc194886c
Algimantas <algimantas@impresspages.org>
2014-01-16 17:09:13 +0200

    Rename test

--- phpunit/Tests/Functional/AdminLoginTest.php
+++ phpunit/Tests/Functional/AdminLoginTest.php
@@ -4,7 +4,7 @@ namespace Tests\Functional;
 
 use PhpUnit\Helper\TestEnvironment;
 
-class AddDeleteWidgetTest extends \PHPUnit_Framework_TestCase
+class AdminLoginTest extends \PHPUnit_Framework_TestCase
 {
     public function setup()
     {
