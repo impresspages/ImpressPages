7dc1fc20e8330002e0cab77a1ce4b817235248ec
Algimantas <algimantas@impresspages.org>
2014-01-17 11:42:14 +0200

    Use ip.jQuery

--- Ip/Internal/Config/assets/config.js
+++ Ip/Internal/Config/assets/config.js
@@ -1,129 +1,131 @@
+var IpConfig;
 
-$( document ).ready(function() {
+(function ($) {
     "use strict";
 
-    IpConfig.init();
+    $(document).ready(function () {
 
-    $('.ipsAutoSave').on('change', IpConfig.autoSaveValue);
-    $('.ipsAutoSave').on('keyup', IpConfig.autoSaveValue);
+        IpConfig.init();
 
-    $('.ipsConfigForm').validator(validatorConfig);
-    $('.ipsConfigForm').data("validator").checkValidity();
+        $('.ipsAutoSave').on('change', IpConfig.autoSaveValue);
+        $('.ipsAutoSave').on('keyup', IpConfig.autoSaveValue);
 
-});
-
-var IpConfig = new function () {
-    "use strict";
-    var queue = [],
-        processing = false;
-
-    this.init = function () {
-        updateCronUrl();
-    }
-
-    var queueAdd = function (fieldid) {
-        queue = removeFromArray(queue, fieldid);
-        queue.push(fieldid);
-        queueProcess();
-    };
-
-    var getFieldValue = function (fieldid) {
-        var $field = $('#' + fieldid);
-        if ($field.attr('type') === 'checkbox') {
-            return $field.prop('checked') ? 1 : 0;
-        }
-
-        return $field.val();
-    }
-
-    var getFieldLanguage = function (fieldid) {
-        var $field = $('#' + fieldid);
-        return $field.data('languageid');
-    }
-
-    var getFieldName = function (fieldid) {
-        var $field = $('#' + fieldid);
-        return $field.data('fieldname');
-    }
-
-    var queueProcess = function () {
-        if (processing) {
-            return;
-        }
-        processing = true;
-        var fieldId = queue.shift();
-
-        if (!fieldId) {
-            processing = false;
-            return;
-        }
-
-        updateCronUrl();
-
-
-        var postData = {
-            'aa' : 'Config.saveValue',
-            'fieldName' : getFieldName(fieldId),
-            'value' : getFieldValue(fieldId),
-            'securityToken' : ip.securityToken,
-            'languageId' : getFieldLanguage(fieldId)
-        };
-        $.ajax({
-            url: '',
-            data: postData,
-            dataType: 'json',
-            type: 'POST',
-            success: function (response) {
-                if (response && response.error) {
-                    var errors = {};
-                    errors[fieldId] = response.error;
-                    $('.ipsConfigForm').data("validator").invalidate(errors);
-                }
-                processing = false;
-                queueProcess();
-            },
-            error: function (response) {
-                if (response && response.readyState === 4) {
-                    if (response.responseText) {
-                        alert(response.responseText);
-                    } else {
-                        alert('Autosave error');
-                    }
-                }
-                processing = false;
-                queueProcess();
-            }
-        });
-    }
-
-    var updateCronUrl = function () {
-        var $urlText = $('#automaticCron').closest('.ipmField').find('.ipmCheckboxText');
-        var $url = $urlText.find('.ipsUrl');
-        // TODO do not pass password, cron should work without password for admin
-        var $passField = $('#cronPassword').closest('.ipmField');
-        if (getFieldValue('automaticCron')) {
-            $urlText.addClass('ipgHide');
-            $passField.addClass('ipgHide');
-        } else {
-            $url.text(ip.baseUrl + '?pa=Cron&pass=' + $('#cronPassword').val());
-            $passField.removeClass('ipgHide');
-            $urlText.removeClass('ipgHide');
-        }
-    }
-
-    this.autoSaveValue = function () {
-        var $this = $(this);
+        $('.ipsConfigForm').validator(validatorConfig);
         $('.ipsConfigForm').data("validator").checkValidity();
-        queueAdd($this.data('fieldid'));
-    };
 
-    var removeFromArray = function (dataArray, value) {
-        for(var i = dataArray.length - 1; i >= 0; i--) {
-            if(dataArray[i] === value) {
-                dataArray.splice(i, 1);
+    });
+
+    IpConfig = new function () {
+        var queue = [],
+            processing = false;
+
+        this.init = function () {
+            updateCronUrl();
+        }
+
+        var queueAdd = function (fieldid) {
+            queue = removeFromArray(queue, fieldid);
+            queue.push(fieldid);
+            queueProcess();
+        };
+
+        var getFieldValue = function (fieldid) {
+            var $field = $('#' + fieldid);
+            if ($field.attr('type') === 'checkbox') {
+                return $field.prop('checked') ? 1 : 0;
+            }
+
+            return $field.val();
+        }
+
+        var getFieldLanguage = function (fieldid) {
+            var $field = $('#' + fieldid);
+            return $field.data('languageid');
+        }
+
+        var getFieldName = function (fieldid) {
+            var $field = $('#' + fieldid);
+            return $field.data('fieldname');
+        }
+
+        var queueProcess = function () {
+            if (processing) {
+                return;
+            }
+            processing = true;
+            var fieldId = queue.shift();
+
+            if (!fieldId) {
+                processing = false;
+                return;
+            }
+
+            updateCronUrl();
+
+
+            var postData = {
+                'aa': 'Config.saveValue',
+                'fieldName': getFieldName(fieldId),
+                'value': getFieldValue(fieldId),
+                'securityToken': ip.securityToken,
+                'languageId': getFieldLanguage(fieldId)
+            };
+            $.ajax({
+                url: '',
+                data: postData,
+                dataType: 'json',
+                type: 'POST',
+                success: function (response) {
+                    if (response && response.error) {
+                        var errors = {};
+                        errors[fieldId] = response.error;
+                        $('.ipsConfigForm').data("validator").invalidate(errors);
+                    }
+                    processing = false;
+                    queueProcess();
+                },
+                error: function (response) {
+                    if (response && response.readyState === 4) {
+                        if (response.responseText) {
+                            alert(response.responseText);
+                        } else {
+                            alert('Autosave error');
+                        }
+                    }
+                    processing = false;
+                    queueProcess();
+                }
+            });
+        }
+
+        var updateCronUrl = function () {
+            var $urlText = $('#automaticCron').closest('.ipmField').find('.ipmCheckboxText');
+            var $url = $urlText.find('.ipsUrl');
+            // TODO do not pass password, cron should work without password for admin
+            var $passField = $('#cronPassword').closest('.ipmField');
+            if (getFieldValue('automaticCron')) {
+                $urlText.addClass('ipgHide');
+                $passField.addClass('ipgHide');
+            } else {
+                $url.text(ip.baseUrl + '?pa=Cron&pass=' + $('#cronPassword').val());
+                $passField.removeClass('ipgHide');
+                $urlText.removeClass('ipgHide');
             }
         }
-        return dataArray;
-    }
-};
 
+        this.autoSaveValue = function () {
+            var $this = $(this);
+            $('.ipsConfigForm').data("validator").checkValidity();
+            queueAdd($this.data('fieldid'));
+        };
+
+        var removeFromArray = function (dataArray, value) {
+            for (var i = dataArray.length - 1; i >= 0; i--) {
+                if (dataArray[i] === value) {
+                    dataArray.splice(i, 1);
+                }
+            }
+            return dataArray;
+        }
+    };
+})(ip.jQuery);
