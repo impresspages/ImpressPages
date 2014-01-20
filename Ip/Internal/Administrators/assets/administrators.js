841586e14964a48089e31c8b980c6c0befcc0236
Mangirdas Skripka <mangirdas@impresspages.org>
2014-01-20 10:32:31 +0200

    add new administrator and hash the password

--- Ip/Internal/Administrators/assets/administrators.js
+++ Ip/Internal/Administrators/assets/administrators.js
@@ -9,9 +9,33 @@
         }
 
 
-        var openAddPopup = function() {
+        var openAddPopup = function () {
             $('.ipsAddAdministratorModal').modal();
+
+            $('.ipsAddAdministratorModal form').off('submit').on('submit', addSubmit);
         };
+
+        var addSubmit = function (e) {
+            e.preventDefault();
+            var form = $(this);
+
+            $.ajax({
+                url: ip.baseUrl, //we assume that for already has m, g, a parameters which will lead this request to required controller
+                dataType: 'json',
+                type : 'POST',
+                data: form.serialize(),
+                success: function (response){
+                    if (response.status && response.status == 'success') {
+                        //form has been successfully submitted.
+                    } else {
+                        //PHP controller says there are some errors
+                        if (response.errors) {
+                            //form.data("validator").invalidate(response.errors);
+                        }
+                    }
+                }
+            });
+        }
 
 
     };
