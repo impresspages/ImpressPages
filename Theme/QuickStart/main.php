0181d3a19ca21669cb6a70422df2e863b00cdc3a
Algimantas <algimantas@impresspages.org>
2014-01-20 17:10:21 +0200

    View overrides

--- Theme/QuickStart/main.php
+++ Theme/QuickStart/main.php
@@ -2,22 +2,20 @@
 <html<?php echo ipHtmlAttributes(); ?>>
 <head>
     <?php
-    ipAddCss(ipThemeUrl('ipContent.css'));
-    ipAddCss(ipThemeUrl('theme.css'));
+    ipAddCss('ipContent.css');
+    ipAddCss('theme.css');
     echo ipHead();
     ?>
 </head>
 <body>
     <div class="topmenu">
-        <?php
-        ipSlot('Ip.menu', 'menu1');
-        ?>
+        <?php ipSlot('Ip.menu', 'menu1'); ?>
     </div>
     <div class="content">
         <?php echo ipBlock('main')->render(); ?>
     </div>
     <?php
-        ipAddJs(ipThemeUrl('theme.js'));
+        ipAddJs('theme.js');
         echo ipJs();
     ?>
 </body>
