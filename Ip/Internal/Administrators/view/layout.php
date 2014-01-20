84f3df401752b95335d23c58a9b925b2637cd13a
Mangirdas Skripka <mangirdas@impresspages.org>
2014-01-20 13:45:02 +0200

    refactor administrator table

--- Ip/Internal/Administrators/view/layout.php
+++ Ip/Internal/Administrators/view/layout.php
@@ -1,9 +1,9 @@
 <div class="ipModuleAdministrators">
     <?php foreach ($administrators as $administrator){ ?>
         <div class="panel panel-default" data-administratorid="<?php echo esc($administrator['id']) ?>">
-            <div class="panel-heading"><?php echo esc($administrator['name']) ?></div>
+            <div class="panel-heading"><?php echo esc($administrator['username']) ?></div>
             <div class="panel-body">
-                <b><?php //echo esc($administrator['name']) ?></b>
+                <b><?php //echo esc($administrator['username']) ?></b>
             </div>
         </div>
     <?php } ?>
--- install/Plugin/Install/sql/structure.sql
+++ install/Plugin/Install/sql/structure.sql
@@ -340,14 +340,11 @@ DROP TABLE IF EXISTS `ip_cms_administrator`;
 
 CREATE TABLE IF NOT EXISTS `ip_cms_administrator` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL DEFAULT '',
-  `pass` text NOT NULL DEFAULT '',
-  `wrong_logins` int(11) NOT NULL DEFAULT '0',
-  `blocked` tinyint(1) NOT NULL DEFAULT '0',
-  `e_mail` varchar(255) NOT NULL DEFAULT '',
-  `row_number` int(11) NOT NULL DEFAULT '0',
+  `username` varchar(255) NOT NULL DEFAULT '',
+  `hash` text NOT NULL DEFAULT '',
+  `email` varchar(255) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`)
+  UNIQUE KEY `username` (`username`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 ;
 
 -- Table structure
