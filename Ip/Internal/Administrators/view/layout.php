7c471fc52cfe924d68ff9b0ce16a9d2734109ff0
Mangirdas Skripka <mangirdas@impresspages.org>
2014-01-22 11:17:33 +0200

    activate administrator

--- Ip/Internal/Administrators/view/layout.php
+++ Ip/Internal/Administrators/view/layout.php
@@ -1,10 +1,15 @@
-<div class="ipModuleAdministrators" ng-app="Administrators" ng-controller="ipAdministratorsController" >
-    <div ng-repeat="administrator in administrators" ng-click="activateAdministrator(administrator)" ng-class="[administrator.id == activeAdministrator.id ? 'active' : '',  'panel', 'panel-default']">
-        <div class="panel-heading">{{administrator.username}}</div>
-        <div class="panel-body">
-            <b><?php //echo esc($administrator['username']) ?></b>
+<div ng-app="Administrators" ng-controller="ipAdministratorsController">
+    <div class="ipModuleAdministrators">
+        <div ng-repeat="administrator in administrators" ng-click="activateAdministrator(administrator)" ng-class="[administrator.id == activeAdministrator.id ? 'active' : '',  'panel', 'panel-default']">
+            <div class="panel-heading">{{administrator.username}}</div>
+            <div class="panel-body">
+                <b><?php //echo esc($administrator['username']) ?></b>
+            </div>
         </div>
+        <a ng-click="addAdministratorModal()"><?php echo __('Add', 'ipAdmin'); ?></a>
     </div>
-    <a ng-click="addAdministratorModal()"><?php echo __('Add', 'ipAdmin'); ?></a>
+    <div ng-show="activeAdministrator">
+
+    </div>
 </div>
 <?php echo ipView('Ip/Internal/Administrators/view/addModal.php', array('createForm' => $createForm))->render(); ?>
