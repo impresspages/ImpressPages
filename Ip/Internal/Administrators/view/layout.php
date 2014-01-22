bd21d7b9657a2ccf4138d8076fdb97ebc42fb0e1
Mangirdas Skripka <mangirdas@impresspages.org>
2014-01-22 10:50:08 +0200

    add administrator using angularJS

--- Ip/Internal/Administrators/view/layout.php
+++ Ip/Internal/Administrators/view/layout.php
@@ -1,10 +1,10 @@
 <div class="ipModuleAdministrators" ng-app="Administrators" ng-controller="ipAdministratorsController" >
-        <div ng-repeat="administrator in administrators" class="panel panel-default">
-            <div class="panel-heading">{{administrator.username}}</div>
-            <div class="panel-body">
-                <b><?php //echo esc($administrator['username']) ?></b>
-            </div>
+    <div ng-repeat="administrator in administrators" class="panel panel-default">
+        <div class="panel-heading">{{administrator.username}}</div>
+        <div class="panel-body">
+            <b><?php //echo esc($administrator['username']) ?></b>
         </div>
-    <a class="ipsAdd"><?php echo __('Add', 'ipAdmin'); ?></a>
+    </div>
+    <a ng-click="addAdministratorModal()"><?php echo __('Add', 'ipAdmin'); ?></a>
 </div>
 <?php echo ipView('Ip/Internal/Administrators/view/addModal.php', array('createForm' => $createForm))->render(); ?>
