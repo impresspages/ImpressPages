7434291fad33e86c222eab0a435df1a086c968c7
Mangirdas Skripka <mangirdas@impresspages.org>
2014-01-22 19:14:37 +0200

    remove administrator

--- Ip/Internal/Administrators/assets/administratorsController.js
+++ Ip/Internal/Administrators/assets/administratorsController.js
@@ -28,6 +28,7 @@ var ipAdministratorsController = null;
         $scope.administrators = ipAdministrators;
         $scope.activeAdministrator = null;
         $scope.editMode = false;
+        $scope.ipAdministratorsAdminId = ipAdministratorsAdminId;
 
 
         $scope.$on('PathChanged', function (event, path) {
@@ -78,6 +79,24 @@ var ipAdministratorsController = null;
             });
             setTimeout(function() {$('.ipsUpdateModal input[name=username]').focus();}, 500);
         }
+
+        $scope.deleteModal = function (zone) {
+            $('.ipsDeleteModal').modal();
+            $('.ipsDeleteModal .ipsDelete').off('click').on('click', function (e) {
+                e.preventDefault();
+                deleteAdministrator($scope.activeAdministrator.id, function () {
+                    $('.ipsDeleteModal').modal('hide');
+
+                    var index = $scope.administrators.indexOf($scope.activeAdministrator);
+                    $scope.activeAdministrator = null;
+                    if (index > -1) {
+                        $scope.administrators.splice(index, 1);
+                    }
+                    $scope.$apply();
+                });
+            });
+        }
+
 
         var addAdministrator = function (username, email, password) {
             var data = {
@@ -143,26 +162,26 @@ var ipAdministratorsController = null;
             });
         }
 
-        var deleteAdministrator = function (pageId, successCallback) {
-//            var data = {
-//                aa: 'Pages.deletePage',
-//                pageId: pageId,
-//                securityToken: ip.securityToken
-//            };
-//
-//            $.ajax({
-//                type: 'POST',
-//                url: ip.baseUrl,
-//                data: data,
-//                context: this,
-//                success: successCallback,
-//                error: function (response) {
-//                    if (ip.developmentEnvironment || ip.debugMode) {
-//                        alert('Server response: ' + response.responseText);
-//                    }
-//                },
-//                dataType: 'json'
-//            });
+        var deleteAdministrator = function (id, successCallback) {
+            var data = {
+                aa: 'Administrators.delete',
+                id: id,
+                securityToken: ip.securityToken
+            };
+
+            $.ajax({
+                type: 'POST',
+                url: ip.baseUrl,
+                data: data,
+                context: this,
+                success: successCallback,
+                error: function (response) {
+                    if (ip.developmentEnvironment || ip.debugMode) {
+                        alert('Server response: ' + response.responseText);
+                    }
+                },
+                dataType: 'json'
+            });
         }
 
 
