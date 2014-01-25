f314c6e9e9b28e45a6a8d7e107904815f7f0be1a
Mangirdas Skripka <mangirdas@impresspages.org>
2014-01-25 23:48:41 +0200

    fix update bootstrap

--- phpunit/bootstrap.php
+++ phpunit/bootstrap.php
@@ -18,8 +18,8 @@ $autoloader->register(TEST_BASE_DIR);
 
 require_once TEST_CODEBASE_DIR . 'Ip/Config.php';
 
-////bootstrap IpUpdate library
-//require_once(TEST_CODEBASE_DIR . 'update/Library/Bootstrap.php');
-//$libraryBootstrap = new \IpUpdate\Library\Bootstrap();
-//$libraryBootstrap->run();
-////end IpUpdate bootstrap
+//bootstrap IpUpdate library
+require_once(TEST_CODEBASE_DIR . 'update/Library/Bootstrap.php');
+$libraryBootstrap = new \IpUpdate\Library\Bootstrap();
+$libraryBootstrap->run();
+//end IpUpdate bootstrap
