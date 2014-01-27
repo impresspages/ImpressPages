5a58d852b5aa85d34e9c421519e5284e990592be
Audrius Jankauskas <audrius@impresspages.org>
2014-01-27 14:01:01 +0200

    fixing login

--- Ip/Internal/Admin/view/passwordResetInfo.php
+++ Ip/Internal/Admin/view/passwordResetInfo.php
@@ -1,6 +1,6 @@
 <div class="loginTitle">
-    <h1><?php _e('Password reset', 'ipAdmin') ?></h1>
+    <h1><?php _e('Password reset', 'ipAdmin'); ?></h1>
 </div>
-<div class="ip">
+<div class="ip loginContent">
     <p><?php _e('You will receive an e-mail with password reset link. Please follow the link and enter a new password.', 'ipAdmin'); ?></p>
 </div>
--- Ip/Internal/Admin/view/passwordResetSuccess.php
+++ Ip/Internal/Admin/view/passwordResetSuccess.php
@@ -1,7 +1,7 @@
 <div class="loginTitle">
-    <h1><?php _e('Password reset', 'ipAdmin') ?></h1>
+    <h1><?php _e('Password reset', 'ipAdmin'); ?></h1>
 </div>
-<div class="ip">
-    <p><?php _e('Your password has been changed.', 'ipAdmin') ?></p>
-    <a href="<?php echo ipFileUrl('admin'); ?>"><?php echo _e('Login', 'ipAdmin'); ?></a>
+<div class="ip loginContent">
+    <p><?php _e('Your password has been changed.', 'ipAdmin'); ?></p>
+    <p><a href="<?php echo ipFileUrl('admin'); ?>"><?php echo _e('Login', 'ipAdmin'); ?></a></p>
 </div>
