d23adf2fc0c81a377af6c290f2fa4b457eef1321
Mangirdas Skripka <mangirdas@impresspages.org>
2014-02-09 22:07:54 +0200

    display two plugins of theme in design tab.

---
+++ Theme/Blank/Plugin/TestTheme2/Event.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Plugin\Test;
+
+
+class Event
+{
+//    public static function ipUrlChanged($data){
+//        var_dump($data);exit;
+//    }
+
+//    public static function ipCronExecute($info)
+//    {
+//        var_dump($info);
+////        if ($info['firstTimeThisDay'] || $info['test']) {
+////            Model::deleteUnusedWidgets();
+////        }
+//    }
+
+
+    public static function ipInit()
+    {//var_dump( $_SERVER);exit;
+        if (ipRequest()->getQuery('testHmvc')) {
+            $request = new \Ip\Request();
+            $request->setQuery(array('pa' => 'Test.testHmvc'));
+
+            echo ipApplication()->handleRequest($request)->getContent(); exit;
+
+        }
+    }
+
+}
