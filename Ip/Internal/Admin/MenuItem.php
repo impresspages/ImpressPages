350c4d6fd0e28a6fcf95c3bd2aa013f03bebecd2
Audrius Jankauskas <audrius@impresspages.org>
2014-02-11 09:59:43 +0200

    New Admin navbar based on Bootstrap

---
+++ Ip/Internal/Admin/MenuItem.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @package ImpressPages
+ *
+ *
+ */
+
+namespace Ip\Internal\Admin;
+
+
+class MenuItem extends \Ip\Menu\Item
+{
+    /**
+     * @var string
+     */
+    protected $icon;
+
+    public function getIcon(){
+        return $this->icon;
+    }
+
+    public function setIcon($icon){
+        $this->icon = $icon;
+    }
+}
