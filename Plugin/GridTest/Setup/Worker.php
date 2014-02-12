875afc20ad67bb08f61e46c33f80c0116e938cef
Mangirdas Skripka <mangirdas@impresspages.org>
2014-02-12 15:43:00 +0200

    fix grid sort

---
+++ Plugin/GridTest/Setup/Worker.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Plugin\GridTest\Setup;
+
+class Worker extends \Ip\SetupWorker{
+
+    public function activate()
+    {
+        $table = ipTable('grid_test');
+        ipDb()->execute("
+CREATE TABLE IF NOT EXISTS $table (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `gridOrder` int(11) NOT NULL,
+  `message` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM;
+        ");
+    }
+
+    public function deactivate()
+    {
+        $table = ipTable('grid_test');
+        ipDb()->execute("
+DROP TABLE $table;
+        ");
+    }
+
+    public function remove()
+    {
+
+    }
+
+
+}
