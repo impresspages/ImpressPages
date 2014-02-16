3ea5f672a0e6632411f8b5128d661a001ae2d2a8
Mangirdas Skripka <mangirdas@impresspages.org>
2014-02-16 22:26:52 +0200

    restore blank config.php file

--- config.php
+++ config.php
@@ -1,20 +1,4 @@
 <?php
 
-/**
- * @package ImpressPages
- */
-
- return array(
-    'SESSION_NAME' => 'ses881187672', // prevents session conflict when two sites runs on the same server
-    'DEVELOPMENT_ENVIRONMENT' => 1, // displays error and debug information. Change to 0 before deployment to production server
-    'ERRORS_SHOW' => 1, // 0 if you don't wish to display errors on the page
-    'TIMEZONE' => 'Europe/Helsinki', // PHP 5 requires timezone to be set.
-    'db' => array (
-        'hostname' => 'localhost',
-        'username' => 'root',
-        'password' => 'maskas',
-        'tablePrefix' => 'ip_',
-        'database' => '4.x',
-        'charset' => 'utf8',
-      ), // Database configuration
-);
+ if(!isset($_GET['install']))
+    header("location: install/?install=1");
