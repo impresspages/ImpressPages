d8e0a565b239909eca7f8bcb641023103900af83
Mangirdas Skripka <mangirdas@impresspages.org>
2014-02-24 18:02:34 +0200

    finetune antular flick

--- Ip/Internal/Administrators/view/deleteModal.php
+++ Ip/Internal/Administrators/view/deleteModal.php
@@ -1,4 +1,4 @@
-<div class="ipsDeleteModal modal fade">
+<div class="ipsDeleteModal modal fade"  ng-cloak>
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
