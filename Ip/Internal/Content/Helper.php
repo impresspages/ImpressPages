b8cceed0af5acb55fafedc94e72326c602872f41
Algimantas <algimantas@impresspages.org>
2014-02-25 14:27:01 +0200

    Removed zones

--- Ip/Internal/Content/Helper.php
+++ Ip/Internal/Content/Helper.php
@@ -25,29 +25,4 @@ class Helper
         $language = new \Ip\Language($data['id'], $data['code'], $data['url'], $data['title'], $data['abbreviation'], $data['isVisible'], $data['textDirection']);
         return $language;
     }
-
-    public static function createZone($zoneData)
-    {
-        if ($zoneData['associated_module']) {
-            $class = '\\Ip\\Internal\\' . $zoneData['associated_module'] . '\\Zone';
-            if (class_exists($class)) {
-                $zoneObject = new $class($zoneData);
-            } else {
-                $class = '\\Plugin\\' . $zoneData['associated_module'] . '\\Zone';
-                $zoneObject = new $class($zoneData);
-            }
-        } else {
-            $zoneObject = new \Ip\DefaultZone($zoneData);
-        }
-
-        $zoneObject->setId($zoneData['id']);
-        $zoneObject->setName($zoneData['name']);
-        $zoneObject->setLayout($zoneData['template']);
-        $zoneObject->setTitle($zoneData['title']);
-        $zoneObject->setUrl($zoneData['url']);
-        $zoneObject->setKeywords($zoneData['keywords']);
-        $zoneObject->setDescription($zoneData['description']);
-        $zoneObject->setAssociatedModule($zoneData['associated_module']);
-        return $zoneObject;
-    }
 }
