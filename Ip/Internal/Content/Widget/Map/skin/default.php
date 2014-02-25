6e719db07ebbd5cd591d82eeb6f01673ae8a6a2d
Mangirdas Skripka <mangirdas@impresspages.org>
2014-02-25 17:17:11 +0200

    map widget marker

--- Ip/Internal/Content/Widget/Map/skin/default.php
+++ Ip/Internal/Content/Widget/Map/skin/default.php
@@ -5,11 +5,11 @@ data-lat="<?php echo esc($lat, 'attr'); ?>"
 data-lng="<?php echo esc($lng, 'attr'); ?>"
 
 <?php if (isset($markerlat)) { ?>
-    data-markerlat="<?php echo esc($markerlat, 'attr'); ?>
+    data-markerlat="<?php echo esc($markerlat, 'attr'); ?>"
 <?php } ?>
 
 <?php if (isset($markerlng)) { ?>
-    data-markerlng="<?php echo esc($markerlng, 'attr'); ?>
+    data-markerlng="<?php echo esc($markerlng, 'attr'); ?>"
 <?php } ?>
 
 style="height: <?php echo ($height); ?>; width: <?php echo ($width); ?>;"
