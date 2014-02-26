0d3e37f273a9e5609350d87685fdf3ff357f7f8f
Algimantas <algimantas@impresspages.org>
2014-02-26 14:53:31 +0200

    Change db

--- install/Plugin/Install/sql/migrations/2014-02-26-page-is-deleted-fields.sql
+++ install/Plugin/Install/sql/migrations/2014-02-26-page-is-deleted-fields.sql
@@ -1,6 +1,5 @@
 ALTER TABLE `ip_page`
 CHANGE `languageCode` `languageCode` varchar(8) COLLATE 'latin1_general_ci' NULL AFTER `id`,
-CHANGE `isVisible` `isPublished` tinyint(1) NOT NULL DEFAULT '0' AFTER `deletedAt`,
 ADD `isDeleted` tinyint(1) NOT NULL DEFAULT '0',
 ADD `deletedAt` timestamp NULL AFTER `updatedAt`,
 ADD INDEX `url` (`languageCode`, `urlPath`);
