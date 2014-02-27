9b52822388c78c05b2fc3d83c7bd562a3e6db5a7
Audrius Jankauskas <audrius@impresspages.org>
2014-02-27 15:34:32 +0200

    update default theme

--- Theme/Air/_header.php
+++ Theme/Air/_header.php
@@ -1,19 +1,12 @@
-<?php
-/**
- * This comment block is used just to make IDE suggestions to work
- * @var $this \Ip\View
- */
-?>
 <?php echo ipDoctypeDeclaration(); ?>
 <html<?php echo ipHtmlAttributes(); ?>>
 <head>
     <?php ipAddCss('assets/theme.css'); ?>
     <?php echo ipHead(); ?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<!--    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>-->
 </head>
 <body>
-<div class="theme clearfix">
+<div class="wrapper clearfix">
     <header class="clearfix col_12">
         <?php echo ipSlot('logo'); ?>
         <div class="right">
