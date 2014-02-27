81d84f2b93d216241cad8f256409a7cdc9db0f41
Audrius Jankauskas <audrius@impresspages.org>
2014-02-27 15:32:41 +0200

    fix title widget

--- Ip/Internal/Content/Widget/Title/skin/default.php
+++ Ip/Internal/Content/Widget/Title/skin/default.php
@@ -1,7 +1,9 @@
-<?php if (!empty($link) && !ipIsManagementState()) { ?>
-    <a href="<?php echo esc($link, 'attr') ?>" <?php echo !empty($blank) ? 'target="_blank"' : '' ?>
-<?php } ?>
-    <h<?php echo (int)$level ?> <?php echo isset($id) ? 'id="'.esc($id).'"': '' ?> class="ipwTitle"><?php echo !empty($title) ? esc($title) : '&nbsp;'; ?></h<?php echo (int)$level ?>>
-<?php if (!empty($link)) { ?>
-    </a>
-<?php } ?>
+<h<?php echo (int)$level; ?> class="_title"<?php echo isset($id) ? ' id="'.esc($id).'"': ''; ?>>
+    <?php if ($showLink) { ?>
+        <a href="<?php echo esc($link, 'attr'); ?>"<?php echo !empty($blank) ? ' target="_blank"' : ''; ?>>
+    <?php } ?>
+        <?php echo !empty($title) ? esc($title) : '&nbsp;'; ?>
+    <?php if ($showLink) { ?>
+        </a>
+    <?php } ?>
+</h<?php echo (int)$level; ?>>
