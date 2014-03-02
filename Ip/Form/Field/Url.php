d3796fb9fc493e02da29b0da7417427c889395db
Audrius Jankauskas <audrius@impresspages.org>
2014-03-02 15:29:19 +0200

    fixing input groups

--- Ip/Form/Field/Url.php
+++ Ip/Form/Field/Url.php
@@ -9,21 +9,26 @@ namespace Ip\Form\Field;
 
 use Ip\Form\Field;
 
-class Url extends Field{
+class Url extends Field
+{
 
-    public function render($doctype, $environment) {
+    public function render($doctype, $environment)
+    {
         return '
+<div class="input-group">
     <input '.$this->getAttributesStr($doctype).' class="form-control '.implode(' ',$this->getClasses()).'" name="'.htmlspecialchars($this->getName()).'" '.$this->getValidationAttributesStr($doctype).' type="text" value="'.htmlspecialchars($this->getValue()).'" />
     <span class="input-group-btn">
         <button class="ipsBrowse btn btn-default" type="button">' . __('Browse', 'ipAdmin') . '</button>
     </span>
+</div>
 ';
     }
 
     /**
     * CSS class that should be applied to surrounding element of this field. By default empty. Extending classes should specify their value.
     */
-    public function getTypeClass() {
+    public function getTypeClass()
+    {
         return 'url';
     }
 
