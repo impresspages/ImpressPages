492fef77a6a6289845392ac79143813871c66db7
Mangirdas Skripka <mangirdas@impresspages.org>
2014-03-02 12:16:12 +0200

    fix migration

--- migrations/2014-03-01-additional-page-fields.sql
+++ migrations/2014-03-01-additional-page-fields.sql
@@ -1,3 +1,3 @@
 ALTER TABLE  `ip_page` ADD  `redirectUrl` VARCHAR( 255 ) NULL DEFAULT NULL AFTER  `description` ,
-ADD  `isDeleted` BOOLEAN NOT NULL DEFAULT FALSE AFTER  `redirectUrl`,
+ADD  `isDisabled` BOOLEAN NOT NULL DEFAULT FALSE AFTER  `redirectUrl`,
 ADD  `isSecured` BOOLEAN NOT NULL DEFAULT FALSE AFTER  `isDeleted`
