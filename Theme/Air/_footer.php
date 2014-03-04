740e718d9b20fb90bbc75c7568accefbb0242e09
Audrius Jankauskas <audrius@impresspages.org>
2014-03-04 13:25:13 +0200

    cleanup

--- Theme/Air/_footer.php
+++ Theme/Air/_footer.php
@@ -1,6 +1,6 @@
 <footer class="clearfix">
     <div class="col_12">
-        <?php echo ipSlot('text', array('id' => 'themeName', 'tag' => 'div', 'default' => __('Theme "Air"', 'Air', false), 'class' => 'left')) ?>
+        <?php echo ipSlot('text', array('id' => 'themeName', 'tag' => 'div', 'default' => __('Theme "Air"', 'Air', false), 'class' => 'left')); ?>
         <div class="right">
             <?php echo sprintf(__('Drag & drop with %s', 'Air'), '<a href="http://www.impresspages.org">' . __('ImpressPages CMS', 'Air') . '</a>'); ?>
         </div>
