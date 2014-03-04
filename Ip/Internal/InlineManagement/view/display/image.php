760df38f7d02b6885d67f714353bb384a6801e68
Audrius Jankauskas <audrius@impresspages.org>
2014-03-04 13:12:32 +0200

    Inline image and text managemenet

--- Ip/Internal/InlineManagement/view/display/image.php
+++ Ip/Internal/InlineManagement/view/display/image.php
@@ -1,1 +1,6 @@
-<img class="<?php echo esc($cssClass) ?>" src="<?php echo esc($value) ?>" alt="" />
+<img
+    class="<?php echo esc($cssClass); ?>"
+    src="<?php echo esc($value); ?>"
+    style="<?php if(!empty($options['height'])) { echo 'height: '.$options['height'].'px;'; } ?><?php if(!empty($options['width'])) { echo 'width: '.$options['width'].'px;'; } ?>"
+    alt=""
+/>
--- Ip/Internal/InlineManagement/view/management/image.php
+++ Ip/Internal/InlineManagement/view/management/image.php
@@ -1,7 +1,10 @@
 <img
      data-key="<?php echo esc($key); ?>"
      data-defaultvalue="<?php echo esc($defaultValue); ?>"
-     data-options='<?php echo json_encode($options); ?>'
+     data-options='<?php echo json_encode($options); // single quotes around because json encodes with double quotes ?>'
      data-cssclass="<?php echo esc($cssClass); ?>"
-     class="ipModuleInlineManagement ipmImage<?php echo $empty ? ' ipmEmpty' : '' ?> <?php echo esc($cssClass); ?>"
-     src="<?php echo $empty ? ipFileUrl('Ip/Internal/InlineManagement/assets/empty.gif') : esc($value); ?>" alt="" />
+     class="ipsModuleInlineManagementImage ipModuleInlineManagement<?php echo $empty ? ' _empty' : ''; ?> <?php echo esc($cssClass); ?>"
+     src="<?php echo $empty ? esc($defaultValue) : esc($value); ?>"
+     alt=""
+     style="<?php if(!empty($options['height'])) { echo 'height: '.$options['height'].'px;'; } ?><?php if(!empty($options['width'])) { echo 'width: '.$options['width'].'px;'; } ?>"
+/>
