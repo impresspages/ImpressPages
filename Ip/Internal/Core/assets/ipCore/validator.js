a87e77054d0fa98e93c86f84f37ecc57d2398b00
Mangirdas Skripka <mangirdas@impresspages.org>
2014-03-06 20:21:34 +0200

    refactor validator

--- Ip/Internal/Core/assets/ipCore/validator.js
+++ Ip/Internal/Core/assets/ipCore/validator.js
@@ -1,53 +1,47 @@
 // defining global variables
 var validatorConfig = '';
-var ipValidator;
+
 
 (function($){
     "use strict";
 
-    ipValidator = new function() {
-        this.init = function() {
-            $.each(ipValidatorTranslations, function(key, value) {
-                if (validatorConfig === '') {
-                    validatorConfig = createConfig(key);
-                }
-                $.tools.validator.localize(key, value);
-            });
+    var createConfig = function(translationsKey) {
+        var config = {
+            'lang' : ip.languageUrl,
+            //'errorClass' : 'ipmControlError',
+            'messageClass' : 'hidden', //hide default jqueryTools absolutely positioned error message
+            //'position' : 'bottom left',
+            //'offset' : [-3, 0],
+            'onFail' : function(e, errors) {
+                $.each(errors, function() {
+                    var err = this;
+                    var $control = this.input;
+                    $control.parents('.form-group')
+                        .addClass('has-error')
+                        .find('.help-error').html(this.messages.join(' '));
+                    if (this.messages.join('') == '') {
+                        //hide error if no error text present
+                        $control.parents('.form-group').find('.help-error').hide()
+                    } else {
+                        $control.parents('.form-group').find('.help-error').show()
+                    }
+                });
+            },
+            'onSuccess' : function(e, valids) {
+                $.each(valids, function() {
+                    var $control = $(this);
+                    $control.parents('.form-group').removeClass('has-error');
+                });
+            }
         };
-
-        var createConfig = function(translationsKey) {
-            var config = {
-                'lang' : ip.languageUrl,
-                //'errorClass' : 'ipmControlError',
-                'messageClass' : 'hidden', //hide default jqueryTools absolutely positioned error message
-                //'position' : 'bottom left',
-                //'offset' : [-3, 0],
-                'onFail' : function(e, errors) {
-                    $.each(errors, function() {
-                        var err = this;
-                        var $control = this.input;
-                        $control.parents('.form-group')
-                            .addClass('has-error')
-                            .find('.help-error').html(this.messages.join(' '));
-                        if (this.messages.join('') == '') {
-                            //hide error if no error text present
-                            $control.parents('.form-group').find('.help-error').hide()
-                        } else {
-                            $control.parents('.form-group').find('.help-error').show()
-                        }
-                    });
-                },
-                'onSuccess' : function(e, valids) {
-                    $.each(valids, function() {
-                        var $control = $(this);
-                        $control.parents('.form-group').removeClass('has-error');
-                    });
-                }
-            };
-            return config;
-        };
-
+        return config;
     };
 
-    ipValidator.init();
+    $.each(ipValidatorTranslations, function(key, value) {
+        if (validatorConfig === '') {
+            validatorConfig = createConfig(key);
+        }
+        $.tools.validator.localize(key, value);
+    });
+
 })(jQuery);
