f8a7db99d6604862813540c785424816706b1271
Mangirdas Skripka <mangirdas@impresspages.org>
2014-03-07 11:15:48 +0200

    move design options reload next to other buttons

--- Ip/Internal/Design/view/optionsBox.php
+++ Ip/Internal/Design/view/optionsBox.php
@@ -6,13 +6,11 @@
                     <h4 class="modal-title"><i class="fa fa-cogs"></i> <?php _e('Theme options', 'ipAdmin'); ?></h4>
                 </div>
                 <div class="_body ipsBody">
-                    <div class="alert alert-warning hidden ipsReload">
-                        <a href="#" class="ipsReloadButton"><?php _e('Preview window needs to be reloaded', 'ipAdmin'); ?></a>
-                    </div>
                     <?php echo $form->render(); ?>
                 </div>
                 <div class="modal-footer ipsActions">
                     <a href="#" class="btn btn-success btn-sm ipsSave"><?php _e('Save', 'ipAdmin'); ?></a>
+                    <a href="#" class="btn btn-default btn-sm ipsReloadButton hidden "><?php _e('Reload preview', 'ipAdmin'); ?></a>
                     <a href="#" class="btn btn-default btn-sm ipsDefault"><?php _e('Preview defaults', 'ipAdmin'); ?></a>
                     <a href="#" class="btn btn-default btn-sm ipsCancel"><?php _e('Cancel', 'ipAdmin'); ?></a>
                 </div>
