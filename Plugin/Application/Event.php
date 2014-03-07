c54ecc358034c52ed5fe528653fcb2b9b0379f1c
Mangirdas Skripka <mangirdas@impresspages.org>
2014-03-07 08:55:27 +0200

    fine tune application plugin

--- Plugin/Application/Event.php
+++ Plugin/Application/Event.php
@@ -7,7 +7,9 @@ class Event
 {
     public static function ipBeforeController()
     {
-
+        //Add CSS, JS, set block content
+        //ipAddCss('assets/application.css');
+        //ipAddJs('assets/application.js');
     }
 
 }
--- Plugin/Application/PublicController.php
+++ Plugin/Application/PublicController.php
@@ -6,14 +6,24 @@ namespace Plugin\Application;
 
 class PublicController extends \Ip\Controller
 {
+    /**
+     * Visit example.com/?pa=Application.hello to view the response
+     * Or visit example.com/hello (see Job.php)
+     * @return \Ip\View
+     */
     public function hello()
     {
         // You can remove this line
         ipAddJs('assets/application.js');
+        ipAddCss('assets/application.css');
+
 
         $data = array(
             'weekday' => date('l')
         );
+
+        //change the layout if you like
+        //ipSetLayout('home.php');
 
         return ipView('view/hello.php', $data);
     }
