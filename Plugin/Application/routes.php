96e66cd0048d0128c7cc451101e73594f6eeeccb
Algimantas <algimantas@impresspages.org>
2014-03-10 16:26:12 +0200

    Route url generation

--- Plugin/Application/routes.php
+++ Plugin/Application/routes.php
@@ -15,8 +15,14 @@ $routes['blog/archive/{month}']= array(
     'name' => 'blog.archive',
 );
 
-
-
+$routes['counter{/number}'] = array(
+    'name' => 'counter',
+    'action' => function ($number = 1) {
+        $content = '<h2>You have clicked counter ' . esc($number) . ' times</h2>';
+        $content .= '<p><a href="' . ipRouteUrl('counter', array('number' => $number + 1)) . '">Click!</a></p>';
+        return $content;
+    },
+);
 
 
 
