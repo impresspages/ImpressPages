f05f8b619d331685535506cd07512936578e1183
Mangirdas Skripka <mangirdas@impresspages.org>
2014-03-11 13:54:34 +0200

    remove old migrations

--- migrations/2014-02-26-page-is-deleted-fields.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `ip_page`
-CHANGE `languageCode` `languageCode` varchar(6) COLLATE 'latin1_general_ci' NULL AFTER `id`,
-ADD `isDeleted` tinyint(1) NOT NULL DEFAULT '0',
-ADD `deletedAt` timestamp NULL AFTER `updatedAt`,
-ADD INDEX `url` (`languageCode`, `urlPath`);
--- migrations/2014-04-10-dbVersion-variable.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO  `4.x`.`ip_storage` (
-`plugin` ,
-`key` ,
-`value`
-)
-VALUES (
-'Ip',  'dbVersion',  '1'
-);
