3564f34c0f3aaae59290b8cab67425d0f44f0138
Mangirdas Skripka <mangirdas@impresspages.org>
2014-03-11 13:30:16 +0200

    refactor update mechanics

---
+++ Ip/Internal/Update/Model.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package   ImpressPages
+ */
+
+
+
+namespace Ip\Internal\Update;
+
+
+
+class Model {
+    const DB_VERSION = 2;
+
+    public static function runMigrations()
+    {
+        $curDbVersion = ipStorage()->get('Ip', 'dbVersion');
+        for($i = $curDbVersion + 1; $i <= self::DB_VERSION; $i++) {
+            $migrationMethod = 'update_' . $i;
+            if (method_exists(__NAMESPACE__ . '\Migration', $migrationMethod)) {
+                Migration::$migrationMethod();
+            }
+            ipStorage()->set('Ip', 'dbVersion', $i);
+        }
+    }
+}
