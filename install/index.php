f44dd001579e2280991a456be0bfe1f0adb9ede2
Algimantas <algimantas@impresspages.org>
2014-03-12 10:24:55 +0200

    Autoformat

--- install/index.php
+++ install/index.php
@@ -4,69 +4,69 @@
  * @package ImpressPages
  */
 
-if((PHP_MAJOR_VERSION < 5) || (PHP_MAJOR_VERSION == 5 && PHP_MINOR_VERSION < 3)) {
-    echo 'Your PHP version is: '.PHP_MAJOR_VERSION.'.'.PHP_MINOR_VERSION.'. To run ImpressPages CMS you need PHP >= 5.3.*';
+if ((PHP_MAJOR_VERSION < 5) || (PHP_MAJOR_VERSION == 5 && PHP_MINOR_VERSION < 3)) {
+    echo 'Your PHP version is: ' . PHP_MAJOR_VERSION . '.' . PHP_MINOR_VERSION . '. To run ImpressPages CMS you need PHP >= 5.3.*';
     exit;
 }
 
 require_once(__DIR__ . '/../Ip/Application.php');
 
-    $application = new \Ip\Application(__DIR__ . '/config.php');
-    $application->init();
-    $options = array(
-        'skipErrorHandler' => true
-    );
-    $application->prepareEnvironment($options);
-    $options = array(
-        'skipInitEvents' => true,
-        'skipModuleInit' => true,
-        'translationsLanguageCode' => 'en'
-    );
+$application = new \Ip\Application(__DIR__ . '/config.php');
+$application->init();
+$options = array(
+    'skipErrorHandler' => true
+);
+$application->prepareEnvironment($options);
+$options = array(
+    'skipInitEvents' => true,
+    'skipModuleInit' => true,
+    'translationsLanguageCode' => 'en'
+);
 
-    if (isset($_REQUEST['lang'])) {
-        $_SESSION['installationLanguage'] = $_REQUEST['lang'];
-    }
+if (isset($_REQUEST['lang'])) {
+    $_SESSION['installationLanguage'] = $_REQUEST['lang'];
+}
 
-    if (isset($_SESSION['installationLanguage'])) {
-        $options['translationsLanguageCode'] = $_SESSION['installationLanguage'];
-    }
+if (isset($_SESSION['installationLanguage'])) {
+    $options['translationsLanguageCode'] = $_SESSION['installationLanguage'];
+}
 
-    // Because module init is skipped, we have to initialize translations manually
-    $translator = \Ip\ServiceLocator::translator();
-    $translator->setLocale($options['translationsLanguageCode']);
+// Because module init is skipped, we have to initialize translations manually
+$translator = \Ip\ServiceLocator::translator();
+$translator->setLocale($options['translationsLanguageCode']);
 
-    $trPluginDir = ipFile('Plugin/Install/translations/');
-    $trOverrideDir = ipFile('file/translations/override/');
-    $translator->addTranslationFilePattern('json', $trPluginDir,    'Install-%s.json', 'Install');
-    $translator->addTranslationFilePattern('json', $trOverrideDir,  'Install-%s.json', 'Install');
+$trPluginDir = ipFile('Plugin/Install/translations/');
+$trOverrideDir = ipFile('file/translations/override/');
+$translator->addTranslationFilePattern('json', $trPluginDir, 'Install-%s.json', 'Install');
+$translator->addTranslationFilePattern('json', $trOverrideDir, 'Install-%s.json', 'Install');
 
-    $request = new \Plugin\Install\Request();
-    $request->setQuery($_GET);
-    $request->setPost($_POST);
-    $request->setServer($_SERVER);
-    $request->setRequest($_REQUEST);
+$request = new \Plugin\Install\Request();
+$request->setQuery($_GET);
+$request->setPost($_POST);
+$request->setServer($_SERVER);
+$request->setRequest($_REQUEST);
 
-    \Ip\ServiceLocator::addRequest($request);
+\Ip\ServiceLocator::addRequest($request);
 
-    $language = new \Ip\Language(null, $options['translationsLanguageCode'], null, null, null, 0, 'ltr');
-    ipContent()->_setCurrentLanguage($language);
+$language = new \Ip\Language(null, $options['translationsLanguageCode'], null, null, null, 0, 'ltr');
+ipContent()->_setCurrentLanguage($language);
 
-    if ($request->isGet()) {
+if ($request->isGet()) {
+    $controller = new \Plugin\Install\PublicController();
+    $response = $controller->index();
+} elseif ($request->isPost()) {
+    $route = Ip\Internal\Core\Job::ipRouteAction_20(array('request' => $request));
+    if (!$route || $route['plugin'] != 'Install' || $route['controller'] != 'PublicController') {
+        $response = new \Ip\Response\PageNotFound();
+    } else {
         $controller = new \Plugin\Install\PublicController();
-        $response = $controller->index();
-    } elseif ($request->isPost()) {
-        $route = Ip\Internal\Core\Job::ipRouteAction_20(array('request' => $request));
-        if (!$route || $route['plugin'] != 'Install' || $route['controller'] != 'PublicController') {
-            $response = new \Ip\Response\PageNotFound();
-        } else {
-            $controller = new \Plugin\Install\PublicController();
-            $response = $controller->{$route['action']}();
-        }
+        $response = $controller->{$route['action']}();
     }
+}
 
 
-    \Ip\ServiceLocator::removeRequest();
+\Ip\ServiceLocator::removeRequest();
 
-    // $response = $application->handleRequest($request, $options);
-    $response->send();
+// $response = $application->handleRequest($request, $options);
+$response->send();
 
