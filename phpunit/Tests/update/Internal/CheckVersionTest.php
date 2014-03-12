97e3d738044984562d9dbbd28a55e3b0ca30c4e4
Mangirdas Skripka <mangirdas@impresspages.org>
2014-03-12 17:44:56 +0200

    set versions in a single place

--- phpunit/Tests/update/Internal/CheckVersionTest.php
+++ phpunit/Tests/update/Internal/CheckVersionTest.php
@@ -12,17 +12,20 @@ class CheckVersionTest extends \PhpUnit\GeneralTestCase
      */
     public function testVersionConstant()
     {
-        $code = file_get_contents(TEST_CODEBASE_DIR.'install/constants.php');
-
-        $position =  strpos($code, '\''.CURRENT_VERSION.'\'');
-        $this->assertNotEquals($position, FALSE, 'install/constants.php has no version string.');
-
-        $position =  strpos($code, '\''.CURRENT_DBVERSION.'\'');
-        $this->assertNotEquals($position, FALSE, 'install/constants.php has no dbversion string.');
+//        $code = file_get_contents(TEST_CODEBASE_DIR.'install/constants.php');
+//
+//        $position =  strpos($code, '\''.CURRENT_VERSION.'\'');
+//        $this->assertNotEquals($position, FALSE, 'install/constants.php has no version string.');
+//
+//        $position =  strpos($code, '\''.CURRENT_DBVERSION.'\'');
+//        $this->assertNotEquals($position, FALSE, 'install/constants.php has no dbversion string.');
 
         $code = file_get_contents(TEST_CODEBASE_DIR.'Ip/Application.php');
 
         $position =  strpos($code, '\''.CURRENT_VERSION.'\'');
+        $this->assertNotEquals($position, FALSE, 'Ip/Application.php has no version string.');
+
+        $position =  strpos($code, '\''.CURRENT_DBVERSION.'\'');
         $this->assertNotEquals($position, FALSE, 'Ip/Application.php has no version string.');
 
     }
