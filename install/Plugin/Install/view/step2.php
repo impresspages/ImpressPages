13d1d3c9eef1ff9e354b7cac4d9f718d88c197e5
Algimantas <algimantas@impresspages.org>
2014-03-12 17:38:37 +0200

    Removed cms

--- install/Plugin/Install/view/step2.php
+++ install/Plugin/Install/view/step2.php
@@ -1,6 +1,6 @@
 <h1><?php _e('ImpressPages Legal Notices', 'Install') ?></h1>
+<h2>ImpressPages</h2>
 <p>
-    ImpressPages - Content Management System<br/>
     Copyright <?php echo date("Y") ?> by <a href="http://www.impresspages.org">ImpressPages, UAB</a>
 </p>
 <p>This program is free software: you can redistribute it and/or modify it under the terms of the <a href="<?php echo ipFileUrl('license.html') ?>">GNU General Public License or MIT License</a>.</p>
