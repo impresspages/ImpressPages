6238947397b620b6af241947486040fe6ab716ff
Mangirdas Skripka <mangirdas@impresspages.org>
2014-03-13 08:29:32 +0200

    remove test plugins from Air theme

--- Theme/Air/home.php
+++ Theme/Air/home.php
@@ -1,5 +1,4 @@
 <?php echo ipView('_header.php')->render(); ?>
-<?php // todox: removed home blocks; loosing examples how front page can be built; 3 columns is now impossible to build ?>
     <div class="main col_12">
         <?php echo ipBlock('main')->exampleContent(' '); ?>
     </div>
