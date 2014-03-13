03e78ef40c493994e16ece49a72078c2ce23dcc8
Algimantas <algimantas@impresspages.org>
2014-03-13 12:00:54 +0200

    Cleanup

--- Ip/Internal/Pages/Event.php
+++ Ip/Internal/Pages/Event.php
@@ -8,11 +8,6 @@ namespace Ip\Internal\Pages;
 
 class Event
 {
-    public static function ipBeforeLanguageDeleted($data)
-    {
-        // TODOX #delete-language-pages
-    }
-
     public static function ipCronExecute($info)
     {
         if ($info['firstTimeThisDay'] || $info['test']) {
