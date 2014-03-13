ba17c33d46975c48a5d05719c90de0627921d90e
Mangirdas Skripka <mangirdas@impresspages.org>
2014-03-13 09:10:41 +0200

    remove AssetsRsync plugin

--- Plugin/AssetRsync/view/options.php
+++
@@ -1,8 +0,0 @@
-<button type="button" class="ipsSyncAssets btn btn-default navbar-btn"><?php _e('Synchronize assets', 'AssetRsync') ?></button>
-
-<div class="panel panel-default">
-    <div class="panel-heading"><?php _e('Options', 'AssetRsync') ?></div>
-    <div class="panel-body">
-        <?php echo $form->render(); ?>
-    </div>
-</div>
