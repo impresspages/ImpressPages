6d538650880c0d0e8b0d35c4a06d93cee6fba70d
Mangirdas Skripka <mangirdas@impresspages.org>
2014-03-14 14:23:31 +0200

    new video widget image

--- Ip/Internal/Content/Widget/Video/skin/default.php
+++ Ip/Internal/Content/Widget/Video/skin/default.php
@@ -1,6 +1,6 @@
 <div class="ipsContainer">
     <?php if (ipIsManagementState() && empty($videoHtml)) { ?>
-        <img style="max-width: 100%; cursor: pointer;" class="defaultImage" src="<?php echo ipFileUrl('Ip/Internal/Content/Widget/Video/assets/video.gif') ?>" /> <!-- //TODOX MOVE STYLE TO CSS #videocss -->
+        <img style="max-width: 100%; cursor: pointer;" class="defaultImage" src="<?php echo ipFileUrl('Ip/Internal/Content/Widget/Video/assets/video.svg') ?>" /> <!-- //TODOX MOVE STYLE TO CSS #videocss -->
     <?php } else { ?>
         <?php echo isset($videoHtml) ? $videoHtml : ''; ?>
     <?php } ?>
