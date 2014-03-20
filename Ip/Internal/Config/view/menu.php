7b83bd781a0a05ec3b1847a6ab4b38ffdc910115
Allan Laal <allan@permanent.ee>
2014-03-20 20:08:58 +0200

    ADD Slot::menu_80() Generate menu with custom ul ID and class. Fixes #345

--- Ip/Internal/Config/view/menu.php
+++ Ip/Internal/Config/view/menu.php
@@ -4,10 +4,23 @@
  * @var $items \Ip\Menu\Item[]
  * @var $this \Ip\View
  */
+
+$ul_id = '';
+if (isset($attributes['ul']['id']))
+{
+	$ul_id = 'id="'.$attributes['ul']['id'].'"';
+}
+
+$ul_class = "level$depth";
+if (isset($attributes['ul']['class']))
+{
+	$ul_class .= ' '.$attributes['ul']['class']; // append to level$depth
+}
+
 ?>
 <?php if (isset($items[0])){?>
     <?php $firstItem = $items[0]; ?>
-    <ul class="level<?php echo $depth; ?>"><?php
+    <ul <?php echo $ul_id; ?> class="<?php echo $ul_class; ?>"><?php
 foreach($items as $item) {
     echo ipView('Ip/Internal/Config/view/menuItem.php', array('menuItem' => $item, 'depth' => $depth))->render();
 } ?></ul>
