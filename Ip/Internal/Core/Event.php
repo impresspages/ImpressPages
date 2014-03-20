afd54146446dc115dfc2f3f4575bdc15cc32ade0
Mangirdas Skripka <mangirdas@impresspages.org>
2014-03-20 09:20:52 +0200

    remove scoped form js

--- Ip/Internal/Core/Event.php
+++ Ip/Internal/Core/Event.php
@@ -20,6 +20,8 @@ class Event
             ipAddJs('Ip/Internal/Core/assets/ipCore/form/color.js');
             ipAddJs('Ip/Internal/Core/assets/ipCore/form/file.js');
             ipAddJs('Ip/Internal/Core/assets/ipCore/form/richtext.js');
+            ipAddJs('Ip/Internal/Core/assets/ipCore/form/repositoryFile.js');
+            ipAddJs('Ip/Internal/Core/assets/ipCore/form/url.js');
             ipAddJs('Ip/Internal/Core/assets/ipCore/form.js');
 
             ipAddJs('Ip/Internal/Core/assets/ipCore/validator.js');
@@ -45,12 +47,6 @@ class Event
                 ipAddJs('Ip/Internal/Core/assets/admin/managementMode.js');
                 ipAddJs('Ip/Internal/Core/assets/admin/functions.js');
 
-                ipAddJs('Ip/Internal/Core/assets/admin/form/repositoryFile.js');
-                ipAddJs('Ip/Internal/Core/assets/admin/form/color.js');
-                ipAddJs('Ip/Internal/Core/assets/admin/form/file.js');
-                ipAddJs('Ip/Internal/Core/assets/admin/form/richtext.js');
-                ipAddJs('Ip/Internal/Core/assets/admin/form/url.js');
-                ipAddJs('Ip/Internal/Core/assets/admin/form.js');
 
                 ipAddJs('Ip/Internal/Core/assets/admin/validator.js');
                 ipAddJs('Ip/Internal/Core/assets/admin/bootstrap.js');
