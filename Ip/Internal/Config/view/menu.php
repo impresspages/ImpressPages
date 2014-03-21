75460b1f9fa1a5f4757dbd8ea58661758ad7c662
Mangirdas Skripka <mangirdas@impresspages.org>
2014-03-21 09:49:49 +0200

    add attributes to menu and language slots

--- Ip/Internal/Config/view/menu.php
+++ Ip/Internal/Config/view/menu.php
@@ -4,23 +4,10 @@
  * @var $items \Ip\Menu\Item[]
  * @var $this \Ip\View
  */
-
-$ul_id = '';
-if (isset($attributes['ul']['id']))
-{
-	$ul_id = 'id="'.$attributes['ul']['id'].'"';
-}
-
-$ul_class = "level$depth";
-if (isset($attributes['ul']['class']))
-{
-	$ul_class .= ' '.$attributes['ul']['class']; // append to level$depth
-}
-
 ?>
 <?php if (isset($items[0])){?>
     <?php $firstItem = $items[0]; ?>
-    <ul <?php echo $ul_id; ?> class="<?php echo $ul_class; ?>"><?php
+    <ul <?php echo $attributesStr; ?>><?php
 foreach($items as $item) {
     echo ipView('Ip/Internal/Config/view/menuItem.php', array('menuItem' => $item, 'depth' => $depth))->render();
 } ?></ul>
