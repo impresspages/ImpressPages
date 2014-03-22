1c6ac841977671093d215a9befa25d97ac18a1a2
Mangirdas Skripka <mangirdas@impresspages.org>
2014-03-22 12:49:50 +0200

    remove missing variable

--- Ip/Internal/Config/view/languages.php
+++ Ip/Internal/Config/view/languages.php
@@ -9,7 +9,7 @@
         <?php /** @var $language \Ip\Language */?>
         <?php if (!$language->isVisible()) { continue; }?>
         <?php $actClass = ($language->isCurrent()) ? ' class="current"' : ''; ?>
-        <li <?php echo $li_class_prepend.$actClass ?>>
+        <li <?php echo $actClass ?>>
             <a title="<?php echo esc($language->getTitle()) ?>" href="<?php echo $language->getLink() ?>">
                 <?php echo esc($language->getAbbreviation())?>
             </a>
