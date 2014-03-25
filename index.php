83b738ab1ddc99e50e490260d8969cb874346565
Algimantas <algimantas@impresspages.org>
2014-03-25 15:43:00 +0200

    Refactoring

--- index.php
+++ index.php
@@ -5,21 +5,11 @@
  */
 
 
-if((PHP_MAJOR_VERSION < 5) || (PHP_MAJOR_VERSION == 5 && PHP_MINOR_VERSION < 3)) {
+if ((PHP_MAJOR_VERSION < 5) || (PHP_MAJOR_VERSION == 5 && PHP_MINOR_VERSION < 3))
+{
     echo 'Your PHP version is: '.PHP_MAJOR_VERSION.'.'.PHP_MINOR_VERSION.'. To run ImpressPages you need PHP >= 5.3.3';
     exit;
 }
 
-
-require_once('Ip/Application.php');
-
-try {
-    $application = new \Ip\Application(__DIR__ . '/config.php');
-    $application->init();
-    $application->run();
-} catch (\Exception $e) {
-    if (isset($log)) {
-        $log->log('System', 'Exception caught', $e->getMessage().' in '.$e->getFile().':'.$e->getLine());
-    }
-    throw $e;
-}
+$configFilename = __DIR__ . '/config.php';
+require_once 'Ip/script/run.php';
