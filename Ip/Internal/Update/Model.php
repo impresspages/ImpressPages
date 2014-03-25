7fdb55f9ba8342f831bfae457ce89656dbc3e2bb
Mangirdas Skripka <mangirdas@impresspages.org>
2014-03-25 13:35:51 +0200

    update to 4.0

--- Ip/Internal/Update/Model.php
+++ Ip/Internal/Update/Model.php
@@ -17,7 +17,7 @@ class Model {
      */
     public static function getDbVersion()
     {
-        return 8; //CHANGE_ON_VERSION_UPDATE
+        return 9; //CHANGE_ON_VERSION_UPDATE
     }
 
     public static function migrationsAvailable()
