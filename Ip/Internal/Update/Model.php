a103928d8c878c4e33c4b9721d4c10c053f1d2b6
Mangirdas Skripka <mangirdas@impresspages.org>
2014-03-26 15:57:32 +0200

    add updatedAt column

--- Ip/Internal/Update/Model.php
+++ Ip/Internal/Update/Model.php
@@ -17,7 +17,7 @@ class Model {
      */
     public static function getDbVersion()
     {
-        return 14; //CHANGE_ON_VERSION_UPDATE
+        return 16; //CHANGE_ON_VERSION_UPDATE
     }
 
     public static function migrationsAvailable()
