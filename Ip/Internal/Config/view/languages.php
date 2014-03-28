03ec9fd291796645c65f5d8e25b2472c18c5318f
Audrius Jankauskas <audrius@impresspages.org>
2014-03-28 11:46:25 +0200

    CSS class rename 'current' -> 'active' for languages

--- Ip/Internal/Config/view/languages.php
+++ Ip/Internal/Config/view/languages.php
@@ -8,10 +8,10 @@
     <?php foreach ($languages as $key => $language) { ?>
         <?php /** @var $language \Ip\Language */?>
         <?php if (!$language->isVisible()) { continue; }?>
-        <?php $actClass = ($language->isCurrent()) ? ' class="current"' : ''; ?>
-        <li <?php echo $actClass ?>>
-            <a title="<?php echo esc($language->getTitle()) ?>" href="<?php echo $language->getLink() ?>">
-                <?php echo esc($language->getAbbreviation())?>
+        <?php $activeClass = ($language->isCurrent()) ? ' class="active"' : ''; ?>
+        <li <?php echo $activeClass; ?>>
+            <a title="<?php echo esc($language->getTitle()); ?>" href="<?php echo $language->getLink(); ?>">
+                <?php echo esc($language->getAbbreviation()); ?>
             </a>
         </li>
     <?php } ?>
