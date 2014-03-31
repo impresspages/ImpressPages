2d0a5be9fd33f7a01d0f6e836bd4f625ee1e4c13
Mangirdas Skripka <mangirdas@impresspages.org>
2014-03-31 17:34:26 +0300

    display header link title when needed

--- Ip/Internal/Content/Widget/Heading/Controller.php
+++ Ip/Internal/Content/Widget/Heading/Controller.php
@@ -49,8 +49,10 @@ class Controller extends \Ip\WidgetController{
     public function generateHtml($revisionId, $widgetId, $data, $skin)
     {
         $data['showLink'] = false;
-        if (!empty($data['link']) && !preg_match('/^((http|https):\/\/)/i', $data['link'])) {
-            $data['link'] = 'http://' . $data['link'];
+        if (!empty($data['link'])) {
+            if (!preg_match('/^((http|https):\/\/)/i', $data['link'])) {
+                $data['link'] = 'http://' . $data['link'];
+            }
 
             // hiding link in administration
             if (!ipIsManagementState()) {
