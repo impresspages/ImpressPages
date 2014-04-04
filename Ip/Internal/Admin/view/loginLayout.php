dd8479ebe74ed3a39bf63afecab1ca114c9af55d
gergo85 <gergo85@gmail.com>
2014-04-04 17:37:09 +0200

    Update loginLayout.php

--- Ip/Internal/Admin/view/loginLayout.php
+++ Ip/Internal/Admin/view/loginLayout.php
@@ -1,21 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="UTF-8">
     <title>ImpressPages</title>
+    <meta charset="UTF-8">
     <link rel="stylesheet" href="<?php echo ipFileUrl('Ip/Internal/Core/assets/admin/admin.css'); ?>">
     <link rel="stylesheet" href="<?php echo ipFileUrl('Ip/Internal/Admin/assets/login.css'); ?>">
     <link rel="shortcut icon" href="<?php echo ipFileUrl('favicon.ico'); ?>">
 </head>
 <body>
 
-
-<a href="http://www.impresspages.org/" class="logo" target="_blank"><img src="<?php echo ipFileUrl('Ip/Internal/Admin/assets/img/logo.png'); ?>"></a>
+<a href="http://www.impresspages.org/" target="_blank" class="logo"><img src="<?php echo ipFileUrl('Ip/Internal/Admin/assets/img/logo.png'); ?>"></a>
 <div class="verticalAlign"></div>
 <div class="login">
     <?php echo $content; ?>
 </div>
-<div class="loginFooter">Copyright 2009-<?php echo date("Y"); ?> by <a href="http://www.impresspages.org/">ImpressPages, UAB</a></div>
+<div class="loginFooter">Copyright 2009-<?php echo date("Y"); ?> by <a href="http://www.impresspages.org/" target="_blank">ImpressPages, UAB</a></div>
 
 <?php echo ipJs(); ?>
 </body>
