09501b27913276c86b8a95125a65e6a240b75660
Mangirdas Skripka <mangirdas@impresspages.org>
2014-04-09 10:26:23 +0300

    update to 4.1.0

--- Ip/Internal/Update/Migration.php
+++ Ip/Internal/Update/Migration.php
@@ -12,6 +12,13 @@ class Migration {
 
     //CHANGE_ON_VERSION_UPDATE
 
+    public static function update_35()
+    {
+        ipStorage()->set('Ip', 'cacheVersion', ipStorage()->get('Ip', 'cacheVersion', 1) + 1);
+        ipDb()->update('storage', array('value' => '"4.1.0"'), array('key' => 'version', 'plugin' => 'Ip'));
+    }
+
+
     public static function update_34()
     {
         ipStorage()->set('Ip', 'cacheVersion', ipStorage()->get('Ip', 'cacheVersion', 1) + 1);
