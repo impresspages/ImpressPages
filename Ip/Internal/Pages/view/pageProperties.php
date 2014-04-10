aad3479d6041babda68730dfe03675c67f49202d
gergo85 <gergo85@gmail.com>
2014-04-10 21:44:50 +0200

    Add icon to Save buttons

--- Ip/Internal/Pages/view/pageProperties.php
+++ Ip/Internal/Pages/view/pageProperties.php
@@ -5,7 +5,7 @@
     <button class="ipsDelete btn btn-danger pull-right" role="button"><?php _e('Delete', 'Ip-admin'); ?><i class="fa fa-fw fa-trash-o"></i></button>
     <span class="pull-right">&nbsp;</span>
     <button class="ipsEdit btn btn-new pull-right" role="button"><?php _e('Edit Content', 'Ip-admin'); ?> <i class="fa fa-fw fa-edit"></i></button>
-    <button class="ipsSave btn btn-default" role="button"><?php _e('Save', 'Ip-admin'); ?></button>
+    <button class="ipsSave btn btn-default" role="button"><?php _e('Save', 'Ip-admin'); ?> <i class="fa fa-fw fa-check"></i></button>
 </div>
 <?php echo $form->render(); ?>
-<button class="ipsSave btn btn-default" role="button"><?php _e('Save', 'Ip-admin'); ?></button>
+<button class="ipsSave btn btn-default" role="button"><?php _e('Save', 'Ip-admin'); ?> <i class="fa fa-fw fa-check"></i></button>
