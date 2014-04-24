58f7614f003e973fffea3967424f4761ec2d4ba2
Mangirdas Skripka <mangirdas@impresspages.org>
2014-04-24 16:32:30 +0300

    fix update tests

---
+++ phpunit/Tests/Update/Update4_0_10Test.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Update;
+
+/**
+ * @group ignoreOnTravis
+ * @group Selenium
+ */
+
+class Update4_0_10Test extends \PhpUnit\Helper\MinkTestCase
+{
+
+    /**
+     * @group Selenium
+     */
+    public function testUpdateFrom4_0_8()
+    {
+        $session = $this->session();
+
+        // install fresh copy of ImpressPages:
+        $installation = new \PhpUnit\Helper\Installation('4.0.10'); //development version
+        $installation->setDefaultConfig(array('testMode' => 1));
+        $installation->install();
+
+
+
+        $adminHelper = new \PhpUnit\Helper\User\Admin($session, $installation);
+
+        $adminHelper->login();
+
+        $this->session()->executeScript("$('.ipsAdminMenuBlock').removeClass('hidden');");
+        $this->find('.ipsAdminMenuBlockContainer a[title=System]')->click();
+
+        $this->find('.ipsStartUpdate')->click();
+
+        $this->waitForElementPresent('p.alert-success');
+        $versionSpan = $this->find('div.page-header small');
+        $this->assertEquals(\Ip\Application::getVersion(), $versionSpan->getText());
+
+
+    }
+
+}
