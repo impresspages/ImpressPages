6d9cae7af2b3a4dcc49fef1e40f6edc151549465
Mangirdas Skripka <mangirdas@impresspages.org>
2014-05-05 21:08:02 +0300

    update to 4.0.13

--- Ip/Application.php
+++ Ip/Application.php
@@ -35,7 +35,7 @@ class Application
      */
     public static function getVersion()
     {
-        return '4.0.12'; //CHANGE_ON_VERSION_UPDATE
+        return '4.0.13'; //CHANGE_ON_VERSION_UPDATE
     }
 
 
