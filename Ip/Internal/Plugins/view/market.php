6eab7905979b521efa842e38195527c3a0c544eb
Algimantas <algimantas@impresspages.org>
2014-05-15 11:51:52 +0300

    Show Plugins

--- Ip/Internal/Plugins/view/market.php
+++ Ip/Internal/Plugins/view/market.php
@@ -1,17 +1,8 @@
 <div class="ipModulePlugins ipsModulePlugins">
-    <div class="_outer ipsModulePluginsContainer">
-        <div class="_container _plugins ipsPlugins">
-            <div class="_actions">
-<!--                <a href="#" class="btn btn-new"><i class="fa fa-plus"></i> --><?php //_e('Add', 'Ip-admin'); ?><!--</a>-->
-            </div>
-            <!-- Plugin market -->
-            AHOI
-        </div>
-    </div>
 
     <div class="_popup ipsPluginMarketPopup hidden">
         <div class="_container" id="ipsModulePluginMarketContainer" data-marketurl="<?php echo esc($marketUrl); ?>">
-            <button type="button" class="btn btn-default _back ipsPluginMarketPopupClose"><i class="fa fa-angle-double-left"></i> <?php _e('Back to Design', 'Ip-admin'); ?></button>
+            <button type="button" class="btn btn-default _back ipsPluginMarketPopupClose"><i class="fa fa-angle-double-left"></i> <?php _e('Back to Plugins', 'Ip-admin'); ?></button>
         </div>
     </div>
 
