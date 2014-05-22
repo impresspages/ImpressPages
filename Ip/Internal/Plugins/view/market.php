a13862f7a2f4959992b4c5175ad75dbfdf4f37ca
Algimantas <algimantas@impresspages.org>
2014-05-22 11:54:38 +0300

    Back to My Plugins button

--- Ip/Internal/Plugins/view/market.php
+++ Ip/Internal/Plugins/view/market.php
@@ -2,7 +2,9 @@
 
     <div class="_popup ipsPluginMarketPopup hidden">
         <div class="_container" id="ipsModulePluginMarketContainer" data-marketurl="<?php echo esc($marketUrl); ?>">
-            <button type="button" class="btn btn-default _back ipsPluginMarketPopupClose"><i class="fa fa-angle-double-left"></i> <?php _e('Back to Plugins', 'Ip-admin'); ?></button>
+            <button type="button" class="btn btn-default _back">
+                <a href="<?php echo ipActionUrl(array('aa' => 'Plugins.index')); ?>"><i class="fa fa-angle-double-left"></i> <?php _e('My Plugins', 'Ip-admin'); ?></a>
+            </button>
         </div>
     </div>
 
