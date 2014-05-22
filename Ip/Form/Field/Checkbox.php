f8d24a246ab5877cc61f379cd41682b919689e8c
gergo85 <gergo85@gmail.com>
2014-05-22 10:33:43 +0200

    Code cleanup

--- Ip/Form/Field/Checkbox.php
+++ Ip/Form/Field/Checkbox.php
@@ -1,6 +1,5 @@
 <?php
-/**
-<?php
+
 /**
  * @package ImpressPages
  *
@@ -8,23 +7,29 @@
 
 namespace Ip\Form\Field;
 
-
 use Ip\Form\Field;
 
-class Checkbox extends Field{
 
-    protected $checked = FALSE;
+class Checkbox extends Field
+{
+
+    protected $checked = false;
     protected $text = null;
     protected $postValue = null;
 
+    /**
+     * Constructor
+     *
+     * @param array $options
+     */
     public function __construct($options = array()) {
         if (isset($options['checked'])) {
             $this->checked = $options['checked'];
         } else {
-            if(isset($options['value']) && ($options['value'] === TRUE || $options['value'] === 1)) {
-                $this->checked = TRUE;
+            if(isset($options['value']) && ($options['value'] === true || $options['value'] === 1)) {
+                $this->checked = true;
             } else {
-                $this->checked = FALSE;
+                $this->checked = false;
             }
         }
         if (isset($options['value']) && $options['value']) {
@@ -40,16 +45,23 @@ class Checkbox extends Field{
         parent::__construct($options);
     }
 
+    /**
+     * Render field
+     *
+     * @param string $doctype
+     * @param $environment
+     * @return string
+     */
     public function render($doctype, $environment) {
         $answer = '';
         if ($this->getChecked()) {
-            $attributes = 'checked="checked"';
+            $attributes = ' checked="checked"';
         } else {
             $attributes = '';
         }
 
         if ($this->getPostValue() !== null) {
-            $value = ' value="'.htmlspecialchars($this->getPostValue(), ENT_QUOTES).'" ';
+            $value = ' value="' . htmlspecialchars($this->getPostValue(), ENT_QUOTES) . '"';
         } else {
             $value = '';
         }
@@ -57,8 +69,8 @@ class Checkbox extends Field{
         $answer .= '
         <div class="checkbox">
             <label>
-                <input '.$this->getAttributesStr($doctype).' class="'.implode(' ',$this->getClasses()).'" name="'.htmlspecialchars($this->getName()).'" type="checkbox" '.$attributes.'  '.$this->getValidationAttributesStr($doctype).' '.$value.' />
-                '.$this->getText().'
+                <input ' . $this->getAttributesStr($doctype) . ' class="' . implode(' ', $this->getClasses()) . '" name="' . htmlspecialchars($this->getName()) . '" type="checkbox"' . $attributes . ' ' . $this->getValidationAttributesStr($doctype) . $value . ' />
+                ' . $this->getText() . '
             </label>
         </div>
         ';
@@ -66,31 +78,67 @@ class Checkbox extends Field{
         return $answer;
     }
 
+    /**
+     * Set checked option
+     *
+     * @param int| $checked
+     */
     public function setChecked($checked) {
         $this->checked = $checked;
     }
 
+    /**
+     * Get checked option
+     *
+     * @return bool
+     */
     public function getChecked() {
         return $this->checked;
     }
 
+    /**
+     * Set post value
+     *
+     * @param string $postValue
+     */
     public function setPostValue($postValue) {
         $this->postValue = $postValue;
     }
 
-
+    /**
+     * Get post value
+     *
+     * @return string
+     */
     public function getPostValue() {
         return $this->postValue;
     }
 
+    /**
+     * Set text
+     *
+     * @param string $text
+     */
     public function setText($text) {
         $this->text = $text;
     }
 
+    /**
+     * Get text
+     *
+     * @return string
+     */
     public function getText() {
         return $this->text;
     }
 
+    /**
+     * Get value as string
+     *
+     * @param string $values
+     * @param string $valueKey
+     * @return string
+     */
     public function getValueAsString($values, $valueKey) {
         if ($this->isChecked($values, $valueKey)) {
             return 'Yes';
@@ -99,6 +147,13 @@ class Checkbox extends Field{
         }
     }
 
+    /**
+     * Is checked?
+     *
+     * @param string $values
+     * @param string $valueKey
+     * @return int
+     */
     public function isChecked($values, $valueKey) {
         if (isset($values[$valueKey]) && $values[$valueKey]) {
             return 1;
@@ -108,12 +163,21 @@ class Checkbox extends Field{
     }
 
     /**
-    * CSS class that should be applied to surrounding element of this field. By default empty. Extending classes should specify their value.
-    */
+     * Get class type
+     *
+     * CSS class that should be applied to surrounding element of this field.
+     * By default empty. Extending classes should specify their value.
+     * @return string
+     */
     public function getTypeClass() {
         return 'confirm';
     }
 
+    /**
+     * Set value
+     *
+     * @param string $value
+     */
     public function setValue($value) {
         if ($value) {
             $this->setChecked(1);
