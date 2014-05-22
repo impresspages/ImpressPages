1dbe5d031ce4997aa7c01590a58ac48f4c9fccb6
gergo85 <gergo85@gmail.com>
2014-05-22 10:37:11 +0200

    Code cleanup

--- Ip/Form/Field/Text.php
+++ Ip/Form/Field/Text.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package ImpressPages
  *
@@ -6,18 +7,30 @@
 
 namespace Ip\Form\Field;
 
-
 use Ip\Form\Field;
 
-class Text extends Field{
 
+class Text extends Field
+{
+
+    /**
+     * Render field
+     *
+     * @param string $doctype
+     * @param $environment
+     * @return string
+     */
     public function render($doctype, $environment) {
-        return '<input '.$this->getAttributesStr($doctype).' class="form-control '.implode(' ',$this->getClasses()).'" name="'.htmlspecialchars($this->getName()).'" '.$this->getValidationAttributesStr($doctype).' type="text" value="'.htmlspecialchars($this->getValue()).'" />';
+        return '<input ' . $this->getAttributesStr($doctype) . ' class="form-control ' . implode(' ', $this->getClasses()) . '" name="' . htmlspecialchars($this->getName()) . '" ' . $this->getValidationAttributesStr($doctype) . ' type="text" value="' . htmlspecialchars($this->getValue()) . '" />';
     }
 
     /**
-    * CSS class that should be applied to surrounding element of this field. By default empty. Extending classes should specify their value.
-    */
+     * Get class type
+     *
+     * CSS class that should be applied to surrounding element of this field.
+     * By default empty. Extending classes should specify their value.
+     * @return string
+     */
     public function getTypeClass() {
         return 'text';
     }
