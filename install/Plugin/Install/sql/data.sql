b340a6f998a83ad36c5c5f574ec04dd55b2e8ce8
Mangirdas Skripka <mangirdas@impresspages.org>
2014-06-02 16:06:03 +0300

    dont update createdAt value on update

--- install/Plugin/Install/sql/data.sql
+++ install/Plugin/Install/sql/data.sql
@@ -41,7 +41,7 @@ INSERT INTO `ip_widget_order` (`widgetName`, `priority`) VALUES
 
 INSERT INTO `ip_storage` (`plugin`, `key`, `value`) VALUES
 ('Ip', 'version', '"4.0.17"'),  /* //CHANGE_ON_VERSION_UPDATE */
-('Ip', 'dbVersion', '45'), /* //CHANGE_ON_VERSION_UPDATE */
+('Ip', 'dbVersion', '46'), /* //CHANGE_ON_VERSION_UPDATE */
 ('Ip', 'theme', '"Air"'),
 ('Ip', 'cachedBaseUrl', ''),
 ('Ip', 'lastSystemMessageSent', ''),
