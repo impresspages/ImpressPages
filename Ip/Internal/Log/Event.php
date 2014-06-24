10efbd0a32871524a6ae0dc96e29d0eb013bb36b
gergo85 <gergo85@gmail.com>
2014-06-24 16:43:39 +0200

    Cleanup code

--- Ip/Internal/Log/Event.php
+++ Ip/Internal/Log/Event.php
@@ -1,17 +1,26 @@
 <?php
+
 /**
  * @package ImpressPages
  *
- *
  */
+
 namespace Ip\Internal\Log;
+
 
 class Event
 {
+
+    /**
+     * Cron execute
+     *
+     * @param array $info
+     */
     public static function ipCronExecute($info)
     {
         if ($info['firstTimeThisMonth'] || $info['test']) {
             Db::deleteOldLogs(ipGetOption('Log.existenceInDays', '90'));
         }
     }
+
 }
