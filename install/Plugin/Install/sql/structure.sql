132829599c0f6f17d68ab4f525c184d79f225d59
maskas <mangirdas@impresspages.org>
2014-07-25 10:11:47 +0300

    fix install

--- install/Plugin/Install/sql/structure.sql
+++ install/Plugin/Install/sql/structure.sql
@@ -110,6 +110,7 @@ CREATE TABLE `ip_repository_file` (
   `fileId` int(11) NOT NULL AUTO_INCREMENT,
   `filename` varchar(255) NOT NULL,
   `plugin` varchar(255) NOT NULL,
+  `baseDir` VARCHAR(255) NOT NULL,
   `instanceId` int(11) NOT NULL COMMENT 'Unique identificator. Tells in which part of the module the file is used. Teoretically there could be two identical records. The same module binds the same file to the same instance. For example: gallery widget adds the same photo twice.',
   `createdAt` int(11) NOT NULL COMMENT 'Time, when this module started to use this resource.',
   PRIMARY KEY (`fileId`),
