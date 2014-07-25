8b0aab726e5505ef2b60f87291e87b4d6b8a7900
maskas <mangirdas@impresspages.org>
2014-07-25 09:59:56 +0300

    update version number

--- Ip/Application.php
+++ Ip/Application.php
@@ -35,7 +35,7 @@ class Application
      */
     public static function getVersion()
     {
-        return '4.1.1'; //CHANGE_ON_VERSION_UPDATE
+        return '4.1.2'; //CHANGE_ON_VERSION_UPDATE
     }
 
 
