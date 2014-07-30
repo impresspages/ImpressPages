38c0780437935dca14bdef42090bb1c9d8e39cf2
maskas <mangirdas@impresspages.org>
2014-07-30 14:35:14 +0300

    better ecommerce exceptions

--- Ip/Ecommerce.php
+++ Ip/Ecommerce.php
@@ -12,7 +12,7 @@ class Ecommerce
     public function subscriptionPaymentUrl($options)
     {
         if (empty($options['item'])) {
-            throw new \Ip\Exception('item name required');
+            throw new \Ip\Exception('"item" setting is missing in subscriptionPaymentUrl function');
         }
         $paymentUrl = ipJob('ipSubscriptionPaymentUrl', $options);
         return $paymentUrl;
@@ -21,7 +21,13 @@ class Ecommerce
     public function paymentUrl($options)
     {
         if (empty($options['item'])) {
-            throw new \Ip\Exception('item name required');
+            throw new \Ip\Exception('"item" setting is missing in paymentUrl function');
+        }
+        if (empty($options['price'])){
+            throw new \Ip\Exception('"price" setting is missing in paymentUrl function');
+        }
+        if (empty($options['currency'])) {
+            throw new \Ip\Exception('"currency" setting is missing in paymentUrl function');
         }
         $paymentUrl = ipJob('ipPaymentUrl', $options);
         return $paymentUrl;
