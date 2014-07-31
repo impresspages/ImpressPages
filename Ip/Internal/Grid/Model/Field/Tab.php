c4d91f073673d368571dd2991642024d5b52ce60
maskas <mangirdas@impresspages.org>
2014-07-31 22:33:26 +0300

    support tabs when creating new record

---
+++ Ip/Internal/Grid/Model/Field/Tab.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @package   ImpressPages
+ */
+
+namespace Ip\Internal\Grid\Model\Field;
+
+
+class Tab extends \Ip\Internal\Grid\Model\Field
+{
+
+
+    public function createField()
+    {
+        return new \Ip\Form\Field\Info();
+    }
+
+    public function createData($postData)
+    {
+        return array();
+    }
+
+    public function updateField($curData)
+    {
+        return new \Ip\Form\Field\Info();
+    }
+
+    public function updateData($postData)
+    {
+        return array();
+    }
+
+
+    public function searchField($searchVariables)
+    {
+        return new \Ip\Form\Field\Info();
+    }
+
+    public function searchQuery($searchVariables)
+    {
+        return new \Ip\Form\Field\Info();
+    }
+}
