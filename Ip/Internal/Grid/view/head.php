0430d0e2b4d8fc5ac7e30116eb673c9ba5f9cb86
Audrius Jankauskas <audrius@impresspages.org>
2014-08-24 17:40:49 +0300

    Layout fix for nested Grid titles

--- Ip/Internal/Grid/view/head.php
+++ Ip/Internal/Grid/view/head.php
@@ -1,8 +1,12 @@
-<?php if (!empty($breadcrumb) && is_array($breadcrumb)) { ?>
-    <?php foreach($breadcrumb as $key => $crumb) { ?>
-        <?php if ($key > 0) { ?>
-            <h1> > </h1>
+<?php
+    $lastCrumb = array_pop($breadcrumb);
+    if (!empty($breadcrumb) && is_array($breadcrumb)) {
+?>
+    <ol class="breadcrumb">
+        <?php foreach($breadcrumb as $key => $crumb) { ?>
+            <li><a href="<?php echo escAttr($crumb['url']); ?>"><?php echo esc($crumb['title']); ?></a></li>
         <?php } ?>
-        <a href="<?php echo escAttr($crumb['url']) ?>"><h1><?php echo esc($crumb['title']) ?></h1></a>
-    <?php } ?>
+        <li class="active"><?php echo esc($lastCrumb['title']); ?></li>
+    </ol>
 <?php } ?>
+<h1><?php echo esc($title); ?></h1>
