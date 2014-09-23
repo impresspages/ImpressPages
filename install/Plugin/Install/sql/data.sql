d3dae73630761386769e8b3bcede1658e05e5491
Mangirdas Skripka <mangirdas@impresspages.org>
2014-09-23 22:57:22 +0300

    update version number 4.2.1

--- install/Plugin/Install/sql/data.sql
+++ install/Plugin/Install/sql/data.sql
@@ -40,8 +40,8 @@ INSERT INTO `ip_widget_order` (`widgetName`, `priority`) VALUES
 
 
 INSERT INTO `ip_storage` (`plugin`, `key`, `value`) VALUES
-('Ip', 'version', '"4.2.0"'),  /* //CHANGE_ON_VERSION_UPDATE */
-('Ip', 'dbVersion', '56'), /* //CHANGE_ON_VERSION_UPDATE */
+('Ip', 'version', '"4.2.1"'),  /* //CHANGE_ON_VERSION_UPDATE */
+('Ip', 'dbVersion', '57'), /* //CHANGE_ON_VERSION_UPDATE */
 ('Ip', 'theme', '"Air"'),
 ('Ip', 'cachedBaseUrl', ''),
 ('Ip', 'lastSystemMessageSent', ''),
