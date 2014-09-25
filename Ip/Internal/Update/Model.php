afe2ff184f2ba71548d6276760b6c2a47c2fca4c
Mangirdas Skripka <mangirdas@impresspages.org>
2014-09-25 23:51:04 +0300

    v4.2.2

--- Ip/Internal/Update/Model.php
+++ Ip/Internal/Update/Model.php
@@ -16,7 +16,7 @@ class Model
      */
     public static function getDbVersion()
     {
-        return 57; //CHANGE_ON_VERSION_UPDATE
+        return 58; //CHANGE_ON_VERSION_UPDATE
     }
 
     public static function migrationsAvailable()
--- install/Plugin/Install/sql/data.sql
+++ install/Plugin/Install/sql/data.sql
@@ -40,8 +40,8 @@ INSERT INTO `ip_widget_order` (`widgetName`, `priority`) VALUES
 
 
 INSERT INTO `ip_storage` (`plugin`, `key`, `value`) VALUES
-('Ip', 'version', '"4.2.1"'),  /* //CHANGE_ON_VERSION_UPDATE */
-('Ip', 'dbVersion', '57'), /* //CHANGE_ON_VERSION_UPDATE */
+('Ip', 'version', '"4.2.2"'),  /* //CHANGE_ON_VERSION_UPDATE */
+('Ip', 'dbVersion', '58'), /* //CHANGE_ON_VERSION_UPDATE */
 ('Ip', 'theme', '"Air"'),
 ('Ip', 'cachedBaseUrl', ''),
 ('Ip', 'lastSystemMessageSent', ''),
