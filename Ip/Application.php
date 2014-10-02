b7db094d67015a619a850dff65c7559c452752f1
Mangirdas Skripka <mangirdas@impresspages.org>
2014-10-02 15:12:45 +0300

    update version number

--- Ip/Application.php
+++ Ip/Application.php
@@ -35,7 +35,7 @@ class Application
      */
     public static function getVersion()
     {
-        return '4.2.2'; //CHANGE_ON_VERSION_UPDATE
+        return '4.2.3'; //CHANGE_ON_VERSION_UPDATE
     }
 
 
--- Ip/Internal/Update/Migration.php
+++ Ip/Internal/Update/Migration.php
@@ -11,6 +11,12 @@ class Migration
 {
     //CHANGE_ON_VERSION_UPDATE
 
+    public static function update_59()
+    {
+        ipStorage()->set('Ip', 'cacheVersion', ipStorage()->get('Ip', 'cacheVersion', 1) + 1);
+        ipDb()->update('storage', array('value' => '"4.2.3"'), array('key' => 'version', 'plugin' => 'Ip'));
+    }
+
     public static function update_58()
     {
         ipStorage()->set('Ip', 'cacheVersion', ipStorage()->get('Ip', 'cacheVersion', 1) + 1);
