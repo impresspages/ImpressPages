bd68140dc5b4098f689f20c516f34f3d6f4bc838
Mangirdas Skripka <mangirdas@impresspages.org>
2014-10-06 16:35:52 +0300

    GRID breadcrumbField setting

--- Ip/Internal/Grid/view/head.php
+++ Ip/Internal/Grid/view/head.php
@@ -1,12 +1,10 @@
 <?php
-    $lastCrumb = array_pop($breadcrumb);
     if (!empty($breadcrumb) && is_array($breadcrumb)) {
 ?>
     <ol class="breadcrumb">
         <?php foreach($breadcrumb as $key => $crumb) { ?>
             <li><a href="<?php echo escAttr($crumb['url']); ?>"><?php echo esc($crumb['title']); ?></a></li>
         <?php } ?>
-        <li class="active"><?php echo esc($lastCrumb['title']); ?></li>
     </ol>
 <?php } ?>
 <h1><?php echo esc($title); ?></h1>
