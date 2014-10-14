7c6eb8af4128b7186ebc88cb9b1449263198e8c5
Mangirdas Skripka <mangirdas@impresspages.org>
2014-10-14 17:52:29 +0300

    Fix content exception class. Fixes #569

--- Ip/Exception/Content.php
+++ Ip/Exception/Content.php
@@ -8,7 +8,7 @@ class Content extends \Ip\Exception
     public function __construct($message, $code = 0, \Exception $previous = null)
     {
         // make sure everything is assigned properly
-        parent::__construct($message, $code, $previous);
+        parent::__construct($message, null,  $code, $previous);
     }
 
 
