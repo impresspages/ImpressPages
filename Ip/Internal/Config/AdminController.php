a33b142253b66b405ead925cf32b8af0d6d70f95
Mangirdas Skripka <mangirdas@impresspages.org>
2014-10-25 15:28:58 +0300

    Anonymous uploads config

--- Ip/Internal/Config/AdminController.php
+++ Ip/Internal/Config/AdminController.php
@@ -51,7 +51,7 @@ class AdminController extends \Ip\Controller
             !in_array($fieldName, array('websiteTitle', 'websiteEmail'))
             &&
             !(
-                in_array($fieldName, array('automaticCron', 'cronPassword', 'removeOldRevisions', 'removeOldRevisionsDays', 'removeOldEmails', 'removeOldEmailsDays'))
+                in_array($fieldName, array('automaticCron', 'cronPassword', 'removeOldRevisions', 'removeOldRevisionsDays', 'removeOldEmails', 'removeOldEmailsDays', 'allowAnonymousUploads'))
                 &&
                 ipAdminPermission('Config advanced')
             )
