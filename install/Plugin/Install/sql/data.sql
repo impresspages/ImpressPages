04517fb0fb60fa9569cc1c1d19b067312b16d4bf
Mangirdas Skripka <mangirdas@impresspages.org>
2014-10-27 17:43:01 +0200

    v4.2.7

--- install/Plugin/Install/sql/data.sql
+++ install/Plugin/Install/sql/data.sql
@@ -40,8 +40,8 @@ INSERT INTO `ip_widget_order` (`widgetName`, `priority`) VALUES
 
 
 INSERT INTO `ip_storage` (`plugin`, `key`, `value`) VALUES
-('Ip', 'version', '"4.2.6"'),  /* //CHANGE_ON_VERSION_UPDATE */
-('Ip', 'dbVersion', '67'), /* //CHANGE_ON_VERSION_UPDATE */
+('Ip', 'version', '"4.2.7"'),  /* //CHANGE_ON_VERSION_UPDATE */
+('Ip', 'dbVersion', '68'), /* //CHANGE_ON_VERSION_UPDATE */
 ('Ip', 'theme', '"Air"'),
 ('Ip', 'cachedBaseUrl', ''),
 ('Ip', 'lastSystemMessageSent', ''),
