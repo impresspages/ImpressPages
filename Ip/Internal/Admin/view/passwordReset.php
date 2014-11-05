5f1b25b04e0eff2c577eb8ec4a4f25471079e090
Mangirdas Skripka <mangirdas@impresspages.org>
2014-11-05 23:13:15 +0200

    remove unnecesarry echo

--- Ip/Internal/Admin/view/passwordReset.php
+++ Ip/Internal/Admin/view/passwordReset.php
@@ -4,6 +4,6 @@
 <div class="ip loginContent">
     <?php echo $passwordResetForm->render(); ?>
     <p class="alternativeLink">
-        <a href="<?php echo ipFileUrl('admin'); ?>"><?php echo _e('Login', 'Ip-admin'); ?></a>
+        <a href="<?php echo ipFileUrl('admin'); ?>"><?php _e('Login', 'Ip-admin'); ?></a>
     </p>
 </div>
