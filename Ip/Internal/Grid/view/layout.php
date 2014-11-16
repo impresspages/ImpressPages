fb8c47df557c4f95f2013d19c808500bc8ddecf1
Mangirdas Skripka <mangirdas@impresspages.org>
2014-11-16 22:42:58 +0200

    move page size to bottom

--- Ip/Internal/Grid/view/layout.php
+++ Ip/Internal/Grid/view/layout.php
@@ -1,8 +1,8 @@
 <?php echo ipView('Ip/Internal/Grid/view/head.php', $this->getVariables()); ?>
-<?php echo ipView('Ip/Internal/Grid/view/pageSize.php', $this->getVariables()); ?>
 <?php echo ipView('Ip/Internal/Grid/view/actions.php', $this->getVariables()); ?>
 <?php echo ipView('Ip/Internal/Grid/view/table.php', $this->getVariables()); ?>
 <?php echo $pagination->render(ipFile('Ip/Internal/Grid/view/pagination.php')); ?>
+<?php echo ipView('Ip/Internal/Grid/view/pageSize.php', $this->getVariables()); ?>
 <?php echo ipView('Ip/Internal/Grid/view/deleteModal.php', $this->getVariables()); ?>
 <?php echo ipView('Ip/Internal/Grid/view/updateModal.php', $this->getVariables()); ?>
 <?php echo ipView('Ip/Internal/Grid/view/createModal.php', $this->getVariables()); ?>
