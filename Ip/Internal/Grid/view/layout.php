d7a4faa09bcdd9b683c6064ffb0dff43e9fa84b1
Mangirdas Skripka <mangirdas@impresspages.org>
2014-11-16 23:35:45 +0200

    show pagination at the top

--- Ip/Internal/Grid/view/layout.php
+++ Ip/Internal/Grid/view/layout.php
@@ -1,8 +1,10 @@
 <?php echo ipView('Ip/Internal/Grid/view/head.php', $this->getVariables()); ?>
 <?php echo ipView('Ip/Internal/Grid/view/actions.php', $this->getVariables()); ?>
+<?php echo $pagination->render(ipFile('Ip/Internal/Grid/view/pagination.php')); ?>
+<?php echo ipView('Ip/Internal/Grid/view/pageSize.php', array_merge($this->getVariables(), array('position' => 'top'))); ?>
 <?php echo ipView('Ip/Internal/Grid/view/table.php', $this->getVariables()); ?>
 <?php echo $pagination->render(ipFile('Ip/Internal/Grid/view/pagination.php')); ?>
-<?php echo ipView('Ip/Internal/Grid/view/pageSize.php', $this->getVariables()); ?>
+<?php echo ipView('Ip/Internal/Grid/view/pageSize.php', array_merge($this->getVariables(), array('position' => 'bottom'))); ?>
 <?php echo ipView('Ip/Internal/Grid/view/deleteModal.php', $this->getVariables()); ?>
 <?php echo ipView('Ip/Internal/Grid/view/updateModal.php', $this->getVariables()); ?>
 <?php echo ipView('Ip/Internal/Grid/view/createModal.php', $this->getVariables()); ?>
