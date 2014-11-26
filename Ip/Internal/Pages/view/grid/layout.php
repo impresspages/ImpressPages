9a4d62db017c16603bc9895011e6734d56cb1112
Mangirdas Skripka <mangirdas@impresspages.org>
2014-11-26 13:04:54 +0200

    update Pages list style moving

--- Ip/Internal/Pages/view/grid/layout.php
+++ Ip/Internal/Pages/view/grid/layout.php
@@ -4,7 +4,8 @@ echo ipView('Ip/Internal/Grid/view/header.php', $this->getVariables());
 
 // Actions
 echo ipView('Ip/Internal/Grid/view/actions.php', $this->getVariables());
-if ($pagination->pagerSize() * ($pagination->currentPage() - 1) + count($data) > 100) {
+if ($pagination->pagerSize() * ($pagination->totalPages() - 1) + count($data) > 100) {
+    // Show top pagination if we have more than 100 records.
     echo ipView('Ip/Internal/Grid/view/pages.php', array_merge($this->getVariables(), array('position' => 'top')));
 }
 
@@ -12,7 +13,8 @@ if ($pagination->pagerSize() * ($pagination->currentPage() - 1) + count($data) >
 echo ipView('table.php', $this->getVariables());
 
 // Actions
-if ($pagination->currentPage() > 1 || count($data) > 10) {
+if ($pagination->pagerSize() * ($pagination->totalPages() - 1) + count($data) > 10 || $pagination->currentPage() > 1) {
+    //show pagination if we have more than 10 records
     echo ipView('Ip/Internal/Grid/view/pages.php', array_merge($this->getVariables(), array('position' => 'bottom')));
 }
 
@@ -21,7 +23,9 @@ echo ipView('Ip/Internal/Grid/view/deleteModal.php', $this->getVariables());
 echo ipView('Ip/Internal/Grid/view/updateModal.php', $this->getVariables());
 echo ipView('Ip/Internal/Grid/view/createModal.php', $this->getVariables());
 echo ipView('Ip/Internal/Grid/view/searchModal.php', $this->getVariables());
+echo ipView('Ip/Internal/Grid/view/moveModal.php', $this->getVariables());
 
 // Footer
 echo ipView('Ip/Internal/Grid/view/footer.php', $this->getVariables());
+
 ?>
