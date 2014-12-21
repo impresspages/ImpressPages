825ca595b02ea1d99df5a715ff57c03ff33053ba
Mangirdas Skripka <mangirdas@impresspages.org>
2014-12-21 17:31:20 +0200

    make Heading widget translations compatible

--- Ip/Internal/Content/Widget/Heading/snippet/controls.php
+++ Ip/Internal/Content/Widget/Heading/snippet/controls.php
@@ -3,7 +3,8 @@
         <div class="btn-toolbar" role="toolbar">
             <div class="btn-group">
                 <?php for($i=1; $i <= $maxLevel; $i++) { ?>
-                <button type="button" data-level="<?php echo $i ?>" class="btn btn-controls ipsH"><?php _e('H' . $i, 'Ip-admin'); ?></button>
+                    <?php $text = 'H' . $i; ?>
+                <button type="button" data-level="<?php echo $i ?>" class="btn btn-controls ipsH"><?php _e($text, 'Ip-admin'); ?></button>
                 <?php } ?>
                 <button type="button" class="btn btn-controls ipsOptions"><?php _e('Options', 'Ip-admin'); ?></button>
             </div>
