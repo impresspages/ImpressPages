0247a1e0df14dd6f672af772b5b988d06bccb77f
Mangirdas Skripka <mangirdas@impresspages.org>
2015-01-12 16:42:48 +0200

    make public TinyMCE config similar to the admin.

--- Ip/Internal/Core/assets/tinymce/defaultPublic.js
+++ Ip/Internal/Core/assets/tinymce/defaultPublic.js
@@ -4,6 +4,22 @@
  */
 
 ipTinyMceConfigPublic = function () {
+
+    return {
+        directionality : ip.languageTextDirection,
+        plugins: "advlist, paste, link, table, colorpicker, textcolor, anchor, alignrollup, autolink",
+        entity_encoding: "raw",
+        menubar: false,
+        statusbar: false,
+        toolbar1: 'bold italic alignrollup styleselect removeformat table, undo redo',
+        toolbar2: 'link bullist numlist outdent indent subscript superscript forecolor backcolor',
+        paste_word_valid_elements: "table,tbody,tr,td,th,strong,em,br,sup,sub,p,span,b,u,i,a,ul,ol,li",
+        forced_root_block: "p",
+        allow_script_urls: false,
+        convert_urls: false
+    };
+
+
     return {
         selector: "textarea",
         plugins: [
