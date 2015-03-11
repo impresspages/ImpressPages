f6d4f641e34497ddc966f40ab419b02defb3d53b
Mangirdas Skripka <mangirdas@impresspages.org>
2015-03-11 15:26:42 +0200

    update script

--- Ip/Internal/Update/Model.php
+++ Ip/Internal/Update/Model.php
@@ -16,7 +16,7 @@ class Model
      */
     public static function getDbVersion()
     {
-        return 79; //CHANGE_ON_VERSION_UPDATE
+        return 80; //CHANGE_ON_VERSION_UPDATE
     }
 
     public static function migrationsAvailable()
