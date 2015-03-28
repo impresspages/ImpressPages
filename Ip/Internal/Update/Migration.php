62498657817553fd6678b0b801322247f253678a
Mangirdas Skripka <mangirdas@impresspages.org>
2015-03-28 12:53:23 +0200

    4.6.0

--- Ip/Internal/Update/Migration.php
+++ Ip/Internal/Update/Migration.php
@@ -13,6 +13,13 @@ class Migration
 
     //CHANGE_ON_VERSION_UPDATE
 
+    public static function update_81()
+    {
+        ipStorage()->set('Ip', 'cacheVersion', ipStorage()->get('Ip', 'cacheVersion', 1) + 1);
+        ipDb()->update('storage', array('value' => '"4.6.0"'), array('key' => 'version', 'plugin' => 'Ip'));
+    }
+
+
     public static function update_80()
     {
         ipStorage()->set('Ip', 'cacheVersion', ipStorage()->get('Ip', 'cacheVersion', 1) + 1);
--- Ip/Internal/Update/Model.php
+++ Ip/Internal/Update/Model.php
@@ -16,7 +16,7 @@ class Model
      */
     public static function getDbVersion()
     {
-        return 80; //CHANGE_ON_VERSION_UPDATE
+        return 81; //CHANGE_ON_VERSION_UPDATE
     }
 
     public static function migrationsAvailable()
