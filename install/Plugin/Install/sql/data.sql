bab9c891566eb2f952341926a389dd7e8bd9af61
Mangirdas Skripka <mangirdas@impresspages.org>
2015-11-01 22:24:54 +0200

    update to 4.6.4

--- install/Plugin/Install/sql/data.sql
+++ install/Plugin/Install/sql/data.sql
@@ -61,8 +61,8 @@ INSERT INTO `ip_storage` (`plugin`, `key`, `value`) VALUES
 ('Config', 'Pages.hideNewPages', '0'),
 ('Cron', 'lastExecutionStart', '[[[[time]]]]'),
 ('Cron', 'lastExecutionEnd', '[[[[time]]]]'),
-('Ip', 'version', '"4.6.3"'),  /* //CHANGE_ON_VERSION_UPDATE */
-('Ip', 'dbVersion', '84'), /* //CHANGE_ON_VERSION_UPDATE */
+('Ip', 'version', '"4.6.4"'),  /* //CHANGE_ON_VERSION_UPDATE */
+('Ip', 'dbVersion', '85'), /* //CHANGE_ON_VERSION_UPDATE */
 ('Ip', 'theme', '"Air"'),
 ('Ip', 'cachedBaseUrl', ''),
 ('Ip', 'lastSystemMessageSent', ''),
