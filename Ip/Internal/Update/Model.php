acca7a7036718a5cad7279c555d8613a33f02a67
Mangirdas Skripka <mangirdas@impresspages.org>
2016-10-30 09:09:57 +0200

    v480

--- Ip/Internal/Update/Model.php
+++ Ip/Internal/Update/Model.php
@@ -16,7 +16,7 @@ class Model
      */
     public static function getDbVersion()
     {
-        return 92; //CHANGE_ON_VERSION_UPDATE
+        return 93; //CHANGE_ON_VERSION_UPDATE
     }
 
     public static function migrationsAvailable()
