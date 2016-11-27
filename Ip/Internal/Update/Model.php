164e8ec110fd1fc4c92bf67802d236fc3b631a34
Mangirdas Skripka <mangirdas.skripka@gmail.com>
2016-11-27 21:39:06 +0200

    update to 4.8.1

--- Ip/Internal/Update/Model.php
+++ Ip/Internal/Update/Model.php
@@ -16,7 +16,7 @@ class Model
      */
     public static function getDbVersion()
     {
-        return 93; //CHANGE_ON_VERSION_UPDATE
+        return 94; //CHANGE_ON_VERSION_UPDATE
     }
 
     public static function migrationsAvailable()
