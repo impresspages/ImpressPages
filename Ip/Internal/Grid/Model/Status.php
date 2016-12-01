ec412b4041b79ac03e8523507c3da45b4c7ec749
Mangirdas Skripka <mangirdas.skripka@gmail.com>
2016-12-01 22:13:36 +0200

    remove magic quotes related code

--- Ip/Internal/Grid/Model/Status.php
+++ Ip/Internal/Grid/Model/Status.php
@@ -73,38 +73,4 @@ class Status
         }
         return $newStatusVariables;
     }
-
-//    protected $config = null;
-//
-//    public function __construct($hash)
-//    {
-//        $this->config = fixMagicQuotes(parse_str($hash));
-//    }
-//
-//
-//    public function hash()
-//    {
-//
-//    }
-//
-//    protected function fixMagicQuotes($data)
-//    {
-//        if (!get_magic_quotes_gpc()) {
-//            return;
-//        }
-//
-//        $process = array(&$data);
-//        while (list($key, $val) = each($process)) {
-//            foreach ($val as $k => $v) {
-//                unset($process[$key][$k]);
-//                if (is_array($v)) {
-//                    $process[$key][stripslashes($k)] = $v;
-//                    $process[] = & $process[$key][stripslashes($k)];
-//                } else {
-//                    $process[$key][stripslashes($k)] = stripslashes($v);
-//                }
-//            }
-//        }
-//        unset($process);
-//    }
 }
--- Ip/Request.php
+++ Ip/Request.php
@@ -259,31 +259,6 @@ class Request
 
     /**
      * @ignore
-     */
-    public function fixMagicQuotes()
-    {
-        if (!get_magic_quotes_gpc()) {
-            return;
-        }
-
-        $process = array(&$_GET, &$_POST, &$_COOKIE, &$_REQUEST);
-        while (list($key, $val) = each($process)) {
-            foreach ($val as $k => $v) {
-                unset($process[$key][$k]);
-                if (is_array($v)) {
-                    $process[$key][stripslashes($k)] = $v;
-                    $process[] = & $process[$key][stripslashes($k)];
-                } else {
-                    $process[$key][stripslashes($k)] = stripslashes($v);
-                }
-            }
-        }
-        unset($process);
-    }
-
-
-    /**
-     * @ignore
      * @return bool true if current url is pointing to website root or one of the languages
      */
     public function _isWebsiteRoot()
