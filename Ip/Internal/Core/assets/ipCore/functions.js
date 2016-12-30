1b655c959e5b324de6c6095a0c8c4da9e6a85370
Mangirdas Skripka <mangirdas.skripka@gmail.com>
2016-12-30 12:28:43 +0200

    resstore urlOverrides config

--- Ip/Internal/Core/assets/ipCore/functions.js
+++ Ip/Internal/Core/assets/ipCore/functions.js
@@ -2,6 +2,11 @@
  Default ImpressPages functions
  */
 function ipFileUrl(path) {
+    for (var prefix in ipUrlOverrides) {
+        if (path.indexOf(prefix) == 0) {
+            return ipUrlOverrides[prefix] + path.substr(prefix.length);
+        }
+    }
     return ip.baseUrl + path;
 }
 
