ffb0d45ff94cb17a698a4ecf591189c776461aec
Mangirdas Skripka <mangirdas.skripka@gmail.com>
2017-01-03 17:21:28 +0200

    correct db migration target

--- Ip/Internal/Update/Model.php
+++ Ip/Internal/Update/Model.php
@@ -16,7 +16,7 @@ class Model
      */
     public static function getDbVersion()
     {
-        return 96; //CHANGE_ON_VERSION_UPDATE
+        return 97; //CHANGE_ON_VERSION_UPDATE
     }
 
     public static function migrationsAvailable()
