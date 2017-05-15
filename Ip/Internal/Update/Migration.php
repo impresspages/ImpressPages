fa51386b08928b949c534e65ff037d7ce3585e49
Mangirdas Skripka <mangirdas.skripka@gmail.com>
2017-05-15 22:29:56 +0300

    v5.0.2

--- Ip/Internal/Update/Migration.php
+++ Ip/Internal/Update/Migration.php
@@ -12,6 +12,12 @@ class Migration
 
 
     //CHANGE_ON_VERSION_UPDATE
+    public static function update_100()
+    {
+        ipStorage()->set('Ip', 'cacheVersion', ipStorage()->get('Ip', 'cacheVersion', 1) + 1);
+        ipDb()->update('storage', array('value' => '"5.0.2"'), array('key' => 'version', 'plugin' => 'Ip'));
+    }
+
     public static function update_99()
     {
         ipStorage()->set('Ip', 'cacheVersion', ipStorage()->get('Ip', 'cacheVersion', 1) + 1);
