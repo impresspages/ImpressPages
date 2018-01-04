784fd3fde01494ebcf2744af2fed284465c07026
Pavel Azanov <pavel.azanov@pb.com>
2018-01-04 15:36:25 +0100

    chore(repository): add copyright

--- Ip/Internal/Repository/assets/ipRepositoryAll.js
+++ Ip/Internal/Repository/assets/ipRepositoryAll.js
@@ -80,6 +80,20 @@
 
 
         _isVisible: function (element, partial, hidden, direction, container) {
+
+            /**
+             * Based on https://github.com/customd/jquery-visible
+             *
+             * Copyright 2012, Digital Fusion
+             * Licensed under the MIT license.
+             * http://teamdf.com/jquery-plugins/license/
+             *
+             * @author Sam Sehnert
+             * @desc A small plugin that checks whether elements are within
+             *       the user visible viewport of a web browser.
+             *       only accounts for vertical position, not horizontal.
+             */
+
             // Set direction default to 'both'.
             direction = direction || 'both';
 
